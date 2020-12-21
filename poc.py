e37401ab107ce720b22430b0bbe8e6cfb6c48130
Andreas.Olofsson <andreas@adapteva.com>
2020-12-21 13:42:45 -0500

    Changing strcture for python packaing

--- poc.py
+++ poc.py
@@ -2,13 +2,22 @@
 import os
 import re
 import sys
-import multiprocessing as mp
+import multiprocessing
+import time
 
 class chip:
-    def __init__(self, name):
+    def __init__(self, cfg, name):
         self.id=name                            #name of object
         self.cfg=cfg                            #
         print("***New***")
+        lec_running=0
+        sta_running=0
+        si_running=0
+        pi_running=0
+        drc_running=0
+        lvs_running=0
+    #HELPER THREAD
+    
     #LAYOUT FLOW
     def setup(self,output):
          print("***Setup***")
@@ -24,20 +33,37 @@ class chip:
          print("***Cleanup***")
     def gdsout(self,input,output):
          print("***Export***")
+
     #VERIFICATION FLOW
-    def leq(self,output):
-         print("***Logical Equivalence***")   
-    def sta(self,input,output):
+    def leq(self,dir):
+         lec_running=1
+         time.sleep(25)
+         print("***Logical Equivalence***")
+    def sta(self,dir):
+         sta_running=1
+         time.sleep(20)
          print("***Timing Signoff***")
-    def si(self,input,output):
+    def si(self,dir):
+         se_running=1
+         time.sleep(15)
          print("***Signal Integrity***")
-    def pi(self,input,output):
-         print("***Power Integrity***")     
-    def drc(self,input,output):
+    def pi(self,dir):
+         pi_running=1
+         time.sleep(10)
+         print("***Power Integrity***")
+    def drc(self,dir):
+         drc_running=1
+         time.sleep(5)
          print("***DRC***")
-    def lvs(self,input,output):
-         print("***LVDS***")
-    
+    def lvs(self,dir):
+         lvs_running=1
+         time.sleep(3)
+         print("***LVS***")
+    def signoff(self,dir):
+         lvs_running=1
+         time.sleep(3)
+         print("***LVS***")
+
 
 ##########################################################
 #End to End Linear Execution flow
@@ -60,10 +86,11 @@ a.export("cfg6","g","h")
 
 ##########################################################
 #Select from the best (one directory per stage)
+#recommended methodology
 ##########################################################
 a.setup("b")
 a.syn("cfg1","setup","syn")
-for(i=0;i<10;i++):
+for x in range(10):
     a.place("cfg2","syn","place" + n)
 #Check Max, the set d accordingly
 a.cts("cfg3","d","e")
@@ -72,20 +99,32 @@ a.cleanup("cfg5","f","g")
 a.export("cfg6","g","h")
 
 ##########################################################
-#Implicit Fork Join Flow
+#Verification Queue
 ##########################################################
 
 ## Servers, # threads, blocking-nonblocking set by config
 
-wait="wait"
-myname="h"
+#Input is a directory to check
+#One directory per design point, easy to tar up
 
-a.leq(myname) ##servers, blocking, # threads
-a.sta(myname)
-a.si(myname)
-a.pi(myname)
-a.drc(myname)
-a.lvs(myname)
-a.signoff(myname) #blocks inside
+
+#Setup
+
+#Execution (fluid...)
+jobdir="job0/hello"           # job directory (alphanumeric)
+a.leq(jobdir)       # logical equivalence check
+a.sta(jobdir)       # static timing checks
+a.si(jobdir)        # signal integrity check 
+a.pi(jobdir)        # power integrity check
+a.drc(jobdir)       # drc check
+a.lvs(jobdir)       # lvs check
+a.signoff(jobdir)   # check that all checks have been run
+
+#if async, the job goes into a queue
+#if sync, it 
+#everything is a directory
+#file names are the same
+#separate
+#signoff should print out a columnized schedule of progress
 
 
