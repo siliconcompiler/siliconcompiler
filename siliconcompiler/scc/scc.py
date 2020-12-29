7c3e11c4bc902b6adbf890366802d94312f8eb32
Andreas.Olofsson <andreas@adapteva.com>
2020-12-29 14:07:16 -0500

    Creating functions fo setting scc_args by priority

--- siliconcompiler/scc/scc.py
+++ siliconcompiler/scc/scc.py
@@ -6,14 +6,169 @@ import argparse
 import os
 import re
 
- ###########################################################################
- # Deriving install directory
- ###########################################################################
-script_path = os.path.dirname(os.path.abspath(__file__))
+############################
+# FUNCTIONS
+############################
 
+def run(config_file, stage, input_dir, output_dir):
 
+    #Read scc_args from json file
+    with open(os.path.abspath(config_file), "r") as f:
+        scc_args = json.load(f)
+
+    #Moving to working directory
+    cwd = os.getcwd()
+    os.makedirs(os.path.abspath(output_dir), exist_ok=True)
+    os.chdir(os.path.abspath(output_dir))
+
+    #Dump TCL (EDA tcl lacks support for json)
+    with open("scc_setup.tcl", 'w') as f:
+        for key in sorted(scc_args.keys()):
+            print('set ', key , scc_args[key], file=f)
+    
+    #Execute EDA tool
+    tool    = scc_args['scc_' + stage + '_tool']
+    script  = os.path.abspath(scc_args['scc_' + stage + '_script'])
+    cmd = tool + " " + script
+    print(cmd)
+    error   = subprocess.run(cmd, shell=True)
+    
+    #Return to CWD
+    os.chdir(cwd)
+
+def design_import(config_file, output_dir):
+
+    #Read scc_args from json file
+    with open(os.path.abspath(config_file), "r") as f:
+        scc_args = json.load(f)
+
+    #Constructing verilator command
+    cmd_fields = ["verilator --lint-only --debug"]
+
+    for value in scc_args['scc_ydir']:
+        cmd_fields.append('-y ' + os.path.abspath(value))
+    for value in scc_args['scc_vlib']:
+        cmd_fields.append('-v ' + os.path.abspath(value))
+    for value in scc_args['scc_idir']:
+        cmd_fields.append('-I ' + os.path.abspath(value))
+    for value in scc_args['scc_define']:
+        cmd_fields.append('-D ' + value)
+    for value in scc_args['scc_source']:
+        cmd_fields.append(os.path.abspath(value))   
+
+    cmd = ' '.join(cmd_fields);
+    
+    #Working directory
+    cwd = os.getcwd()
+    os.makedirs(os.path.abspath(output_dir), exist_ok=True)
+    os.chdir(os.path.abspath(output_dir))
+      
+    #Execute Parser/Linter
+    error = subprocess.run(cmd, shell=True)
+    error = subprocess.run('cat obj_dir/*.vpp > output.vpp', shell=True)
+    
+    #Return to CWD
+    os.chdir(cwd)
+
+def cfg_default():
+    
+    install_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir    = re.sub("siliconcompiler/siliconcompiler", "siliconcompiler",install_dir,1)
+    pdklib      = root_dir + "/third_party/pdklib/virtual/nangate45/r1p0/pnr/"
+    iplib       = root_dir + "/third_party/iplib/virtual/nangate45/NangateOpenCellLibrary/r1p0/lib/"
+
+    args = {}
+    args['scc_import_dir']     = "build/import"
+    args['scc_syn_tool']       = "yosys -c"
+    args['scc_syn_script']     = install_dir + "asic/syn.tcl"
+    args['scc_syn_dir']        = "build/syn"
+    args['scc_place_tool']     = "openroad -no_init -exit"
+    args['scc_place_dir']      = "build/place"
+    args['scc_place_script']   = install_dir + "asic/place.tcl"
+    args['scc_cts_tool']       = "openroad -no_init -exit"
+    args['scc_cts_script']     = install_dir + "asic/cts.tcl"
+    args['scc_cts_dir']        = "build/cts"
+    args['scc_route_tool']     = "openroad -no_init -exit"
+    args['scc_route_script']   = install_dir + "asic/route.tcl"
+    args['scc_route_dir']      = "build/route"
+    args['scc_signoff_tool']   = "openroad -no_init -exit"
+    args['scc_signoff_script'] = install_dir + "asic/signoff.tcl"
+    args['scc_signoff_dir']    = "build/signoff"
+    args['scc_export_tool']    = "openroad -no_init -exit"
+    args['scc_export_script']  = install_dir + "asic/export.tcl"
+    args['scc_export_dir']     = "build/export"
+    args['scc_techfile']       = pdklib + "nangate45.tech.lef"
+    args['scc_lib']            = iplib + "NangateOpenCellLibrary_typical.lib"
+    args['scc_minlayer']       = "M2"
+    args['scc_maxlayer']       = "M5"
+    args['scc_effort']         = "high"
+    args['scc_output']         = "output"
+    args['scc_jobs']           = 4
+    args['scc_start']          = "init"
+    args['scc_end']            = "export"
+
+    print(args)
+    return(args)
+
+def cfg_env():
+    args = {}
+    args['scc_ydir']      = os.getenv('SCC_YDIR')
+    args['scc_vlib']      = os.getenv('SCC_VLIB')
+    args['scc_idir']      = os.getenv('SCC_IDIR')
+    args['scc_libext']    = os.getenv('SCC_LIBEXT')
+    args['scc_cmdfile']   = os.getenv('SCC_CMDFILE')
+    args['scc_sdcfile']   = os.getenv('SCC_SDCFILE')
+    args['scc_flowname']  = os.getenv('SCC_FLOWNAME')
+    args['scc_techfile']  = os.getenv('SCC_TECHFILE')
+    args['scc_layermap']  = os.getenv('SCC_LAYERMAP')
+    args['scc_minlayer']  = os.getenv('SCC_MINLAYER')
+    args['scc_maxlayer']  = os.getenv('SCC_MAXLAYER')
+    args['scc_lib']       = os.getenv('SCC_LIB')
+    args['scc_scenario']  = os.getenv('SCC_SCENARIO')
+    args['scc_effort']    = os.getenv('SCC_EFFORT')
+    print(args)
+    return(args)
+def cfg_file():
+    print("file")
+def cfg_cmdline():
+    print("command")
+def cfg_merge(all_args):
+    scc_args = {}
+    print("merge")
+    return(scc_args)
+def cfg_print():
+    print("print")
+
+############################
+# COMMAND LINE SCRIPT
+############################
 if __name__ == "__main__":
 
+    script_path = os.path.dirname(os.path.abspath(__file__))
+
+    #1. Reading of config into mega dictionary
+    all_args = {}
+    all_args['default']  = cfg_default()  # set in scc.py 
+    all_args['env']      = cfg_env()      # set in .bashrc
+    all_args['file']     = cfg_file()     # set by -cfg
+    all_args['cmdline']  = cfg_cmdline()  # set by all others 
+ 
+    #2. Prioritized merge of all values
+    scc_args = cfg_merge(all_args)
+
+    #3. Setup design
+    design_import(scc_args, scc_args['scc_import_dir'])
+
+    #4. Run thhrough compiler
+    run(scc_args, "syn",     scc_args['scc_import_dir'],scc_args['scc_syn_dir'])
+    run(scc_args, "place",   scc_args['scc_syn_dir'],scc_args['scc_place_dir'])
+    run(scc_args, "cts",     scc_args['scc_place_dir'],scc_args['scc_cts_dir'])
+    run(scc_args, "route",   scc_args['scc_cts_dir'],scc_args['scc_route_dir'])
+    run(scc_args, "signoff", scc_args['scc_route_dir'],scc_args['scc_signoff_dir'])
+    run(scc_args, "export",  scc_args['scc_signoff_dir'],scc_args['scc_export_dir'])
+    sys.exit()
+  
+    
     ###########################################################################
     # Shared argument structure for all tools
     # 1. SCC Default (lowest)
@@ -30,50 +185,14 @@ if __name__ == "__main__":
 
     scc_args['default'] = {}
 
-    install_dir = os.path.dirname(os.path.abspath(__file__))
-    install_dir = re.sub("siliconcompiler/siliconcompiler", "siliconcompiler",install_dir,1)
-    pdklib      = install_dir + "/third_party/pdklib/"
-    iplib       = install_dir + "/third_party/iplib/"
-    edalib      = install_dir + "/third_party/edalib/"
-
-    scc_args['default']['scc_flow_init']   = "scc"
-    scc_args['default']['scc_flow_syn']    = "scc"
-    scc_args['default']['scc_flow_place']  = "scc"
-    scc_args['default']['scc_flow_cts']    = "scc"
-    scc_args['default']['scc_flow_route']  = "scc"
-    scc_args['default']['scc_flow_finish'] = "scc"
-
-    scc_args['default']['scc_techfile']    = pdklib + "virtual/nangate45/r1p0/pnr/nangate45.tech.lef"
-    scc_args['default']['scc_lib']         = iplib + "virtual/nangate45/NangateOpenCellLibrary/r1p0/lib/NangateOpenCellLibrary_typical.lib"
-    scc_args['default']['scc_minlayer']    = "M2"
-    scc_args['default']['scc_maxlayer']    = "M5"
-    scc_args['default']['effort']          = "high"
-    scc_args['default']['scc_output']      = "output"
-    scc_args['default']['scc_jobs']        = 1
-    scc_args['default']['scc_start']       = "init"
-    scc_args['default']['scc_end']         = "export"
+  
     
     
     ###########################################################################
     # Environment Configuration
     ###########################################################################
     
-    scc_args['env']['scc_home']      = os.getenv('SCC_HOME')
-    scc_args['env']['scc_ydir']      = os.getenv('SCC_YDIR')
-    scc_args['env']['scc_vlib']      = os.getenv('SCC_VLIB')
-    scc_args['env']['scc_idir']      = os.getenv('SCC_IDIR')
-    scc_args['env']['scc_libext']    = os.getenv('SCC_LIBEXT')
-    scc_args['env']['scc_cmdfile']   = os.getenv('SCC_CMDFILE')
-    scc_args['env']['scc_sdcfile']   = os.getenv('SCC_SDCFILE')
-    scc_args['env']['scc_flowname']  = os.getenv('SCC_FLOWNAME')
-    scc_args['env']['scc_techfile']  = os.getenv('SCC_TECHFILE')
-    scc_args['env']['scc_layermap']  = os.getenv('SCC_LAYERMAP')
-    scc_args['env']['scc_minlayer']  = os.getenv('SCC_MINLAYER')
-    scc_args['env']['scc_maxlayer']  = os.getenv('SCC_MAXLAYER')
-    scc_args['env']['scc_lib']       = os.getenv('SCC_LIB')
-    scc_args['env']['scc_scenario']  = os.getenv('SCC_SCENARIO')
-    scc_args['env']['scc_effort']    = os.getenv('SCC_EFFORT')
-    
+  
     
     print(scc_args)
     exit
