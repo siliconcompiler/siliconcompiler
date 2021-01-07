2120055cd46e091b5871d39ddcc892ba5df6ba65
Andreas.Olofsson <andreas@adapteva.com>
2021-01-07 12:29:15 -0500

    Directory Reorg

--- siliconcompiler/__init__.py
+++ siliconcompiler/__init__.py
@@ -1,5 +1,5 @@
 from siliconcompiler.floorplan import *
 from siliconcompiler.core import *
-from siliconcompiler.lef import *
-from siliconcompiler.def import *
+from siliconcompiler.leflib import *
+from siliconcompiler.deflib import *
 
