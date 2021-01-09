11908082222e89cca42f35a2084441cccfdb8f5b
WRansohoff <WRansohoff@users.noreply.github.com>
2021-01-09 12:04:17 -0500

    Fix build directory dict value in cli.py
    The `defaults()` method in core.py uses the 'sc_build' key, so I figured that was probably correct.
    
    This, along with your recent addition of the '-constraints' flag, lets me run `sc` on OpenROAD's nangate45 gcd example. It looks like many of the build steps are still placeholders, but it does finish successfully with real inputs.

--- siliconcompiler/cli.py
+++ siliconcompiler/cli.py
@@ -35,7 +35,7 @@ def main():
     chip.lock()
     
     #Printing out run-config
-    chip.writejson(chip.cfg['sc_build_dir']['values'][0] + "/setup.json")
+    chip.writejson(chip.cfg['sc_build']['values'][0] + "/setup.json")
     
     #Compiler
     chip.run("import")
