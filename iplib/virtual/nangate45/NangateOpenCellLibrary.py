a0ef96a88fb9c88facee87d7161102c75a1d2fbe
aolofsson <andreas@zeroasic.com>
2021-01-27 16:08:00 -0500

    Adding setup files for Nangate library

---
+++ iplib/virtual/nangate45/NangateOpenCellLibrary.py
@@ -0,0 +1,34 @@
+####################################################
+# BOILER PLATE
+####################################################
+import os
+import siliconcompiler as sc
+
+mychip = sc.Chip()
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+
+####################################################
+# Setup
+####################################################
+
+library = 'NangateOpenCellLibrary'
+version = 'r1p0'
+ipdir = scriptdir + '/' + library + '/' + version
+
+# timing
+mychip.set('sc_stdlib', library, 'timing', 'typical', ipdir+'lib/NangateOpenCellLibrary_typical.lib')
+
+# lef
+mychip.set('sc_stdlib', library, 'lef', ipdir+'lef/NangateOpenCellLibrary.macro.lef')
+
+# gds
+mychip.set('sc_stdlib', library, 'gds', ipdir+'gds/NangateOpenCellLibrary.gds')
+
+########################################################
+# Write JSON
+########################################################
+
+mychip.abspath()
+
+mychip.writecfg(scriptdir + '/' + library + '.json')
+           
