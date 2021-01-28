8f97ff04d058eead6e08b72bcfcbe1bde505d827
aolofsson <andreas@zeroasic.com>
2021-01-27 19:22:04 -0500

    Fixing directory path bugs

--- iplib/virtual/nangate45/NangateOpenCellLibrary.py
+++ iplib/virtual/nangate45/NangateOpenCellLibrary.py
@@ -6,7 +6,7 @@ import siliconcompiler as sc
 
 mychip = sc.Chip()
 scriptdir = os.path.dirname(os.path.realpath(__file__))
-outputfile = scriptdir + '/' + library + '.json'
+
 
 ####################################################
 # Setup
@@ -15,15 +15,16 @@ outputfile = scriptdir + '/' + library + '.json'
 library = 'NangateOpenCellLibrary'
 version = 'r1p0'
 ipdir = scriptdir + '/' + library + '/' + version
+outputfile = scriptdir + '/' + library + '.json'
 
 # timing
-mychip.set('sc_stdlib', library, 'timing', 'typical', ipdir+'lib/NangateOpenCellLibrary_typical.lib')
+mychip.set('sc_stdlib', library, 'timing', 'typical', ipdir+'/lib/NangateOpenCellLibrary_typical.lib')
 
 # lef
-mychip.set('sc_stdlib', library, 'lef', ipdir+'lef/NangateOpenCellLibrary.macro.lef')
+mychip.set('sc_stdlib', library, 'lef', ipdir+'/lef/NangateOpenCellLibrary.macro.lef')
 
 # gds
-mychip.set('sc_stdlib', library, 'gds', ipdir+'gds/NangateOpenCellLibrary.gds')
+mychip.set('sc_stdlib', library, 'gds', ipdir+'/gds/NangateOpenCellLibrary.gds')
 
 ########################################################
 # Write JSON
