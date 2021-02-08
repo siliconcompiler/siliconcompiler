72077ac0e71806eb561b19686fc537b56d3ce5a1
aolofsson <andreas@zeroasic.com>
2021-02-08 11:10:17 -0500

    Filling out nangate45 definitions -Adding pnrlayer definitions for openroad. Generally not set by commmercial PDKs since this would be derived directly from the techlef -Addding defaults for density, aspect ratio, etc

--- siliconcompiler/foundry/nangate45.py
+++ siliconcompiler/foundry/nangate45.py
@@ -27,17 +27,38 @@ def nangate45_pdk(chip, root):
     chip.add('sc_pdk_process', process)
     chip.add('sc_pdk_version', version)
     chip.add('sc_pdk_stackup', stackup)
-    
-    # PNR tech file
-    chip.add('sc_pdk_pnrtech',stackup, libarch, vendor,
-               pdkdir+'/pnr/nangate45.tech.lef')
-
+   
     # DRC
     chip.add('sc_tool','drc','script',pdkdir+'/drc/FreePDK45.lydrc')
 
     # hard coded target lib
     chip.add('sc_target_stackup',chip.get('sc_pdk_stackup')[0])
-    
+
+    # PNR tech file
+    chip.add('sc_pdk_pnrtech',stackup, libarch, vendor,
+               pdkdir+'/pnr/nangate45.tech.lef')
+
+    # Techlef Overrides
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal1 X 0.095 0.19')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal1 Y 0.07  0.14')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal2 X 0.095 0.19')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal2 Y 0.07  0.14')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal3 X 0.095 0.19')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal3 Y 0.07  0.14')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal4 X 0.095 0.28')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal4 Y 0.07  0.28')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal5 X 0.095 0.28')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal5 Y 0.07  0.28')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal6 X 0.095 0.28')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal6 Y 0.07  0.28')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal7 X 0.095 0.8')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal7 Y 0.07  0.8')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal8 X 0.095 0.8')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal8 Y 0.07  0.8')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal9 X 0.095 1.6')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal9 Y 0.07  1.6')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal10 X 0.095 1.6')
+    chip.add('sc_pdk_pnrlayer',stackup, 'metal10 Y 0.07 1.6')
     
 ####################################################
 # Library Setup
@@ -84,6 +105,11 @@ def nangate45_lib(chip, root):
     chip.add('sc_mcmm_scenario','nominal','pexcorner', corner)
     chip.add('sc_mcmm_scenario','nominal','opcond', corner+" "+libname)
     chip.add('sc_mcmm_scenario','nominal','objectives', objectives)
+
+    #defaukt floor-planning settings
+    chip.add('sc_density', '30')
+    chip.add('sc_coremargin', '1.0')
+    chip.add('sc_aspectratio', '1')
     
 #########################
 if __name__ == "__main__":    
