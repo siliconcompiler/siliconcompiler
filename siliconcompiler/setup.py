9c3e14cae94b8e6e5f31abf74961c66a63d1afbd
aolofsson <andreas@zeroasic.com>
2021-03-01 14:04:26 -0500

    Standardizing interfaces for eda,pdk,lib setup -Removes all the hard coded stuff at the cost of one level of indiretion and riname discipline -Options are resolved at run time based on dict -Setup is done once at launch based on target name -Should work with sclib as well..

--- siliconcompiler/setup.py
+++ siliconcompiler/setup.py
@@ -1,37 +1,109 @@
-#FREEPDK45
-from foundry.virtual.freepdk45.pdk.sc_pdk import setup_freepdk45_pdk
-from foundry.virtual.freepdk45.libs.sc_libs import setup_freepdk45_libs
+import sys
+import importlib
+from siliconcompiler.schema import schema_istrue
+from siliconcompiler.schema import schema_path
 
-#ASAP7
-from foundry.virtual.asap7.pdk.sc_pdk import setup_asap7_pdk
-from foundry.virtual.asap7.libs.sc_libs import setup_asap7_libs
+#############################
+#Initial Setup
+############################
+def find_target(chip):
+    ''' Finds the target setup files based on a single string name.
+    A select set of built targets can be set, otherwise the target
+    is search using the SCPATH environment variable.
+    '''
 
-#SKYWATER130
-from foundry.skywater.skywater130.pdk.sc_pdk import setup_skywater130_pdk
-from foundry.skywater.skywater130.libs.sc_libs import setup_skywater130_libs
+    target = chip.cfg['target']['value'][-1]
 
-#EDA
-from eda.klayout.klayout import setup_klayout
-from eda.yosys.yosys import setup_yosys
-from eda.openroad.openroad import setup_openroad
-from eda.verilator.verilator import setup_verilator
+    if target in ('freepdk45', 'asap7', 'skywater130'):            
+        #Dynamic PDK Module Setup
+        pdkdir = "foundry.virtual." + target + ".pdk"
+        module = importlib.import_module('.sc_pdk', package=pdkdir)
+        setup_pdk = getattr(module,"setup_pdk")
+        setup_pdk(chip)
 
-#Setup Method Called from CLI
-def setup_open(chip, target):
+        #Dynamic LIB Module Setup
+        libsdir = "foundry.virtual." + target + ".libs"
+        module = importlib.import_module('.sc_libs', package=libsdir)
+        setup_libs = getattr(module,"setup_libs")
+        setup_libs(chip)
 
-    #PDK Setup
-    if target == 'freepdk45': 
-        setup_freepdk45_pdk(chip)
-        setup_freepdk45_libs(chip)
-    elif target == 'asap7':
-        setup_asap7_pdk(chip)
-        setup_asap7_libs(chip)
-    elif target == 'skywater130':
-        setup_skywater130_pdk(chip)
-        setup_skywater130_libs(chip)    
+        # EDA Flow is hard coded for builtin
+        for stage in (['import']):
+            setup_stage(chip, stage, "verilator")
 
-    #EDA Setup
-    setup_verilator(chip)
-    setup_yosys(chip)
-    setup_openroad(chip)
-    setup_klayout(chip)
+        for stage in (['syn']):
+            setup_stage(chip, stage, "yosys")
+
+        for stage in (['floorplan', 'place', 'cts', 'route', 'signoff']):
+            if chip.cfg['mode'] == 'fpga' :
+                setup_stage(chip, stage, "vpr")
+            else:
+                setup_stage(chip, stage, "openroad")
+
+        for stage in (['export']):
+            if chip.cfg['mode'] == 'fpga' :
+                setup_stage(chip, stage, "vpr")
+            else:
+                setup_stage(chip, stage, "klayout")
+    else:
+        if os.getenv('SCPATH') == None:
+            chip.logger.error('Environment variable $SCPATH has \
+            not been set, required for closed targets')
+            sys.exit()
+        module = importlib.import_module(target)
+        setup_target = getattr(module,"setup_target")
+        setup_target(chip)
+
+#helper fucntion
+def setup_stage(chip, stage, vendor):
+    edadir = "eda." + vendor
+    module = importlib.import_module('.'+vendor, package=edadir)
+    setup_tool = getattr(module,"setup_tool")
+    setup_tool(chip,stage)
+
+#############################
+# Runtime Options
+############################
+
+def find_options(chip, stage):
+    vendor = chip.cfg['tool'][stage]['vendor']['value'][-1]
+    tool  = chip.cfg['tool'][stage]['exe']['value'][-1]
+    module = importlib.import_module('.'+tool,
+                                     package="eda." + vendor)
+    setup_options = getattr(module,"setup_options")
+    options = setup_options(chip,stage)
+    return options
+
+############################
+# Setup Run Command
+############################
+def setup_cmd(chip,stage):
+
+    #Set Executable
+    exe = chip.cfg['tool'][stage]['exe']['value'][-1] #scalar
+    cmd_fields = [exe]
+
+    #Dynamically generate options
+    options = find_options(chip,stage)
+    cmd_fields.extend(options)        
+
+    #Resolve Paths
+    if schema_istrue(chip.cfg['tool'][stage]['copy']['value']):
+        for value in chip.cfg['tool'][stage]['script']['value']:
+            abspath = schema_path(value)
+            cmd_fields.append(abspath)
+    else:
+        for value in chip.cfg['tool'][stage]['script']['value']:
+            cmd_fields.append(value)      
+
+    #Piping to log file
+    logfile = exe + ".log"
+    if schema_istrue(chip.cfg['quiet']['value']):
+        cmd_fields.append("> " + logfile)
+    else:
+        cmd_fields.append("| tee " + logfile)
+    cmd = ' '.join(cmd_fields)
+
+    return cmd
+
+   
