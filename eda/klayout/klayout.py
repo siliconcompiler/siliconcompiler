3fc6ab078995afcbf1c1162e0936d8b1ddc92491
Will <5217539+WRansohoff@users.noreply.github.com>
2021-03-10 19:11:34 -0500

    Add pre-process step to prepare the klayout stage for GDS generation.

--- eda/klayout/klayout.py
+++ eda/klayout/klayout.py
@@ -1,3 +1,4 @@
+import os
 
 def setup_tool(chip, step):
 
@@ -13,11 +14,44 @@ def setup_tool(chip, step):
           if stage == 'gdsview':               
                chip.add('flow', stage, 'opt', '-nn')
           elif stage == 'export':               
-               chip.add('flow', stage, 'opt', '-rm')
-          
-
+               chip.add('flow', stage, 'opt', '-zz')
 
 def setup_options(chip,stage):
      
      options = chip.get('flow', stage, 'opt')
      return options
+
+################################
+# Pre and Post Run Commands
+################################
+# TODO: Adapt to other PDKs besides freepdk45.
+def pre_process(chip,step):
+    ''' Tool specific function to run before step execution
+    '''
+    foundry_path = '%s/foundry/%s/%s/pdk/%s'%(os.getenv('SCPATH'), chip.cfg['pdk_foundry']['value'][-1], chip.cfg['target']['value'][-1], chip.cfg['pdk_rev']['value'][-1])
+    lefs_path = '%s/foundry/%s/%s/libs/%s/%s/lef'%(os.getenv('SCPATH'), chip.cfg['pdk_foundry']['value'][-1], chip.cfg['target']['value'][-1], chip.cfg['target_lib']['value'][0], chip.cfg['pdk_rev']['value'][-1])
+    tech_file = '%s/setup/klayout/%s.lyt'%(foundry_path, chip.cfg['target']['value'][-1])
+    if step == 'export':
+         chip.add('flow', step, 'opt', '-nn')
+         chip.add('flow', step, 'opt', tech_file)
+         chip.add('flow', step, 'opt', '-rd')
+         chip.add('flow', step, 'opt', 'design_name=%s'%chip.cfg['design']['value'][-1])
+         chip.add('flow', step, 'opt', '-rd')
+         chip.add('flow', step, 'opt', 'in_def=inputs/%s.def'%chip.cfg['design']['value'][-1])
+         chip.add('flow', step, 'opt', '-rd')
+         chip.add('flow', step, 'opt', 'seal_gds=""')
+         chip.add('flow', step, 'opt', '-rd')
+         chip.add('flow', step, 'opt', 'in_gds=%s/%s'%(os.getenv('SCPATH'), chip.cfg['stdcell'][chip.cfg['target_lib']['value'][0]]['gds']['value'][-1]))
+         chip.add('flow', step, 'opt', '-rd')
+         chip.add('flow', step, 'opt', 'out_gds=outputs/%s.gds'%chip.cfg['design']['value'][-1])
+         chip.add('flow', step, 'opt', '-rd')
+         chip.add('flow', step, 'opt', 'tech_file=%s'%tech_file)
+         chip.add('flow', step, 'opt', '-rd')
+         chip.add('flow', step, 'opt', 'foundry_lefs=%s'%lefs_path)
+         chip.add('flow', step, 'opt', '-rm')
+         chip.add('flow', step, 'opt', 'def2gds.py')
+
+def post_process(chip,step):
+    ''' Tool specific function to run after step execution
+    '''
+    pass
