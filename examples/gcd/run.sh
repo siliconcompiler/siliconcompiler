6ef42253e9ab33922a3572ff29c1a569d710c7c3
aolofsson <andreas@zeroasic.com>
2021-03-29 18:00:50 -0400

    Updating gcd example for new schema

--- examples/gcd/run.sh
+++ examples/gcd/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 sc examples/gcd/gcd.v \
+   -pdk_rev "1.0" \
    -target "freepdk45" \
-   -diesize "0 0 100.13 100.8" \
-   -coresize "10.07 11.2 90.25 91" \
-   -stop "place" \
-   -design gcd -quiet
+   -asic_diesize "0 0 100.13 100.8" \
+   -asic_coresize "10.07 11.2 90.25 91" \
+   -stop "route" \
+   -design gcd \
+   -quiet
