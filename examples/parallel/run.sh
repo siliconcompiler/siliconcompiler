794e6eda61b8dae1648c2ffc6208b37df4967758
aolofsson <andreas@zeroasic.com>
2021-04-05 22:50:29 -0400

    Example cleanup

--- examples/parallel/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sc examples/gcd/gcd.v \
-   -pdk_rev "1.0" \
-   -target "freepdk45" \
-   -asic_diesize "0 0 100.13 100.8" \
-   -asic_coresize "10.07 11.2 90.25 91" \
-   -loglevel "INFO" \
-   -design gcd \
-   -quiet
