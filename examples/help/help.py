9de13d70c44d71a633c3f6f10f0de35d989d4f32
aolofsson <andreas@zeroasic.com>
2021-04-22 08:40:33 -0400

    Adding reftable print example

--- examples/help/help.py
+++ examples/help/help.py
@@ -7,13 +7,23 @@ import siliconcompiler
 #Create one (or many...) instances of Chip class
 chip = siliconcompiler.Chip(loglevel="INFO")
 
-# Reading in default config files unless cfg file is set
-chip.help('design')
-
-# Print all help
+# Print full help
 allkeys = chip.getkeys()
-for key in allkeys:
-    chip.help(*key)
+with open("docs/sc_refmanual.md", 'w') as f:
+    for key in allkeys:
+        chip.help(*key, file=f)
 
-
-
+# Print reference card
+with open("docs/sc_reftable.md", 'w') as f:
+    outlist = ['param', 'desription', 'type', 'required', 'default', 'value']
+    outstr = " | {: <52} | {: <30} | {: <15} | {: <10} | {: <10}|".format(*outlist)
+    print(outstr, file=f)
+    outlist = [':----',
+               ':----',
+               ':----',
+               ':----',
+               ':----']
+    outstr = " | {: <52} | {: <30} | {: <15} | {: <10} | {: <10}|".format(*outlist)
+    print(outstr, file=f)
+    for key in allkeys:
+        chip.help(*key, file=f, mode='refcard')
