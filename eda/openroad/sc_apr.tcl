ffa12eb686209b61c52fedcc48c18f7c632c7fec
aolofsson <andreas@zeroasic.com>
2021-04-22 15:01:34 -0400

    Flattening sc_apr file -No need for adapter, one "main" file should be enough for flow -Then branch off to per step files as needed (2 layers!)

--- eda/openroad/sc_apr.tcl
+++ eda/openroad/sc_apr.tcl
@@ -1,47 +1,108 @@
 ###############################
-# Python to TCL Interface
+# Reading Schema
 ###############################
 
-#Dictionary Configuration
 source ./sc_schema.tcl
 
-#Converting dictionary to scalars
-source ./sc_adapter.tcl
+###############################
+# Schema Adaptet
+###############################
 
-###############################
-# Openroad Setup
-###############################
-#1. Read lef/def/sdc
-#2. Misc common settings
+#Handling remote/local script execution 
+set sc_step   [dict get $sc_cfg status step]
 
-source ./sc_setup.tcl
-
-###############################
-# Select Execution Step
-###############################
-set verbose "-echo -verbose"
-switch $sc_step {
-    "floorplan" {
-	source $verbose "./sc_floorplan.tcl"
-    }
-    "place" {
-	source $verbose "./sc_place.tcl"
-    }
-    "cts" {
-	source $verbose "./sc_cts.tcl"
-    }
-    "route" {
-	source $verbose "./sc_route.tcl"
-    }
+if {[dict get $sc_cfg flow $sc_step copy] eq True} {
+    set sc_refdir "."
+} else {
+    set sc_refdir [dict get $sc_cfg flow $sc_step refdir]
 }
+    
+set sc_step        [dict get $sc_cfg status step]
+set sc_design      [dict get $sc_cfg design]
+set sc_optmode     [dict get $sc_cfg optmode]
+
+set sc_stackup     [dict get $sc_cfg asic stackup]
+set sc_targetlib   [dict get $sc_cfg asic targetlib]
+set sc_diesize     [dict get $sc_cfg asic diesize]
+set sc_coresize    [dict get $sc_cfg asic coresize]
+set sc_minlayer    [dict get $sc_cfg asic minlayer]
+set sc_maxlayer    [dict get $sc_cfg asic maxlayer]
+
+set sc_mainlib     [lindex $sc_targetlib 0]
+set sc_libarch     [dict get $sc_cfg stdcell $sc_mainlib libtype]
+set sc_site        [dict get $sc_cfg stdcell $sc_mainlib site]
+
+set sc_minmetal    [dict get $sc_cfg pdk aprlayer $sc_stackup $sc_minlayer name]
+set sc_maxmetal    [dict get $sc_cfg pdk aprlayer $sc_stackup $sc_maxlayer name]
+set sc_techlef     [dict get $sc_cfg pdk aprtech $sc_stackup $sc_libarch openroad]
+set sc_filler      [dict get $sc_cfg pdk aprtech $sc_stackup $sc_libarch openroad]
+set sc_tapmax      [dict get $sc_cfg pdk tapmax]
+set sc_tapoffset   [dict get $sc_cfg pdk tapoffset]
+
+if {[dict exists $sc_cfg macrolib]} {    
+    set sc_macrolib    [dict get $sc_cfg asic macrolib]
+} else {
+    set sc_macrolib    ""
+}
+
+if {[dict exists $sc_cfg constraint]} {    
+    set sc_constraint  [dict get $sc_cfg constraint]
+} else {
+    set sc_constraint  ""
+}
+if {[dict exists $sc_cfg def]} {    
+    set sc_def  [dict get $sc_cfg def]
+} else {
+    set sc_def  ""
+}
+
+###############################
+# Common Setup
+###############################
+
+# Techlef
+read_lef  $sc_techlef
+
+# Stdcells
+foreach lib $sc_targetlib {
+    read_liberty [dict get $sc_cfg stdcell $lib model typical nldm lib]
+    read_lef [dict get $sc_cfg stdcell $lib lef]
+    set site [dict get $sc_cfg stdcell $lib site]
+}
+
+# Macrolibs
+foreach lib $sc_macrolib {
+    read_liberty [dict get $sc_cfg macro $lib model typical nldm lib]
+    read_lef [dict get $sc_cfg macro $lib lef]
+}
+
+# Floorplan reads synthesis verilog, others read def
+if {$sc_step == "floorplan"} {
+    read_verilog "inputs/$sc_design.v"
+    link_design $sc_design
+    foreach sdc $sc_constraint {
+	read_sdc $sdc
+    }
+} else {    
+    read_def "inputs/$sc_design.def"
+    read_sdc "inputs/$sc_design.sdc"
+}
+
+###############################
+# Source Step Script
+###############################
+
+source -echo -verbose "$sc_refdir/sc_$sc_step.tcl"
 
 ###############################
 # Reporting
 ###############################
-source ./sc_report.tcl
+
+source -echo -verbose "$sc_refdir/sc_report.tcl"
 
 ###############################
 # Write Design Data
 ###############################
-source ./sc_write.tcl
+
+source -echo -verbose "$sc_refdir/sc_write.tcl"
 
