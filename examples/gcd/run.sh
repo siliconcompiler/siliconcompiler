e4851c82bb150ecc629c4d10b4f454f04a6e475f
aolofsson <andreas@zeroasic.com>
2021-04-23 10:48:35 -0400

    Addding constraints to run file

--- examples/gcd/run.sh
+++ examples/gcd/run.sh
@@ -2,10 +2,9 @@
 
 OPT=$1
 sc examples/gcd/gcd.v \
-   -pdk_rev "1.0" \
    -target "freepdk45" \
+   -constraint "examples/gcd/constraint.sdc" \
    -asic_diesize "0 0 100.13 100.8" \
    -asic_coresize "10.07 11.2 90.25 91" \
    -loglevel "INFO" \
-   -design gcd \
-   -quiet $OPT
+   -design gcd  $OPT
