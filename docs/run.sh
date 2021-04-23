bc5a3ad03f35cc47beb5b96682ce9bb24be389f9
aolofsson <andreas@zeroasic.com>
2021-04-23 10:49:24 -0400

    Adding temp runu file for refcard and help manual -Merge with sphinx eventually

--- docs/run.sh
+++ docs/run.sh
@@ -1,1 +1,11 @@
-pandoc docs/sc_refcard.md -V geometry:landscape --columns 1000 --pdf-engine=xelatex -o docs/sc_refcard.pdf
+
+# PErequisites
+# sudo apt-get install pandoc
+# sudo apt-get install texlive
+# sudo apt-get install texlive-fonts-recommended texlive-fonts-extra
+
+pandoc sc_reftable.md -V geometry:landscape --columns 1000 --pdf-engine=xelatex -o sc_reftable.pdf
+
+pandoc sc_refmanual.md --pdf-engine=xelatex -o sc_refmanual.pdf
+
+
