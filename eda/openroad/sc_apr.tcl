10a75f6579d814b7363f8cd3db6cc1a56da7d2b7
aolofsson <andreas@zeroasic.com>
2021-04-23 10:36:44 -0400

    Feactoruing to move common code to sc_apr from individual steps -Can't wait to get rid of the triton fixups... -Addding layer to metal conversion -Moving openroad magic variables to the front, not that many... -Removing clock query, handled inside cts

--- eda/openroad/sc_apr.tcl
+++ eda/openroad/sc_apr.tcl
@@ -7,7 +7,12 @@ source ./sc_schema.tcl
 ###############################
 # Openroad Constants
 ###############################
+
 set openroad_place_density 0.3
+set openroad_overflow_iter 100
+set openroad_pad_global_place 2
+set openroad_cluster_diameter 100
+set openroad_cluster_size 30
 
 ###############################
 # Schema Adapter
@@ -26,8 +31,6 @@ if {[dict get $sc_cfg flow $sc_step copy] eq True} {
 set sc_design     [lindex [dict get $sc_cfg design] end]
 set sc_optmode     [lindex [dict get $sc_cfg optmode] end]
 
-
-
 ###############################
 # Required
 ###############################
@@ -43,6 +46,11 @@ set sc_hpinlayer   [lindex [dict get $sc_cfg asic hpinlayer] end]
 set sc_vpinlayer   [lindex [dict get $sc_cfg asic vpinlayer] end]
 set sc_hpinmetal   [dict get $sc_cfg pdk aprlayer $sc_stackup $sc_hpinlayer name]
 set sc_vpinmetal   [dict get $sc_cfg pdk aprlayer $sc_stackup $sc_vpinlayer name]
+set sc_rclayer     [lindex [dict get $sc_cfg asic rclayer] end]
+set sc_clklayer    [lindex [dict get $sc_cfg asic clklayer] end]
+set sc_rcmetal     [dict get $sc_cfg pdk aprlayer $sc_stackup $sc_rclayer name]
+set sc_clkmetal    [dict get $sc_cfg pdk aprlayer $sc_stackup $sc_clklayer name]
+
 set sc_aspectratio [lindex [dict get $sc_cfg asic aspectratio] end]
 set sc_minlayer    [lindex [dict get $sc_cfg asic minlayer] end]
 set sc_maxlayer    [lindex [dict get $sc_cfg asic maxlayer] end]
@@ -77,55 +85,51 @@ set sc_tapoffset   [lindex [dict get $sc_cfg pdk tapoffset] end]
 
 # MACROS
 if {[dict exists $sc_cfg macrolib]} {    
-    set sc_macrolibs    [dict get $sc_cfg asic macrolib]
+    set sc_macrolibs [dict get $sc_cfg asic macrolib]
 } else {
     set sc_macrolibs    ""
 }
 
 # CONSTRAINTS
 if {[dict exists $sc_cfg constraint]} {    
-    set sc_constraint  [dict get $sc_cfg constraint]
+    set sc_constraint [dict get $sc_cfg constraint]
 } else {
     set sc_constraint  ""
 }
 
 # DEF
 if {[dict exists $sc_cfg def]} {    
-    set sc_def  [dict get $sc_cfg def]
+    set sc_def [dict get $sc_cfg def]
 } else {
     set sc_def  ""
 }
 
-# CLOCKS
-if {[dict exists $sc_cfg clock]} { 
-    set sc_clocks ""
-    dict for {key value} [dict get $sc_cfg clock] {
-	lappend sc_clocks $key
-    }
-    set sc_clocknets ""
-    foreach clock $sc_clocks {
-	lappend sc_clknets  [dict get $sc_cfg clock name]
-    }
-}
-
 ###############################
-# Common Setup
+# Read Files
 ###############################
 
-# Techlef
-read_lef  $sc_techlef
-
-# Stdcells
-foreach lib $sc_targetlibs {
-    read_liberty [dict get $sc_cfg stdcell $lib model typical nldm lib]
-    read_lef [dict get $sc_cfg stdcell $lib lef]
-    set site [dict get $sc_cfg stdcell $lib site]
+# Triton Hack b/c it can't handle multiple LEFs!!
+# TODO: Fix as soon as triton is merged
+if {$sc_step == "route"} {    
+    exec "$sc_refdir/mergeLef.py" --inputLef \
+	$sc_techlef \
+	[dict get $sc_cfg stdcell $sc_mainlib lef] \
+	--outputLef "triton_merged.lef"
+    read_lef "triton_merged.lef"
+} else {
+    #Techlef
+    read_lef  $sc_techlef
+    # Stdcells
+    foreach lib $sc_targetlibs {
+	read_liberty [dict get $sc_cfg stdcell $lib model typical nldm lib]
+	read_lef [dict get $sc_cfg stdcell $lib lef]
+    }
 }
-
+ 
 # Macrolibs
 foreach lib $sc_macrolibs {
     read_liberty [dict get $sc_cfg macro $lib model typical nldm lib]
-    read_lef [dict get $sc_cfg macro $lib lef]
+    read_lef [dict get $sc_cfg macro $lib lef]    
 }
 
 # Floorplan reads synthesis verilog, others read def
@@ -139,6 +143,19 @@ if {$sc_step == "floorplan"} {
     read_def "inputs/$sc_design.def"
     read_sdc "inputs/$sc_design.sdc"
 }
+
+###############################
+# Common Setup
+###############################
+
+set_dont_use $sc_dontuse
+
+set_wire_rc -clock  -layer $sc_clkmetal
+set_wire_rc -signal -layer $sc_rcmetal
+
+set_placement_padding -global \
+    -left $openroad_pad_global_place \
+    -right $openroad_pad_global_place
 
 ###############################
 # Source Step Script
