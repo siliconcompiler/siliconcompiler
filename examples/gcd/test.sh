f2416fe3496c1e24fe68365439b8f05c16f54b78
WRansohoff <5217539+WRansohoff@users.noreply.github.com>
2021-04-24 17:01:21 -0400

    Fix CI tests to work with the lack of jobids and openroad refactoring.

--- examples/gcd/test.sh
+++ examples/gcd/test.sh
@@ -3,7 +3,8 @@ sc ./examples/gcd/gcd.v \
   -design gcd \
   -target freepdk45 \
   -asic_diesize "0 0 100.13 100.8" \
-  -asic_coresize "10.07 11.2 90.25 91"
+  -asic_coresize "10.07 11.2 90.25 91" \
+  -constraint examples/gcd/constraint.sdc
 
 if test -f "./build/export/job1/outputs/gcd.gds"; then
   echo "Success!"
