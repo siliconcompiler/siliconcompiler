15e08337c0dab953a5c6931e4156ad4bf44ecea1
WRansohoff <5217539+WRansohoff@users.noreply.github.com>
2021-05-07 14:47:26 -0400

    Replace unified 'test.py' script with individual pytest-compatible files.

---
+++ tests/asic/test_gcd.py
@@ -0,0 +1,27 @@
+import os
+import pathlib
+import subprocess
+from test_fixture import test_wrapper
+
+##################################
+def test_gcd_local():
+    '''Basic CLI test: build the GCD example by running `sc` as a command-line app.
+    '''
+
+    # Use subprocess to test running the `sc` scripts as a command-line program.
+    # Pipe stdout to /dev/null to avoid printing to the terminal.
+    gcd_ex_dir = os.path.abspath(str(pathlib.Path(__file__).parent.parent.parent)) + '/examples/gcd/'
+
+    # Run the build command.
+    subprocess.run(['sc',
+                    gcd_ex_dir + '/gcd.v',
+                    '-design', 'gcd',
+                    '-target', 'freepdk45',
+                    '-asic_diesize', '0 0 100.13 100.8',
+                    '-asic_coresize', '10.07 11.2 90.25 91',
+                    '-constraint', gcd_ex_dir + '/constraint.sdc',
+                    '-loglevel', 'NOTSET'],
+                   stdout = subprocess.DEVNULL)
+
+    # Verify that a GDS file was generated.
+    assert os.path.isfile('build/gcd/job1/export/outputs/gcd.gds')
---
+++ tests/asic/test_gcd_py.py
@@ -0,0 +1,36 @@
+import os
+import pathlib
+import subprocess
+from test_fixture import test_wrapper
+
+##################################
+def test_gcd_local_py():
+    '''Basic Python API test: build the GCD example using only Python code.
+    '''
+
+    # Create instance of Chip class
+    chip = siliconcompiler.Chip(loglevel='NOTSET')
+
+    # Inserting value into configuration
+    chip.add('source', 'examples/gcd/gcd.v')
+    chip.add('design', 'gcd')
+    chip.add('clock', 'clock_name', 'pin', 'clk')
+    chip.add('constraint', "examples/gcd/constraint.sdc")
+    chip.set('target', "freepdk45")
+    chip.set('asic', 'diesize', "0 0 100.13 100.8")
+    chip.set('asic', 'coresize', "10.07 11.2 90.25 91")
+    chip.set_jobid()
+
+    chip.target()
+
+    chip.set('stop', 'export')
+    chip.set('quiet', 'true')
+
+    # Run the chip's build process synchronously.
+    chip.run()
+
+    # (Printing the summary makes it harder to see other test case results.)
+    #chip.summary()
+
+    # Verify that a GDS file was generated.
+    assert os.path.isfile('build/gcd/job1/export/outputs/gcd.gds')
