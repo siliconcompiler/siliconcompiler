5c3f819a32e4e26a873de2add0ce53a9c3676abc
aolofsson <andreas@zeroasic.com>
2021-05-21 14:50:36 -0400

    File cleanup, methodology for constarints is "design.sdc"

--- examples/gcd/run.sh
+++ examples/gcd/run.sh
@@ -3,7 +3,7 @@
 OPT=$1
 sc examples/gcd/gcd.v \
    -target "freepdk45" \
-   -constraint "examples/gcd/constraint.sdc" \
+   -constraint "examples/gcd/gcd.sdc" \
    -asic_diesize "0 0 100.13 100.8" \
    -asic_coresize "10.07 11.2 90.25 91" \
    -loglevel "INFO" \
