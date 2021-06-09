fa93f6398932c0aefac02c3cfc32be340ff049e0
Noah Moroze <noah@zeroasic.com>
2021-06-09 11:54:30 -0400

    Remove unused variable

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ import shutil
 @pytest.fixture(autouse=True, scope='session')
 def test_session_wrapper():
     # Create and enter a temporary build directory.
-    topdir = os.getcwd()
     if os.path.isdir('pytest_tmp'):
         shutil.rmtree('pytest_tmp')
     os.mkdir('pytest_tmp')
