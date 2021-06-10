9c128c628c22c7b665bb51fc4ee9b5d2b2a66160
Zachary Yedidia <zyedidia@gmail.com>
2021-06-09 21:33:19 -0400

    Update fpga target

--- eda/targets/fpga.py
+++ eda/targets/fpga.py
@@ -18,7 +18,10 @@ def setup_eda(chip, name=None):
 
         for step in chip.cfg['steplist']['value']:         
             if step == 'import':
-                vendor = 'verilator'
+                if chip.get('ir')[-1] == 'uhdm':
+                    vendor = 'surelog'
+                elif chip.get('ir')[-1] == 'verilog':
+                    vendor = 'verilator'
             elif step == 'syn':
                 vendor = 'yosys'
             elif step == 'apr':
