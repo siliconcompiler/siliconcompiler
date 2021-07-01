8964f341dbd3722cbd55157114e4096978f1cb23
Noah Moroze <noah@zeroasic.com>
2021-07-01 14:08:00 -0400

    Add Netgen LVS

---
+++ eda/magic/lvs.tcl
@@ -0,0 +1,43 @@
+source ./sc_schema.tcl
+source ./pdkpath.tcl
+
+set sc_design  [lindex [dict get $sc_cfg design] end]
+set sc_mainlib [lindex [dict get $sc_cfg asic targetlib] 0]
+set sc_stackup [lindex [dict get $sc_cfg asic stackup] end]
+set sc_libtype [lindex [dict get $sc_cfg stdcell $sc_mainlib libtype] end]
+set sc_techlef [dict get $sc_cfg pdk aprtech $sc_stackup $sc_libtype lef]
+set sc_liblef  [dict get $sc_cfg stdcell $sc_mainlib lef]
+
+lef read $sc_techlef
+lef read $sc_liblef
+
+# TODO: may have to read additional LEFs (such as macros), but not sure
+
+# Read DEF and load design
+def read "inputs/${sc_design}.def"
+
+load $sc_design -dereference;
+select top cell;
+
+# Abstract every cell under top level
+foreach cell [cellname list children] {
+    load $cell -dereference;
+    property LEFview TRUE;
+};
+
+# Extract layout to Spice netlist
+load $sc_design -dereference
+select top cell;
+extract no all;
+extract do local;
+extract unique;
+extract;
+ext2spice lvs;
+ext2spice ${sc_design}.ext;
+feedback save extract_${sc_design}.log;
+
+# Run Netgen
+set setup_file ${PDKPATH}/netgen/lvs_setup.tcl
+::netgen -batch lvs "${sc_design}.spice ${sc_design}" "inputs/${sc_design}.v ${sc_design}" $setup_file outputs/${sc_design}.lvs.out
+
+exit
--- eda/magic/magic_setup.py
+++ eda/magic/magic_setup.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 
 ################################
 # Tool Setup
@@ -15,14 +16,20 @@ def setup_tool(chip, step):
     chip.add('flow', step, 'vendor', 'magic')
     chip.add('flow', step, 'exe', 'magic')
     chip.add('flow', step, 'refdir', refdir)
-    chip.add('flow', step, 'script', refdir + '/gds_drc.tcl')
+
+    if step == 'drc':
+        script = 'gds_drc.tcl'
+    elif step == 'lvs':
+        script = 'lvs.tcl'
+
+    chip.add('flow', step, 'script', refdir + '/' + script)
 
 def setup_options(chip,step):
     options = chip.get('flow', step, 'option')
 
     target_tech = chip.cfg['target']['value'][-1].split('_')[0]
 
-    magicrc = '%s/%s.magicrc'%(
+    magicrc = '%s/magic/%s.magicrc'%(
         pdk_path(chip),
         target_tech)
 
@@ -52,12 +59,17 @@ def post_process(chip, step):
     '''
     design = chip.get('design')[-1]
 
-    with open(f'outputs/{design}.drc', 'r') as f:
-        for line in f:
-            errors = re.search('^\[INFO\]: COUNT: (\d+)', line)
+    if step == 'drc':
+        with open(f'outputs/{design}.drc', 'r') as f:
+            for line in f:
+                errors = re.search(r'^\[INFO\]: COUNT: (\d+)', line)
 
-            if errors:
-                chip.set('real', step, 'errors', errors.group(1))
+                if errors:
+                    chip.set('real', step, 'errors', errors.group(1))
+    elif step == 'lvs':
+        # Need to pass along DEF to export stage
+        shutil.copy(f'inputs/{design}.def', f'outputs/{design}.def')
+
 
 ################################
 # Utilities
@@ -76,7 +88,7 @@ def pdk_path(chip):
 
     target_tech = chip.cfg['target']['value'][-1].split('_')[0]
 
-    return f'%s/%s/%s/pdk/{pdk_rev}/setup/magic'%(
+    return f'%s/%s/%s/pdk/{pdk_rev}/setup/'%(
         sc_path,
         chip.cfg['pdk']['foundry']['value'][-1],
         target_tech)
