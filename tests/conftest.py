dcc90272dfce6798bcdf032fac5c9182f3125ba0
Will <5217539+WRansohoff@users.noreply.github.com>
2021-07-02 14:29:12 -0400

    Use try/except when creating the temporary test directory, to avoid a race condition when run in parallel.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ import shutil
 @pytest.fixture(autouse=True, scope='session')
 def test_session_wrapper():
     # Create and enter a temporary build directory.
-    if not os.path.isdir('pytest_tmp'):
+    try:
         os.mkdir('pytest_tmp')
+    except FileExistsError:
+        pass
     os.chdir('pytest_tmp')
