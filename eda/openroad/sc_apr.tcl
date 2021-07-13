fe3b62204e0887f25169cc2a3d836df38d26337a
Will <5217539+WRansohoff@users.noreply.github.com>
2021-07-13 11:20:06 -0400

    Add padding to the detailed placement step, like the official OpenROAD flow does.

--- eda/openroad/sc_apr.tcl
+++ eda/openroad/sc_apr.tcl
@@ -23,22 +23,26 @@ set target_tech [lindex $targetlist 0]
 if {$target_tech eq "freepdk45"} {
     set openroad_place_density 0.3
     set openroad_pad_global_place 2
+    set openroad_pad_detail_place 1
     set openroad_macro_place_halo "22.4 15.12"
     set openroad_macro_place_channel "18.8 19.95"
 } elseif {$target_tech eq "asap7"} {
     set openroad_place_density 0.77
     set openroad_pad_global_place 2
+    set openroad_pad_detail_place 1
     set openroad_macro_place_halo "22.4 15.12"
     set openroad_macro_place_channel "18.8 19.95"
 } elseif {$target_tech eq "skywater130"} {
     set openroad_place_density 0.6
     set openroad_pad_global_place 4
+    set openroad_pad_detail_place 2
     set openroad_macro_place_halo "1 1"
     set openroad_macro_place_channel "80 80"
 } else {
     puts "WARNING: OpenROAD tuning constants not set for $target_tech in sc_apr.tcl, using generic values."
     set openroad_place_density 0.3
     set openroad_pad_global_place 2
+    set openroad_pad_detail_place 1
     set openroad_macro_place_halo "22.4 15.12"
     set openroad_macro_place_channel "18.8 19.95"
 }
