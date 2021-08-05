a55315857d831d151c15b6287555efc0c99e221b
Andreas.Olofsson <andreas.d.olofsson@gmail.com>
2021-08-05 16:55:14 -0400

    Restoring nextpnr and icepack based on nmoroze work -Was deleted as we moved to fusesoc, but we are now moving back to native to reduce the stack height and dependancies.

--- siliconcompiler/tools/icepack/icepack_setup.py
+++ siliconcompiler/tools/icepack/icepack_setup.py
@@ -1,45 +1,51 @@
+import os
+import importlib
+import re
+import sys
+import siliconcompiler
+from siliconcompiler.floorplan import *
+from siliconcompiler.schema import schema_path
+
 ################################
-# Setup icepack
+# Setup Tool (pre executable)
 ################################
 
 def setup_tool(chip, step):
     ''' Sets up default settings on a per step basis
     '''
-
-    chip.add('flow', step, 'threads', '4')
-    chip.add('flow', step, 'format', 'cmdline')
-    chip.add('flow', step, 'copy', 'false')
-    chip.add('flow', step, 'vendor', 'icepack')
-    chip.add('flow', step, 'exe', 'icepack')
-    chip.add('flow', step, 'refdir', '')
-    chip.add('flow', step, 'option', '')
-
-################################
-# Set icepack Runtime Options
-################################
-
-def setup_options(chip, step):
-    ''' Per step function that returns a dynamic options string based on
-    the dictionary settings.
-    '''
+    tool = 'icepack'
+    chip.set('eda', tool, step, 'vendor', tool)
+    chip.set('eda', tool, step, 'exe', tool)
+    chip.add('eda', tool, step, 'format', 'cmdline')
+    chip.set('eda', tool, step, 'threads', '4')
+    chip.set('eda', tool, step, 'copy', 'false')
 
     #Get default opptions from setup
-    options = chip.get('flow', step, 'option')
-
     topmodule = chip.get('design')[-1]
 
+    options = []
     options.append("inputs/" + topmodule + ".asc")
     options.append("outputs/" + topmodule + ".bit")
+    chip.add('eda', tool, step, 'option', options)
 
-    return options
-
-def pre_process(chip, step):
-    ''' Tool specific function to run before step execution
-    '''
-    pass
-
-def post_process(chip, step, status):
+################################
+# Post_process (post executable)
+################################
+def post_process(chip, step):
     ''' Tool specific function to run after step execution
     '''
-    #TODO: return error code
-    return status
+    return 0
+
+##################################################
+if __name__ == "__main__":
+
+    # File being executed
+    prefix = os.path.splitext(os.path.basename(__file__))[0]
+    output = prefix + '.json'
+
+    # create a chip instance
+    chip = siliconcompiler.Chip(defaults=False)
+    # load configuration
+    setup_tool(chip, step='bitstream')
+    # write out results
+    chip.writecfg(output)
