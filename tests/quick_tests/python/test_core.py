5f0f70210194b1a7b465c2691cafe3173de6e36a
Noah Moroze <noah@zeroasic.com>
2021-08-09 16:08:23 -0400

    Fix test_core

--- tests/quick_tests/python/test_core.py
+++ tests/quick_tests/python/test_core.py
@@ -7,7 +7,7 @@ class TestCore(unittest.TestCase):
     def test_set_get(self):
         chip = Chip()
         chip.set('target', 'freepdk45_asicflow')
-        self.assertEqual(chip.get('target'), ['freepdk45_asicflow'])
+        self.assertEqual(chip.get('target'), 'freepdk45_asicflow')
 
     def test_cli_multi_source(self):
         ''' Regression test for bug where CLI parser wasn't handling multiple
@@ -15,9 +15,11 @@ class TestCore(unittest.TestCase):
         '''
         chip = Chip()
 
-        args = ['sc', 'foo.v', 'bar.v', '-target', 'freepdk45_asicflow']
+        args = ['sc', 'examples/ibex/ibex_alu.v', 'examples/ibex/ibex_branch_predict.v', 
+                '-target', 'freepdk45_asicflow']
         with unittest.mock.patch('sys.argv', args):
             chip.cmdline()
 
-        assert chip.get('source') == ['foo.v', 'bar.v']
-        assert chip.get('target')[-1] == 'freepdk45_asicflow'
+        assert chip.get('source') == ['examples/ibex/ibex_alu.v', 
+                                      'examples/ibex/ibex_branch_predict.v']
+        assert chip.get('target') == 'freepdk45_asicflow'
