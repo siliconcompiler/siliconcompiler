becec16b0fe9b958e58e63a378d9e23d327b12c0
aolofsson <andreas@zeroasic.com>
2021-08-12 10:36:48 -0400

    Making loglevel default INFO -Default setting should be the one that expresses functionality/flow -User can elect to set no visibilty (ie ERROR) or lots of info (DEBUG).

--- siliconcompiler/core.py
+++ siliconcompiler/core.py
@@ -35,7 +35,7 @@ class Chip:
     """Siliconcompiler Compiler Chip Object Class"""
 
     ###########################################################################
-    def __init__(self, loglevel="DEBUG", defaults=True):
+    def __init__(self, loglevel="INFO", defaults=True):
         '''Initializes Chip object
 
         Args:
