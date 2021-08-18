a80f2d6fb363aa6de7cc7e4861f3cf5ae3c3487f
Noah Moroze <noah@zeroasic.com>
2021-08-18 19:54:42 +0000

    Include power pins in Verilog netlist
    Necessary to check power connections with LVS

--- siliconcompiler/tools/openroad/sc_write.tcl
+++ siliconcompiler/tools/openroad/sc_write.tcl
@@ -1,5 +1,5 @@
 write_def "outputs/$sc_design.def"
 write_sdc "outputs/$sc_design.sdc"
-write_verilog "outputs/$sc_design.v"
+write_verilog -include_pwr_gnd "outputs/$sc_design.v"
 write_db "outputs/$sc_design.odb"
 
