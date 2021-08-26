a8ddd8a1352828268d4eccdfbb899e9eb1630825
Noah Moroze <noah@zeroasic.com>
2021-08-25 23:33:54 -0400

    Update create_die_area API to be more future-proof
    We might want to support non-rectangular die/core areas in the future.

--- tests/daily_tests/asic/test_gcd_floorplan/gcd_floorplan.py
+++ tests/daily_tests/asic/test_gcd_floorplan/gcd_floorplan.py
@@ -1,7 +1,7 @@
 def setup_floorplan(fp):
     cell_h = fp.std_cell_height
-    fp.create_die_area(72 * cell_h, 72 * cell_h, core_area=(8 * cell_h, 8 * cell_h, 64 * cell_h, 64 * cell_h))
-    die_w, die_h = fp.die_area
+    fp.create_die_area([(0, 0), (72 * cell_h, 72 * cell_h)], core_area=[(8 * cell_h, 8 * cell_h), (64 * cell_h, 64 * cell_h)])
+    die_w, die_h = fp.die_area[1]
 
     in_pins = ['clk']
     in_pins += [f'req_msg[{i}]' for i in range(32)]
