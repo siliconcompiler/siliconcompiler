6bf711220f9c87ebd3cfb7cef59a00a70e2bc577
Noah Moroze <noah@zeroasic.com>
2021-09-08 12:46:16 -0400

    Store version!
    This lays most of the groundwork, now it's off to the races!

--- siliconcompiler/leflib/_leflib.pyx
+++ siliconcompiler/leflib/_leflib.pyx
@@ -1,19 +1,104 @@
-# from libc.stdio cimport FILE, fopen, fclose
+# These imports let us use libc file I/O to communicate with C++ lef library
+from libc.stdio cimport FILE, fopen, fclose
 
-# cdef extern from "lefrReader.hpp":
-#     int lefrInit()
+ctypedef int (*lefrDoubleCbkFnType)(lefrCallbackType_e typ, double number, void* data)
 
+# Must "forward declare" everything we reference from lefrReader.hpp here
+cdef extern from "lefrReader.hpp":
+    int lefrInit()
+    int lefrRead()
+    int lefrRead (FILE *file, const char *fileName, void* userData)
+    void lefrSetVersionCbk(lefrDoubleCbkFnType cbk)
+    ctypedef enum lefrCallbackType_e:
+       lefrUnspecifiedCbkType,
+       lefrVersionCbkType,
+       lefrVersionStrCbkType,
+       lefrDividerCharCbkType,
+       lefrBusBitCharsCbkType,
+       lefrUnitsCbkType,
+       lefrCaseSensitiveCbkType,
+       lefrNoWireExtensionCbkType,
+       lefrPropBeginCbkType,
+       lefrPropCbkType,
+       lefrPropEndCbkType,
+       lefrLayerCbkType,
+       lefrViaCbkType,
+       lefrViaRuleCbkType,
+       lefrSpacingCbkType,
+       lefrIRDropCbkType,
+       lefrDielectricCbkType,
+       lefrMinFeatureCbkType,
+       lefrNonDefaultCbkType,
+       lefrSiteCbkType,
+       lefrMacroBeginCbkType,
+       lefrPinCbkType,
+       lefrMacroCbkType,
+       lefrObstructionCbkType,
+       lefrArrayCbkType,
+       lefrSpacingBeginCbkType,
+       lefrSpacingEndCbkType,
+       lefrArrayBeginCbkType,
+       lefrArrayEndCbkType,
+       lefrIRDropBeginCbkType,
+       lefrIRDropEndCbkType,
+       lefrNoiseMarginCbkType,
+       lefrEdgeRateThreshold1CbkType,
+       lefrEdgeRateThreshold2CbkType,
+       lefrEdgeRateScaleFactorCbkType,
+       lefrNoiseTableCbkType,
+       lefrCorrectionTableCbkType,
+       lefrInputAntennaCbkType,
+       lefrOutputAntennaCbkType,
+       lefrInoutAntennaCbkType,
+       lefrAntennaInputCbkType,
+       lefrAntennaInoutCbkType,
+       lefrAntennaOutputCbkType,
+       lefrManufacturingCbkType,
+       lefrUseMinSpacingCbkType,
+       lefrClearanceMeasureCbkType,
+       lefrTimingCbkType,
+       lefrMacroClassTypeCbkType,
+       lefrMacroOriginCbkType,
+       lefrMacroSizeCbkType,
+       lefrMacroFixedMaskCbkType,
+       lefrMacroEndCbkType,
+       lefrMaxStackViaCbkType,
+       lefrExtensionCbkType,
+       lefrDensityCbkType,
+       lefrFixedMaskCbkType,
+       lefrMacroSiteCbkType,
+       lefrMacroForeignCbkType,
+       lefrLibraryEndCbkType
+
+# Hold parsed LEF data in a global that gets cleared by parse() on each call.
+# The intended use of the LEF parser library is to pass around this data
+# structure via the void* passed into each callback, but this lets us avoid
+# having to deal with raw pointers to Python objects.
+# TODO: we could maybe make things a bit cleaner by encapsulating this into a
+# class, but probably no need for the additional complexity.
+_data = {}
+
+cdef int version_cb(lefrCallbackType_e type, double number, void* data):
+    global _data
+    _data['version'] = number
+    return 0
+
+# The single wrapper function we expose
 def parse(path):
-    # if lefrInit() != 0:
-    #     return None
+    global _data
+    _data = {}
+    if lefrInit() != 0:
+        return None
 
-    #  f_ptr = fopen(path, 'r')
-    #  if f_ptr == 0:
-    #      print("Couldn't open file " + path)
-    #      return None
+    lefrSetVersionCbk(version_cb)
 
-    #  r = lefrRead(f_ptr, path, NULL)
+    f_ptr = fopen(path, 'r')
+    if f_ptr == NULL:
+        print(b"Couldn't open file " + path)
+        return None
 
-    #  fclose(f_ptr)
+    r = lefrRead(f_ptr, path, NULL)
 
-    return "SUCCESS"
+    fclose(f_ptr)
+
+    return _data
