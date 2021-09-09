ab826375ed7dfeff4b9f945046078d1ee06580a5
Noah Moroze <noah@zeroasic.com>
2021-09-09 15:00:25 -0400

    Don't use deref(p) as RHS of any assignment
    Although we can get away with it in some cases, this is super dangerous
    and to be extra careful we just shouldn't do it. Even though the
    assignment performs a copy by value, the problem is some of the LEF
    parser objects have members that are pointers to other objects, and they
    free these pointers when their destructor is called. Therefore, these
    internal objects will be double free()-d when the functionr eturns.

--- siliconcompiler/leflib/_leflib.pyx
+++ siliconcompiler/leflib/_leflib.pyx
@@ -1,6 +1,24 @@
 # These imports let us use libc file I/O to communicate with C++ lef library
 from libc.stdio cimport fopen, fclose
 
+# All of the callback functions that do not receive primitive types instead
+# receive a pointer to an object defined by the C++ LEF parser. In order to access
+# members of this object, we must dereference these pointers using Cython's
+# `deref` operator.
+#
+# IMPORTANT NOTE: For memory safety purposes, we MUST NOT use a deref'd pointer
+# as the right-hand side of an assignment. 
+#
+# Although we can get away with it in some cases, this is super dangerous and to
+# be extra careful we should never do it. Even though the assignment performs
+# a copy-by-value, the problem is some of the LEF parser objects have members
+# that are pointers to other objects, and they free these pointers when their
+# destructor is called. Therefore, these internal objects will be double
+# free()'d when the function returns and the destructor of the original object
+# is called internally.
+#
+# Therefore, the correct way to call a method `func` of a C++ object pointer `o`
+# is `deref(o).func()`.
 from cython.operator import dereference as deref
 
 cimport _leflib
@@ -25,61 +43,56 @@ cdef int double_cb(lefrCallbackType_e cb_type, double value, lefiUserData data):
     elif cb_type == lefrVersionCbkType:
         _state.data['version'] = value
 
-cdef int units_cb(lefrCallbackType_e t, lefiUnits* unitsptr, lefiUserData data):
+cdef int units_cb(lefrCallbackType_e t, lefiUnits* u, lefiUserData data):
     if 'units' not in _state.data:
         _state.data['units'] = {}
  
-    cdef lefiUnits units = deref(unitsptr)
-    if units.hasDatabase():
-        _state.data['units']['database'] = units.databaseNumber()
-    if units.hasCapacitance():
-        _state.data['units']['capacitance'] = units.capacitance()
-    if units.hasResistance():
-        _state.data['units']['resistance'] = units.resistance()
-    if units.hasTime():
-        _state.data['units']['time'] = units.time()
-    if units.hasPower():
-        _state.data['units']['power'] = units.power()
-    if units.hasCurrent():
-        _state.data['units']['current'] = units.current()
-    if units.hasVoltage():
-        _state.data['units']['voltage'] = units.voltage()
-    if units.hasFrequency():
-        _state.data['units']['frequency'] = units.frequency()
+    if deref(u).hasDatabase():
+        _state.data['units']['database'] = deref(u).databaseNumber()
+    if deref(u).hasCapacitance():
+        _state.data['units']['capacitance'] = deref(u).capacitance()
+    if deref(u).hasResistance():
+        _state.data['units']['resistance'] = deref(u).resistance()
+    if deref(u).hasTime():
+        _state.data['units']['time'] = deref(u).time()
+    if deref(u).hasPower():
+        _state.data['units']['power'] = deref(u).power()
+    if deref(u).hasCurrent():
+        _state.data['units']['current'] = deref(u).current()
+    if deref(u).hasVoltage():
+        _state.data['units']['voltage'] = deref(u).voltage()
+    if deref(u).hasFrequency():
+        _state.data['units']['frequency'] = deref(u).frequency()
 
     return 0
 
-cdef int layer_cb(lefrCallbackType_e cb_type, lefiLayer* layer_ptr, void* data):
+cdef int layer_cb(lefrCallbackType_e cb_type, lefiLayer* l, void* data):
     if 'layers' not in _state.data:
         _state.data['layers'] = {}
 
-    cdef lefiLayer layer = deref(layer_ptr)
-    name = layer.name().decode('ascii')
+    name = deref(l).name().decode('ascii')
     _state.data['layers'][name] = {}
  
-    if layer.hasType():
-        _state.data['layers'][name]['type'] = layer.type().decode('ascii')
-    if layer.hasPitch():
-        _state.data['layers'][name]['pitch'] = layer.pitch()
-    if layer.hasXYPitch():
-        _state.data['layers'][name]['pitch'] = (layer.pitchX(), layer.pitchY())
-    if layer.hasOffset():
-        _state.data['layers'][name]['offset'] = layer.offset()
-    if layer.hasXYOffset():
-        _state.data['layers'][name]['offset'] = (layer.offsetX(), layer.offsetY())
-    if layer.hasWidth():
-        _state.data['layers'][name]['width'] = layer.width()
-    if layer.hasArea():
-        _state.data['layers'][name]['area'] = layer.area()
-    if layer.hasDirection():
-        _state.data['layers'][name]['direction'] = layer.direction().decode()
+    if deref(l).hasType():
+        _state.data['layers'][name]['type'] = deref(l).type().decode('ascii')
+    if deref(l).hasPitch():
+        _state.data['layers'][name]['pitch'] = deref(l).pitch()
+    if deref(l).hasXYPitch():
+        _state.data['layers'][name]['pitch'] = (deref(l).pitchX(), deref(l).pitchY())
+    if deref(l).hasOffset():
+        _state.data['layers'][name]['offset'] = deref(l).offset()
+    if deref(l).hasXYOffset():
+        _state.data['layers'][name]['offset'] = (deref(l).offsetX(), deref(l).offsetY())
+    if deref(l).hasWidth():
+        _state.data['layers'][name]['width'] = deref(l).width()
+    if deref(l).hasArea():
+        _state.data['layers'][name]['area'] = deref(l).area()
+    if deref(l).hasDirection():
+        _state.data['layers'][name]['direction'] = deref(l).direction().decode()
 
     return 0
 
 cdef int macro_cb(lefrCallbackType_e cb_type, lefiMacro* m, void* data):
-    # TODO: for some reason assigning deref(m) with a cdef results in double
-    # free() error, so we have to just call functions directly on deref'd one.
-
     if 'macros' not in _state.data:
         _state.data['macros'] = {}
 
