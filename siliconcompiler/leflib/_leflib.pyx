6653ecbcc0adfb4cf7ad9f61a09bb63358f8887a
Noah Moroze <noah@zeroasic.com>
2021-09-09 14:33:06 -0400

    Use class to encapsulate global data
    By using a class to encapsulate global data, we avoid having to modify
    global primitive types via assignment in functions, and we no longer
    have to sprinkle the 'global' keyword everywhere. This should be safer
    from bugs related to use of a global.

--- siliconcompiler/leflib/_leflib.pyx
+++ siliconcompiler/leflib/_leflib.pyx
@@ -5,71 +5,74 @@ from cython.operator import dereference as deref
 
 cimport _leflib
 
+class ParserState:
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.data = {}
+        self.cur_macro = None
+
 # Hold parsed LEF data in a global that gets cleared by parse() on each call.
 # The intended use of the LEF parser library is to pass around this data
-# structure via the void* passed into each callback, but this lets us avoid
-# having to deal with raw pointers to Python objects.
-# TODO: we could maybe make things a bit cleaner by encapsulating this into a
-# class, but probably no need for the additional complexity.
-_data = {}
+# structure via the void* passed into each callback, but using a global lets us
+# avoid having to deal with raw pointers to Python objects.
+_state = ParserState()
 
 cdef int double_cb(lefrCallbackType_e cb_type, double value, lefiUserData data):
-    global _data
     if cb_type == lefrManufacturingCbkType:
-        _data['manufacturinggrid'] = value
+        _state.data['manufacturinggrid'] = value
     elif cb_type == lefrVersionCbkType:
-        _data['version'] = value
+        _state.data['version'] = value
 
 cdef int units_cb(lefrCallbackType_e t, lefiUnits* unitsptr, lefiUserData data):
-    global _data
-    if 'units' not in _data:
-        _data['units'] = {}
-    
+    if 'units' not in _state.data:
+        _state.data['units'] = {}
+ 
     cdef lefiUnits units = deref(unitsptr)
     if units.hasDatabase():
-        _data['units']['database'] = units.databaseNumber()
+        _state.data['units']['database'] = units.databaseNumber()
     if units.hasCapacitance():
-        _data['units']['capacitance'] = units.capacitance()
+        _state.data['units']['capacitance'] = units.capacitance()
     if units.hasResistance():
-        _data['units']['resistance'] = units.resistance()
+        _state.data['units']['resistance'] = units.resistance()
     if units.hasTime():
-        _data['units']['time'] = units.time()
+        _state.data['units']['time'] = units.time()
     if units.hasPower():
-        _data['units']['power'] = units.power()
+        _state.data['units']['power'] = units.power()
     if units.hasCurrent():
-        _data['units']['current'] = units.current()
+        _state.data['units']['current'] = units.current()
     if units.hasVoltage():
-        _data['units']['voltage'] = units.voltage()
+        _state.data['units']['voltage'] = units.voltage()
     if units.hasFrequency():
-        _data['units']['frequency'] = units.frequency()
+        _state.data['units']['frequency'] = units.frequency()
 
     return 0
 
 cdef int layer_cb(lefrCallbackType_e cb_type, lefiLayer* layer_ptr, void* data):
-    global _data
-    if 'layers' not in _data:
-        _data['layers'] = {}
+    if 'layers' not in _state.data:
+        _state.data['layers'] = {}
 
     cdef lefiLayer layer = deref(layer_ptr)
     name = layer.name().decode('ascii')
-    _data['layers'][name] = {}
-    
+    _state.data['layers'][name] = {}
+ 
     if layer.hasType():
-        _data['layers'][name]['type'] = layer.type().decode('ascii')
+        _state.data['layers'][name]['type'] = layer.type().decode('ascii')
     if layer.hasPitch():
-        _data['layers'][name]['pitch'] = layer.pitch()
+        _state.data['layers'][name]['pitch'] = layer.pitch()
     if layer.hasXYPitch():
-        _data['layers'][name]['pitch'] = (layer.pitchX(), layer.pitchY())
+        _state.data['layers'][name]['pitch'] = (layer.pitchX(), layer.pitchY())
     if layer.hasOffset():
-        _data['layers'][name]['offset'] = layer.offset()
+        _state.data['layers'][name]['offset'] = layer.offset()
     if layer.hasXYOffset():
-        _data['layers'][name]['offset'] = (layer.offsetX(), layer.offsetY())
+        _state.data['layers'][name]['offset'] = (layer.offsetX(), layer.offsetY())
     if layer.hasWidth():
-        _data['layers'][name]['width'] = layer.width()
+        _state.data['layers'][name]['width'] = layer.width()
     if layer.hasArea():
-        _data['layers'][name]['area'] = layer.area()
+        _state.data['layers'][name]['area'] = layer.area()
     if layer.hasDirection():
-        _data['layers'][name]['direction'] = layer.direction().decode()
+        _state.data['layers'][name]['direction'] = layer.direction().decode()
 
     return 0
 
@@ -77,15 +80,14 @@ cdef int macro_cb(lefrCallbackType_e cb_type, lefiMacro* m, void* data):
     # TODO: for some reason assigning deref(m) with a cdef results in double
     # free() error, so we have to just call functions directly on deref'd one.
 
-    global _data
-    if 'macros' not in _data:
-        _data['macros'] = {}
+    if 'macros' not in _state.data:
+        _state.data['macros'] = {}
 
     name = deref(m).name().decode('ascii')
-    _data['macros'][name] = {}
-   
+    _state.data['macros'][name] = {}
+
     if deref(m).hasSize():
-        _data['macros'][name]['size'] = {
+        _state.data['macros'][name]['size'] = {
             'width': deref(m).sizeX(),
             'height': deref(m).sizeY()
         }
@@ -94,8 +96,8 @@ cdef int macro_cb(lefrCallbackType_e cb_type, lefiMacro* m, void* data):
 
 # The single wrapper function we expose
 def parse(path):
-    global _data
-    _data = {}
+    _state.clear()
+
     if lefrInit() != 0:
         return None
 
@@ -117,4 +119,4 @@ def parse(path):
 
     fclose(f_ptr)
 
-    return _data
+    return _state.data
