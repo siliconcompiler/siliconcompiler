5c2988fb1355c7f80eb234fb1110af60edfd0cfc
aolofsson <andreas@zeroasic.com>
2021-09-13 16:14:01 -0400

    Adding tools units tests

---
+++ tests/tools/test_verilator.py
@@ -0,0 +1,44 @@
+# Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import sys
+import pytest
+import os
+import siliconcompiler
+import importlib
+
+if __name__ != "__main__":
+    from tests.fixtures import test_wrapper
+
+def test_verilator():
+    '''Verilator setup unit test
+    '''
+
+    chip = siliconcompiler.Chip()
+
+    # per tool variables
+    tool = 'verilator'
+    step = 'import'
+    chip.set('design', 'mytopmodule')
+    chip.set('ydir', '$WORKDIR')
+    chip.set('idir', '$WORKDIR')
+    chip.set('vlib', '$WORKDIR/mylib.v')
+    chip.set('define', 'MYPARAM=1')
+    chip.set('cmdfile', '$WORKDIR/myfiles.f')
+    chip.set('source', 'mytopmodule.v')
+
+    # template / boiler plate code
+    searchdir = "siliconcompiler.tools." + tool
+    modulename = '.'+tool+'_setup'
+    module = importlib.import_module(modulename, package=searchdir)
+    setup_tool = getattr(module, "setup_tool")
+    setup_tool(chip, step, '0')
+
+    # test results
+    localcfg = chip.getcfg('eda',tool)
+    chip.writecfg(tool + '_setup.json', cfg=localcfg)
+    assert os.path.isfile(tool+'_setup.json')
+
+    return localcfg
+
+#########################
+if __name__ == "__main__":
+    test_verilator()
