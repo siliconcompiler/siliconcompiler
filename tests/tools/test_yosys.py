fb168710b829b7863bc8e02ac856f2e1450d6fdb
Andreas Olofsson <andreas@zeroasic.com>
2021-09-13 13:17:47 -0400

    Adding getcfg() method to core API (#497)
    * Adding getcfg() core method
    -Turned out to be trivial addition with current search method.
    -Needed for autodoc generator, but could be useful for others as well.
    
    * Adding tool unit test prototype
    
    * Name change

---
+++ tests/tools/test_yosys.py
@@ -0,0 +1,31 @@
+# Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import sys
+import pytest
+import os
+import siliconcompiler
+from siliconcompiler.tools.yosys.yosys_setup import setup_tool
+
+if __name__ != "__main__":
+    from tests.fixtures import test_wrapper
+
+def test_yosys():
+    '''Yosys setup unit test
+    '''
+
+    chip = siliconcompiler.Chip()
+
+    # set variables
+    chip.set('design', 'mytopmodule')
+    # run setup
+    setup_tool(chip, step='syn', index='0')
+    # write out dictionary
+    localcfg = chip.getcfg('eda','yosys')
+    chip.writecfg('yosys_setup.json', cfg=localcfg)
+    # check that file was written
+    assert os.path.isfile('yosys_setup.json')
+    # make test a reusable function
+    return localcfg
+
+#########################
+if __name__ == "__main__":
+    test_yosys()
