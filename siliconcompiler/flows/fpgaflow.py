2d4c9e0484ff56c0ff97bc89cd959f10f708de4f
aolofsson <andreas@zeroasic.com>
2021-09-14 14:15:47 -0400

    Standardizing fpga flow -Lookup table based approach based on part number regex

--- siliconcompiler/flows/fpgaflow.py
+++ siliconcompiler/flows/fpgaflow.py
@@ -37,23 +37,22 @@ def setup_flow(chip):
     else:
         partname = "UNDEFINED"
 
-    # Inferring vendor name based on part number
-    if re.match('ice', partname):
-        chip.set('fpga','vendor', 'lattice')
+    # Partname lookup
+    (vendor, flow) = flow_lookup(partname)
+    chip.set('fpga', 'vendor', vendor)
 
-    # A simple linear flow
-    flowpipe = ['import',
-                'syn',
-                'apr',
-                'bitstream']
-
-    # Linear flow
-    index = '0'
+    #Setting up pipeline
+    #TODO: Going forward we want to standardize steps
+    if flow in ('yosys-nextpnr', 'openfpga'):
+        flowpipe = ['import', 'syn', 'apr', 'bitstream']
+    elif flow in ('vivado', 'quartus'):
+        flowpipe = ['import', 'compile']
 
     # Set the steplist which can run remotely (if required)
     chip.set('remote', 'steplist', flowpipe[1:])
 
     # Minimal setup
+    index = '0'
     for i, step in enumerate(flowpipe):
 
         # Metrics
@@ -63,41 +62,132 @@ def setup_flow(chip):
 
         # Goals
         chip.set('metric', step, index, 'drv', 'errors', 0)
-        #chip.set('metric', step, index, 'drv', 'warnings', 0)
         chip.set('metric', step, index, 'drv', 'goal', 0.0)
         chip.set('metric', step, index, 'holdwns', 'goal', 0.0)
         chip.set('metric', step, index, 'holdtns', 'goal', 0.0)
         chip.set('metric', step, index, 'setupwns', 'goal', 0.0)
         chip.set('metric', step, index, 'setuptns', 'goal', 0.0)
 
+        # Inputs
         if i > 0:
             chip.add('flowgraph', flowpipe[i], index, 'input', flowpipe[i-1], "0")
 
-    # Per step tool selection
-    for step in flowpipe:
-        if step == 'import':
-            tool = 'verilator'
-        elif step == 'syn':
-            tool = 'yosys'
-        elif step == 'apr':
-            if re.match('ice', partname):
-                tool = 'nextpnr'
-            else:
-                # TODO: eventually we want to drive vpr directly without going
-                # through openfpga
-                # tool = 'vpr'
-                tool = 'openfpga'
-        elif step == 'bitstream':
-            if re.match('ice', partname):
-                tool = 'icepack'
-            else:
-                tool = 'openfpga'
-        elif step == 'program':
-            if re.match('ice', partname):
-                tool = 'iceprog'
-            else:
-                tool = 'openfpga'
+        # Tools
+        tool = tool_lookup(flow, step)
         chip.set('flowgraph', step, index, 'tool', tool)
+
+##################################################
+
+def flow_lookup(partname):
+    '''
+    Returns a flow,vendor tuple based on a partnumber
+    regular expression.
+    '''
+
+    partname = partname.lower()
+
+    ###########
+    # xilinx
+    ###########
+
+    spartan6 = bool(re.match('^xc6', partname))
+    spartan7 = bool(re.match('^xc7s', partname))
+    artix = bool(re.match('^xc7a', partname))
+    artixultra = bool(re.match('^au', partname))
+    kintex7 = bool(re.match('^xc7k', partname))
+    kintexultra = bool(re.match('^xcku', partname))
+    zynq = bool(re.match('^z\-7', partname))
+    zynqultra = bool(re.match('^zu', partname))
+    virtex7 = bool(re.match('^xc7v', partname))
+    virtexultra = bool(re.match('^xcvu', partname))
+
+    xilinx = (spartan6 | spartan7 |
+              artix | artixultra |
+              kintex7 | kintexultra |
+              zynq | zynqultra |
+              virtex7 | virtexultra)
+
+    #############
+    # intel
+    #############
+
+    cyclone4 = bool(re.match('^ep4', partname))
+    cyclone5 = bool(re.match('^5cs', partname))
+    cyclone10 = bool(re.match('^10cl', partname))
+    stratix5 = bool(re.match('^5sg', partname))
+
+    intel = (cyclone10 | cyclone4 | cyclone5 |
+             stratix5)
+
+    ###########
+    # yosys
+    ###########
+
+    ice40 = re.match('^ice40', partname)
+
+    ###########
+    # openfpga
+    ###########
+
+    openfpga = re.match('^openfpga', partname)
+
+    if xilinx:
+        vendor = 'xilinx'
+        flow = 'vivado'
+    elif intel:
+        vendor = 'intel'
+        flow = 'quartus'
+    elif ice40:
+        vendor = 'lattice'
+        flow = 'yosys-nextpnr'
+    elif openfpga:
+        vendor = 'openfpga'
+        flow = 'openfpga'
+    else:
+        vendor = 'UNSUPPORTED'
+        flow = 'UNSUPPORTED'
+
+    return (vendor, flow)
+
+##################################################
+
+def tool_lookup(flow, step):
+    '''
+    Return tool based on flow and step combo.
+    '''
+
+    '''
+    Utility function for looking up tool
+    based on flow and step.
+    '''
+
+    # common first step
+    if step == "import":
+        tool = 'verilator'
+    # open source ice40 flow
+    elif flow == "yosys-nextpnr":
+        if step == "syn":
+            tool = "yosys"
+        elif step == "apr":
+            tool = "nextpnr"
+        elif step == "bitstream":
+            tool = "icepack"
+        elif step == "program":
+            tool = "iceprog"
+    # experimental flow
+    elif flow == "openfpga":
+        if step == "syn":
+            tool = "yosys"
+        else:
+            tool = 'openfpga'
+    # intel/quartus
+    elif flow == "quartus":
+        tool = 'quartus'
+    # xilinx/vivado
+    elif flow == "vivado":
+        tool = 'vivado'
+
+    return tool
 
 ##################################################
 if __name__ == "__main__":
