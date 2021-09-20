01aa6aa1707e3dad4209be4ea34c401102eaad5c
aolofsson <andreas@zeroasic.com>
2021-09-20 13:39:55 -0400

    Moving dynamic options for klayout to runtime_options -This needs to move into a python source file!

---
+++ siliconcompiler/tools/klayout/klayout.py
@@ -0,0 +1,165 @@
+import os
+import re
+import shutil
+import siliconcompiler
+from siliconcompiler.schema_utils import schema_path
+
+####################################################################
+# Make Docs
+####################################################################
+
+def make_docs():
+    '''
+    Klayout is a production grade viewer and editor of GDSII and
+    Oasis data with customizable Python and Ruby interfaces.
+
+    Documentation: https://www.klayout.de
+
+    Sources: https://github.com/KLayout/klayout
+
+    Installation: https://github.com/KLayout/klayout
+
+    '''
+
+    chip = siliconcompiler.Chip()
+    chip.target('freepdk45')
+    chip.set('arg','step','export')
+    chip.set('arg','index','<index>')
+    setup_tool(chip)
+
+    return chip
+
+####################################################################
+# Setup tool
+####################################################################
+
+def setup_tool(chip, mode="batch"):
+    '''
+    Setup function for Klayout
+    '''
+
+    tool = 'klayout'
+    refdir = 'tools/'+tool
+    step = chip.get('arg','step')
+    index = chip.get('arg','index')
+
+    if mode == 'show':
+        clobber = True
+        script = '/klayout_show.py'
+        option = '-nn'
+    else:
+        clobber = False
+        script = '/klayout_export.py'
+        option = '-zz'
+
+    chip.set('eda', tool, step, index, 'exe', 'klayout', clobber=clobber)
+    chip.set('eda', tool, step, index, 'copy', 'true', clobber=clobber)
+    chip.set('eda', tool, step, index, 'refdir', refdir, clobber=clobber)
+    chip.set('eda', tool, step, index, 'script', refdir + script, clobber=clobber)
+    chip.set('eda', tool, step, index, 'vswitch', '-zz -v', clobber=clobber)
+    chip.set('eda', tool, step, index, 'version', '0.26.10', clobber=clobber)
+    chip.set('eda', tool, step, index, 'option', 'cmdline', option, clobber=clobber)
+
+################################
+#  Custom runtime options
+################################
+
+def runtime_options(chip):
+
+    ''' Custom runtime options, returnst list of command line options.
+    '''
+
+    step = chip.get('arg','step')
+    index = chip.get('arg','index')
+
+    cmdlist = []
+
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+    sc_root =  re.sub('siliconcompiler/siliconcompiler/tools/klayout',
+                      'siliconcompiler',
+                      scriptdir)
+    sc_path = sc_root + '/third_party/foundry'
+
+    # TODO: should support multiple target libs?
+    libname = chip.get('asic', 'targetlib')[0]
+    pdk_rev = chip.get('pdk', 'version')
+    lib_rev = chip.get('library', libname, 'version')
+    targetlist = chip.get('target').split('_')
+    platform =  targetlist[0]
+
+    #TODO: pass in SC cfg file klayout python script?
+    foundry_path = f'%s/%s/%s/pdk/{pdk_rev}'%(sc_path, chip.get('pdk','foundry'), platform)
+    lefs_path = f'%s/%s/%s/libs/{libname}/{lib_rev}/lef'%(sc_path,chip.get('pdk','foundry'),platform)
+    tech_file = '%s/setup/klayout/%s.lyt'%(foundry_path,platform)
+    config_file = '%s/setup/klayout/fill.json'%(foundry_path)
+
+    #TODO: Fix, currenly only accepts one GDS file, need to loop
+    if step == 'export':
+        options = []
+        options.append('-rd')
+        options.append('design_name=%s'%(chip.get('design')))
+        options.append('-rd')
+        options.append(f'in_def=inputs/%s.def'%(chip.get('design')))
+        options.append('-rd')
+        options.append('seal_file=""')
+        options.append('-rd')
+        stdcell_gds = chip.get('library', libname, 'gds')[0]
+        gds_files = [f'{sc_root}/{stdcell_gds}']
+        for macrolib in chip.get('asic', 'macrolib'):
+            for gds in chip.get('library', macrolib, 'gds'):
+                gds_files.append(schema_path(gds))
+        gds_list = ' '.join(gds_files)
+        options.append(f'in_files="{gds_list}"')
+        options.append('-rd')
+        options.append('out_file=outputs/%s.gds'%(chip.get('design')))
+        options.append('-rd')
+        options.append('tech_file=%s'%tech_file)
+        options.append('-rd')
+        if os.path.isfile(config_file):
+            options.append('config_file=%s'%config_file)
+        else:
+            options.append('config_file=""')
+        options.append('-rd')
+        options.append('foundry_lefs=%s'%lefs_path)
+
+        options.append('-rd')
+        lef_files = []
+        for macrolib in chip.get('asic', 'macrolib'):
+            for lef in chip.get('library', macrolib, 'lef'):
+                lef_files.append(schema_path(lef))
+        lef_list = ' '.join(lef_files)
+        options.append(f'macro_lefs="{lef_list}"')
+        options.append('-r')
+        options.append('klayout_export.py')
+
+        return options
+
+################################
+# Version Check
+################################
+
+def check_version(chip, version):
+    ''' Tool specific version checking
+    '''
+    step = chip.get('arg','step')
+    index = chip.get('arg','index')
+    required = chip.get('eda', 'klayout', step, index, 'version')
+    #insert code for parsing the funtion based on some tool specific
+    #semantics.
+    #syntax for version is string, >=string
+
+    return 0
+
+##################################################
+if __name__ == "__main__":
+    # File being executed
+    prefix = os.path.splitext(os.path.basename(__file__))[0]
+    output = prefix + '.json'
+
+    # create a chip instance
+    chip = siliconcompiler.Chip()
+    chip.target("freepdk45")
+    # load configuration
+    setup_tool(chip, step='export', index='0')
+    # write out results
+    chip.writecfg(output)
