a112e1fded6dc6f8c9e60841801043e379a4310a
Noah Moroze <noahmoroze@gmail.com>
2021-09-28 14:16:57 -0400

    Fix macOS multiprocessing bug
    fixes #553

--- siliconcompiler/core.py
+++ siliconcompiler/core.py
@@ -30,6 +30,13 @@ from timeit import default_timer as timer
 from siliconcompiler.client import *
 from siliconcompiler.schema import *
 
+# Fix for multprocessing bug on MacOS
+# Source: https://github.com/Koed00/django-q/issues/389#issuecomment-699481976
+import platform
+if platform.system() != "Linux":
+    from multiprocessing import set_start_method
+    set_start_method("fork")
+
 class Chip:
     """Object for configuring and executing hardware design flows.
 
