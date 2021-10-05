6abf4b363e8bd82e1eb2ca2a2488c95aee330c48
Will <5217539+WRansohoff@users.noreply.github.com>
2021-10-05 11:19:28 -0400

    Use consistent string formatting in the 'fetch_results' method.

--- siliconcompiler/client.py
+++ siliconcompiler/client.py
@@ -341,9 +341,9 @@ def fetch_results(chip):
     top_design = chip.get('design')
     job_hash = chip.get('remote', 'jobhash')
     job_nameid = f"{chip.get('jobname')}{chip.get('jobid')}"
-    subprocess.run(['unzip', '-q', '%s.zip'%job_hash])
+    subprocess.run(['unzip', '-q', f'{job_hash}.zip'])
     # Remove the results archive after it is extracted.
-    os.remove('%s.zip'%job_hash)
+    os.remove(f'{job_hash}.zip')
 
     # Call 'delete_job' to remove the run from the server.
     delete_job(chip)
