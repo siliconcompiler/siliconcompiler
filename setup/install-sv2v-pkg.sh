15db7e8ff6c90bdb0db95f289a4dd27a73888fcf
WRansohoff <5217539+WRansohoff@users.noreply.github.com>
2021-10-19 13:44:25 -0500

    Update sv2v version to 0.0.9
    The new asicflow requires a newer version of sv2v, which can write to files instead of stdout. It looks like the required functionality is included in the 0.0.9 release.

--- setup/install-sv2v-pkg.sh
+++ setup/install-sv2v-pkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p deps
 cd deps
-wget -q https://github.com/zachjs/sv2v/releases/download/v0.0.7/sv2v-Linux.zip
+wget -q https://github.com/zachjs/sv2v/releases/download/v0.0.9/sv2v-Linux.zip
 unzip sv2v-Linux
 sudo mv sv2v-Linux/sv2v /usr/bin
 rm -rf sv2v-Linux
