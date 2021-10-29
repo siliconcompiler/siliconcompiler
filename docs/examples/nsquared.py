5069b75a4b5d03f8e26ea8518a9dfb1c6bb5a7d6
Andreas.Olofsson <andreas.d.olofsson@gmail.com>
2021-10-29 08:31:01 -0400

    Adding comparison graph betwee n2 and sc

--- docs/examples/nsquared.py
+++ docs/examples/nsquared.py
@@ -1,22 +1,40 @@
 import siliconcompiler
-chip = siliconcompiler.Chip()
-
-
 
 #design to eda, eda to pdk
-designs=8
-eda=4
-pdks=8
+designs = 4
+eda = 2
+pdks = 4
 
+# Status Quo problem
+chip = siliconcompiler.Chip()
 for i in range(designs):
-    chip.add('flowgraph','design'+str(i),'0', 'tool', 'manual')
+    chip.set('flowgraph','design', str(i), 'tool', 'source')
     for j in range(eda):
-        chip.add('flowgraph','eda'+str(j),'0', 'input', 'design'+str(i)+'0')
+        chip.add('flowgraph','eda', str(j), 'input', 'design'+str(i))
 
 for j in range(eda):
     for k in range(pdks):
-        chip.add('flowgraph','pdk'+str(k),'0', 'input', 'eda'+str(j)+'0')
+        chip.add('flowgraph','pdk', str(k), 'input', 'eda'+str(j))
 
-chip.write_flowgraph('nsquared.png',
-                     fillcolor='#1c4587', fontcolor='#f1c232',
-                     border=False, landscape=True)
+chip.write_flowgraph('nsquared.png', fillcolor='#FFFFFF')
+
+# SC option
+chip = siliconcompiler.Chip()
+
+#design
+
+#ir0
+for i in range(designs):
+    chip.set('flowgraph','design', str(i), 'tool', 'source')
+    chip.add('flowgraph','sc0', '0', 'input', 'design'+str(i))
+
+#eda
+for i in range(eda):
+    chip.add('flowgraph','eda', str(i), 'input', 'sc00')
+    chip.add('flowgraph','sc1', '0', 'input', 'eda'+str(i))
+
+#pdk
+for i in range(pdks):
+    chip.add('flowgraph','pdk', str(i), 'input', 'sc10')
+
+chip.write_flowgraph('sc_ir.png',fillcolor='#FFFFFF')
