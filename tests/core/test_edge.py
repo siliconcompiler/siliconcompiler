0fcaf8e2665d3bb5a12622dca5ba6f2919f53aff
aolofsson <andreas@zeroasic.com>
2021-11-01 14:43:19 -0400

    Updating edge unit test for edge() api change

--- tests/core/test_edge.py
+++ tests/core/test_edge.py
@@ -10,15 +10,16 @@ def test_edge():
 
     #nodes
     chip.node('import', 'surelog')
-    chip.node('syn', 'yosys', n=syn_np)
+    for i in range(syn_np):
+        chip.node('syn', 'yosys', index=i)
     chip.node('synmin', 'minimum')
     chip.node('floorplan', 'openroad')
 
     #edges
-    chip.edge('import', 'syn', nhead=syn_np)
-    chip.edge('syn', 'synmin', ntail=syn_np)
+    for i in range(syn_np):
+        chip.edge('import', 'syn', head_index=i)
+        chip.edge('syn', 'synmin', tail_index=i)
     chip.edge('synmin', 'floorplan')
-
     chip.write_flowgraph('flow.png')
     assert os.path.isfile('flow.png')
 
