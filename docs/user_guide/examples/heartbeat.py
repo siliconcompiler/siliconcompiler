52764d8c71f64f8e4a91ed830d251d2103644660
aolofsson <andreas@zeroasic.com>
2021-11-05 12:21:02 -0400

    Removing deprecated remote schema parameters

--- docs/user_guide/examples/heartbeat.py
+++ docs/user_guide/examples/heartbeat.py
@@ -5,9 +5,6 @@ chip.add('source', 'heartbeat.v')             # define list of source files
 chip.set('design', 'heartbeat')               # set top module
 chip.clock(name='clk', pin='clk', period=1.0) # define a clock
 chip.target('asicflow_freepdk45')             # load builtin compiler settings
-#remote_server = 'server.siliconcompiler.com'  # remote server address
-#chip.set('remote','addr', remote_server)      # specify name of remote server
-#chip.set('remote','user', '<signup email>')   # userid
-#chip.set('remote','password','<password>')    # password
+#chip.set('remote', True)                     # runs compilation remotely
 chip.run()                                    # run compilation
 chip.summary()                                # print results summary
