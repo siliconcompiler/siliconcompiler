bafb096bcac4d0b7bd8e129fc5eb713cec64b47b
aolofsson <andreas@zeroasic.com>
2021-11-05 22:09:47 -0400

    Adding show_file at end of heartbeat example -Uncomment to enable

--- docs/user_guide/examples/heartbeat.py
+++ docs/user_guide/examples/heartbeat.py
@@ -8,3 +8,4 @@ chip.target('asicflow_freepdk45')             # load builtin compiler settings
 #chip.set('remote', True)                     # runs compilation remotely
 chip.run()                                    # run compilation
 chip.summary()                                # print results summary
+#chip.show_file()                             # show layout file
