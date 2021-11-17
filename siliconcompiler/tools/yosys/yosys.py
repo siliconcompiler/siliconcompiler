1af75498f5ff64cf3b3d2d40b7654fbbb09f18b6
aolofsson <andreas@zeroasic.com>
2021-11-17 09:05:49 -0500

    Setting errors metric to 0 for yosys -Not needed, errors always fatal.

--- siliconcompiler/tools/yosys/yosys.py
+++ siliconcompiler/tools/yosys/yosys.py
@@ -145,12 +145,13 @@ def post_process(chip):
     index = chip.get('arg','index')
 
     if step == 'syn':
+        #TODO: looks like Yosys exits on error, so no need to check metric
+        chip.set('metric', step, index, 'errors', 'real', 0, clobber=True)
         with open(step + ".log") as f:
             for line in f:
                 area = re.search(r'Chip area for module.*\:\s+(.*)', line)
                 cells = re.search(r'Number of cells\:\s+(.*)', line)
                 warnings = re.search(r'Warnings.*\s(\d+)\s+total', line)
-
                 if area:
                     chip.set('metric', step, index, 'cellarea', 'real', round(float(area.group(1)),2), clobber=True)
                 elif cells:
