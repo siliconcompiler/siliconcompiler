0af571ba52f91ca079ba2cdff79c6615ec6e644a
aolofsson <andreas@zeroasic.com>
2021-11-27 20:55:51 -0500

    Quickstart cleanup - Moving away from clock as this is not orthogonal to the sc app.

--- docs/user_guide/examples/heartbeat.py
+++ docs/user_guide/examples/heartbeat.py
@@ -2,7 +2,7 @@ import siliconcompiler                        # import python package
 chip = siliconcompiler.Chip()                 # create chip object
 chip.set('source', 'heartbeat.v')             # define list of source files
 chip.set('design', 'heartbeat')               # set top module
-chip.clock(name='clk', pin='clk', period=1.0) # define a clock
+chip.set('constraint', 'heartbeat.sdc')       # set constraints file
 chip.target('asicflow_freepdk45')             # load predefined target
 chip.run()                                    # run compilation
 chip.summary()                                # print results summary
