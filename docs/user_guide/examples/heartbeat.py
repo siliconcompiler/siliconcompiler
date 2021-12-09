c586a7e9ba3a115a5b8d085a4c835497d0527458
Noah Moroze <noah@zeroasic.com>
2021-12-09 12:55:22 -0500

    Update heartbeat.py to check __name__ == '__main__'
    This is necessary to avoid a multiprocessing bug on Windows.

--- docs/user_guide/examples/heartbeat.py
+++ docs/user_guide/examples/heartbeat.py
@@ -1,9 +1,14 @@
-import siliconcompiler                        # import python package
-chip = siliconcompiler.Chip()                 # create chip object
-chip.set('source', 'heartbeat.v')             # define list of source files
-chip.set('design', 'heartbeat')               # set top module
-chip.set('constraint', 'heartbeat.sdc')       # set constraints file
-chip.target('asicflow_freepdk45')             # load predefined target
-chip.run()                                    # run compilation
-chip.summary()                                # print results summary
-chip.show()                                   # show layout file
+import siliconcompiler                            # import python package
+
+def main():
+    chip = siliconcompiler.Chip()                 # create chip object
+    chip.set('source', 'heartbeat.v')             # define list of source files
+    chip.set('design', 'heartbeat')               # set top module
+    chip.set('constraint', 'heartbeat.sdc')       # set constraints file
+    chip.target('asicflow_freepdk45')             # load predefined target
+    chip.run()                                    # run compilation
+    chip.summary()                                # print results summary
+    chip.show()                                   # show layout file
+
+if __name__ == '__main__':
+    main()
