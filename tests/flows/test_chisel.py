ffcf862e6c54ed2fe279c3fa99441967eec08702
Noah Moroze <noah@zeroasic.com>
2021-12-17 15:49:47 -0500

    Make chisel test an end-to-end test

--- tests/flows/test_chisel.py
+++ tests/flows/test_chisel.py
@@ -7,16 +7,14 @@ import pytest
 def test_chisel(datadir):
     design = 'gcd'
     gcd_src = os.path.join(datadir, f'{design}.scala')
-    step = 'import'
 
     chip = siliconcompiler.Chip(loglevel="INFO")
 
     chip.add('source', gcd_src)
     chip.set('design', design)
-    chip.set('mode', 'sim')
-    chip.set('arg', 'step', step)
-    chip.target('chisel')
+    chip.set('frontend', 'chisel')
+    chip.target('asicflow_freepdk45')
 
     chip.run()
 
-    assert chip.find_result('v', step=step) is not None
+    assert chip.find_result('gds', step='export') is not None
