aad035266d794c5293a90344e53747ce81d1c672
Noah Moroze <noah@zeroasic.com>
2021-12-18 23:22:25 -0500

    Fix bambu install script

--- setup/install-bambu.sh
+++ setup/install-bambu.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
-sudo apt-get install -y autoconf autoconf-archive automake libtool g++ gcc-4.8 \
-    g++-4.8 gcc-5 g++-5 gcc-6 g++-6 gcc-7 g++-7 gcc-8 g++-8 gcc-4.8-plugin-dev \
-    gcc-5-plugin-dev gcc-6-plugin-dev gcc-7-plugin-dev  gcc-8-plugin-dev \
-    gcc-4.8-multilib gcc-5-multilib gcc-6-multilib gcc-7-multilib gcc-8-multilib \
-    g++-4.8-multilib g++-5-multilib g++-6-multilib g++-7-multilib g++-8-multilib \
-    gfortran-4.8 gfortran-4.8-multilib gfortran-5 gfortran-5-multilib gfortran-6 \
-    gfortran-6-multilib gfortran-7 gfortran-7-multilib gfortran-8 \
-    gfortran-8-multilib clang-4.0 libclang-4.0-dev libclang-6.0-dev clang-6.0 \
-    libclang-6.0-dev clang-7 libclang-7-dev libbdd-dev libboost-all-dev \
-    libmpc-dev libmpfr-dev libxml2-dev liblzma-dev libmpfi-dev zlib1g-dev \
-    libicu-dev bison doxygen flex graphviz iverilog verilator make \
-    libsuitesparse-dev libglpk-dev
+sudo apt-get install -y autoconf autoconf-archive automake libtool g++ \
+    gcc-7 g++-7 gcc-8 g++-8 gcc-7-plugin-dev  gcc-8-plugin-dev gcc-7-multilib \
+    gcc-8-multilib g++-7-multilib g++-8-multilib gfortran-7 gfortran-7-multilib \
+    gfortran-8 gfortran-8-multilib libclang-6.0-dev clang-6.0 libclang-6.0-dev \
+    clang-7 libclang-7-dev libbdd-dev libboost-all-dev libmpc-dev libmpfr-dev \
+    libxml2-dev liblzma-dev libmpfi-dev zlib1g-dev libicu-dev bison doxygen flex \
+    graphviz iverilog verilator make libsuitesparse-dev libglpk-dev libgmp-dev
 
 mkdir -p deps
 cd deps
 git clone https://github.com/ferrandi/PandA-bambu.git
 cd PandA-bambu
+
+sudo mkdir -p /opt/panda
+sudo chown $USER:$USER /opt/panda
 
 make -f Makefile.init
 
@@ -28,3 +26,5 @@ make
 sudo make install
 
 cd ../../../
+
+echo "Please add \"export PATH="/opt/panda/bin:\$PATH"\" to your .bashrc"
