cecfcba338e3aa0d37802122d5352aedc651f7aa
Noah Moroze <noahmoroze@gmail.com>
2022-01-04 17:06:38 -0500

    Test KLayout on all platforms after wheels built

--- .github/workflows/bin/setup_wheel_env_linux.sh
+++ .github/workflows/bin/setup_wheel_env_linux.sh
@@ -3,6 +3,10 @@
 # Install dependencies
 yum install -y libuuid-devel zlib-devel java-1.8.0-openjdk-devel graphviz
 
+# Install Klayout (for chip.show() test)
+wget https://www.klayout.org/downloads/CentOS_7/klayout-0.27.5-0.x86_64.rpm
+sudo rpm -i klayout-0.27.5-0.x86_64.rpm
+
 # Build surelog (install prefix defined outside file)
 git submodule update --init --recursive third_party/tools/surelog
 cd third_party/tools/surelog
