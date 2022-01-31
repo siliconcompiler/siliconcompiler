fc3f40d23d94dba81e472d1b07cff14dedb2e398
aolofsson <andreas@zeroasic.com>
2022-01-31 11:53:28 -0500

    Adding test and data for new check_logfile() method

---
+++ tests/core/test_check_logfile.py
@@ -0,0 +1,25 @@
+import os
+import siliconcompiler
+
+def test_check_logfile(datadir):
+
+    chip = siliconcompiler.Chip(loglevel="INFO")
+
+    # mandatory to have manifest loaded
+    manifest = os.path.join(datadir, 'gcd.pkg.json')
+    chip.read_manifest(manifest)
+
+    # add regex
+    chip.write_manifest("tmp.json", prune=False)
+    chip.add('eda', 'openroad', 'regex', 'place', '0', 'warnings', "WARNING")
+    #chip.add('eda', 'openroad', 'regex', 'place', '0', 'warnings', "-v DPL")
+
+    # check log
+    logfile = os.path.join(datadir, 'place.log')
+    chip.check_logfile('place', logfile)
+
+
+#########################
+if __name__ == "__main__":
+    from tests.fixtures import datadir
+    test_check_logfile(datadir(__file__))
