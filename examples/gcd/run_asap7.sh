cc4084d3ca420a4299b5e588c7363c1e13e8f14b
aolofsson <andreas@zeroasic.com>
2022-02-08 14:35:46 -0500

    Adding multi-vt asap7 library

---
+++ examples/gcd/run_asap7.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+OPT=$1
+
+echo $OPT
+sc examples/gcd/gcd.v \
+   -target "asap7_demo" \
+   -constraint "examples/gcd/gcd.sdc" \
+   -asic_diearea "(0,0)" \
+   -asic_diearea "(100.13,100.8)" \
+   -asic_corearea "(10.07,11.2)" \
+   -asic_corearea "(90.25,91)" \
+   -loglevel "INFO" \
+   -quiet \
+   -relax \
+   -design gcd $OPT
