c2ce6e3dce2ed3b6c53aa3586a8a1fa6ba5930d4
aolofsson <andreas@zeroasic.com>
2022-02-08 08:11:43 -0500

    Addding asap7 target

---
+++ siliconcompiler/targets/asap7_demo.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import re
+import siliconcompiler
+
+def make_docs():
+    '''
+    Documentation
+    '''
+
+    chip = siliconcompiler.Chip()
+    setup(chip)
+    return chip
+
+def setup(chip):
+    '''
+    Target setup
+    '''
+
+    #1. Defining the project
+    target = 'asap7_demo'
+    chip.set('target', target)
+
+    #2. Load PDK, flow, libs combo
+    chip.load_pdk('asap7')
+    chip.load_flow('asicflow')
+    chip.load_lib('asap7sc7p5t')
+
+    #3. Select default flow
+    chip.set('flow', 'asicflow')
+
+    #4. Select libraries
+    chip.set('asic', 'logiclib', 'asap7sc7p5t_rvt')
+
+    #5. Project specific design choices
+    chip.set('asic', 'stackup', '10M')
+    chip.set('asic', 'minlayer', "m2")
+    chip.set('asic', 'maxlayer', "m7")
+    chip.set('asic', 'maxfanout', 64)
+    chip.set('asic', 'maxlength', 1000)
+    chip.set('asic', 'maxslew', 0.2e-9)
+    chip.set('asic', 'maxcap', 0.2e-12)
+    chip.set('asic', 'rclayer', 'clk', "m5")
+    chip.set('asic', 'rclayer', 'data',"m3")
+    chip.set('asic', 'hpinlayer', "m4")
+    chip.set('asic', 'vpinlayer', "m5")
+    chip.set('asic', 'density', 10)
+    chip.set('asic', 'aspectratio', 1)
+    chip.set('asic', 'coremargin', 0.270)
+
+    #5. Timing corners
+    corner = 'typical'
+    chip.set('mcmm','worst','libcorner', corner)
+    chip.set('mcmm','worst','pexcorner', corner)
+    chip.set('mcmm','worst','mode', 'func')
+    chip.set('mcmm','worst','check', ['setup','hold'])
+
+#########################
+if __name__ == "__main__":
+
+    chip = make_docs()
