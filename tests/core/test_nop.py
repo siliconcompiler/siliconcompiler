9443cb5458fd44520c5527037e00eea5fed6f9b2
Andreas.Olofsson <andreas.d.olofsson@gmail.com>
2022-03-03 16:04:39 -0500

    Test for nop operation -Place holder

---
+++ tests/core/test_nop.py
@@ -0,0 +1,25 @@
+# Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import siliconcompiler
+import pytest
+
+
+##################################
+def test_nop():
+    '''API test for nop methods
+    '''
+
+    chip = siliconcompiler.Chip()
+    chip.load_target('freepdk45_demo')
+    chip.set('design', 'gcd')
+    chip.set('flow', 'test')
+    chip.node('test', 'import', 'surelog')
+    chip.node('test', 'nop1', 'nop')
+    chip.node('test', 'nop2', 'nop')
+    chip.edge('test', 'import', 'nop1')
+    chip.edge('test', 'nop1', 'nop2')
+    chip.check_manifest()
+    chip.write_flowgraph("nop.png")
+
+#########################
+if __name__ == "__main__":
+    test_nop()
