abb299afd2ff5336388971d4bca2ea5c4c026b66
Noah Moroze <noah@zeroasic.com>
2022-03-10 12:48:55 -0500

    Update examples in Execution Model

--- docs/user_guide/examples/heartbeat_flowgraph.py
+++ docs/user_guide/examples/heartbeat_flowgraph.py
@@ -3,11 +3,13 @@ chip = siliconcompiler.Chip()              # create chip object
 chip.set('source', 'heartbeat.v')          # define list of source files
 chip.set('design', 'heartbeat')            # set top module
 chip.set('constraint', 'heartbeat.sdc')    # set constraints file
-chip.load_pdk('freepdk45')                 # load freepdk45
+chip.load_target('freepdk45_demo')         # load freepdk45
 # start of flowgraph setup
-chip.node('import', 'surelog')             # use surelog for import
-chip.node('syn', 'yosys')                  # use yosys for synthesis
-chip.edge('import', 'syn')                 # perform syn after import
+flow = 'synflow'
+chip.node(flow, 'import', 'surelog')       # use surelog for import
+chip.node(flow, 'syn', 'yosys')            # use yosys for synthesis
+chip.edge(flow, 'import', 'syn')           # perform syn after import
+chip.set('flow', flow)
 # end of flowgraph setup
 chip.run()                                 # run compilation
 chip.summary()                             # print results summary
