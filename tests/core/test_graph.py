b3810c8bc26dde92d5eb3fbaf2338699ee186f30
aolofsson <andreas@zeroasic.com>
2022-03-15 18:29:56 -0400

    New graph pipeline test

--- tests/core/test_graph.py
+++ tests/core/test_graph.py
@@ -7,9 +7,40 @@ def test_graph():
     chip = siliconcompiler.Chip()
     chip.load_target('skywater130_demo')
 
-    chip.graph("top","asicflow", prefix="a_")
-    chip.graph("top","signoffflow", prefix="b_")
-    chip.edge("top", "a_export", "b_import")
+    #RTL
+    chip.pipeline('rtl', [{'import' : 'surelog'},
+                          {'syn' : 'yosys'},
+                          {'export' : 'nop'},])
+
+    #APR
+    chip.pipeline('apr', [{'import' : 'nop'},
+                          {'floorplan' : 'openroad'},
+                          {'physyn' : 'openroad'},
+                          {'place' : 'openroad'},
+                          {'cts' : 'openroad'},
+                          {'route' : 'openroad'},
+                          {'dfm' : 'openroad'},
+                          {'export' : 'klayout'}])
+
+    #SIGNOFF
+    chip.node('signoff', 'import', 'nop')
+    chip.node('signoff', 'extspice', 'magic')
+    chip.node('signoff', 'drc', 'magic')
+    chip.node('signoff', 'lvs', 'netgen')
+    chip.node('signoff', 'export', 'join')
+
+    chip.edge('signoff', 'import', 'drc')
+    chip.edge('signoff', 'import', 'extspice')
+    chip.edge('signoff', 'extspice', 'lvs')
+    chip.edge('signoff', 'lvs', 'export')
+    chip.edge('signoff', 'drc', 'export')
+
+    #TOP
+    chip.graph("top","rtl", name="rtl")
+    chip.graph("top","apr", name="apr")
+    chip.graph("top","signoff", name="dv")
+    chip.edge("top", "rtl", "apr")
+    chip.edge("top", "apr", "dv")
 
     chip.write_flowgraph("top.png", flow="top")
 
