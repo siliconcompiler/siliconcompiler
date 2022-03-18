e9e1968bc31eb86086cd71ebb9e7a98355c3fed2
Tommaso Marinelli <tommarin@ucm.es>
2022-03-18 21:32:04 +0100

    Add dependencies to KLayout setup script

--- setup/install-klayout.sh
+++ setup/install-klayout.sh
@@ -14,7 +14,13 @@ else
     echo "Script doesn't support Ubuntu version $version."
 fi
 
+# Fetch package
 wget $url
+# Install dependencies
+sudo apt-get install -y libqt5core5a libqt5designer5 libqt5gui5 libqt5multimedia5 \
+       libqt5multimediawidgets5 libqt5network5 libqt5opengl5 libqt5printsupport5 \
+       libqt5sql5 libqt5svg5 libqt5widgets5 libqt5xml5 libqt5xmlpatterns5 libruby2.7
+# Install package
 sudo dpkg -i klayout_${pkg_version}_amd64.deb
 
 echo "Please add \"export QT_QPA_PLATFORM=offscreen\" to your .bashrc"
