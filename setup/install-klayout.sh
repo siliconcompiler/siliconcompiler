64b33a8946470a5269d684a1cfc80ee42584ff2a
Tommaso Marinelli <tommarin@ucm.es>
2022-03-18 19:02:37 +0100

    Fix package version in KLayout setup script

--- setup/install-klayout.sh
+++ setup/install-klayout.sh
@@ -3,18 +3,19 @@
 mkdir -p deps
 cd deps
 
+pkg_version="0.27.8-1"
 version=$(lsb_release -sr)
 
 if [ "$version" = "18.04" ]; then
-    url=https://www.klayout.org/downloads/Ubuntu-18/klayout_0.27.8-1_amd64.deb
+    url=https://www.klayout.org/downloads/Ubuntu-18/klayout_${pkg_version}_amd64.deb
 elif [ "$version" = "20.04" ]; then
-    url=https://www.klayout.org/downloads/Ubuntu-20/klayout_0.27.8-1_amd64.deb
+    url=https://www.klayout.org/downloads/Ubuntu-20/klayout_${pkg_version}_amd64.deb
 else
     echo "Script doesn't support Ubuntu version $version."
 fi
 
 wget $url
-sudo dpkg -i klayout_0.26.12-1_amd64.deb
+sudo dpkg -i klayout_${pkg_version}_amd64.deb
 
 echo "Please add \"export QT_QPA_PLATFORM=offscreen\" to your .bashrc"
 
