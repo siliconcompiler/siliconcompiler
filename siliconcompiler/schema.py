38d8d7942665e72c96195834f49669869c1b4952
aolofsson <andreas@zeroasic.com>
2022-03-19 17:43:06 -0400

    Removing CPU cores from metric -It's not really a metric, just a detail of the platform.

--- siliconcompiler/schema.py
+++ siliconcompiler/schema.py
@@ -1838,20 +1838,6 @@ def schema_metric(cfg, step='default', index='default',group='default'):
             Metric tracking total peak program memory footprint on a per
             step and index basis, specified in bytes.""")
 
-    item = 'cpucores'
-    scparam(cfg, ['metric', step, index, item, group],
-            sctype='int',
-            scope='job',
-            require='asic',
-            shorthelp=f"Metric: {item}",
-            switch=f"-metric_{item} 'step index group <float>'",
-            example=[
-                f"cli: -metric_{item} 'dfm 0 goal 1024'",
-                f"api: chip.set('metric','dfm','0','{item}','real', 1024)"],
-            schelp=f"""
-            Metric tracking the number of machine CPU cores on a per
-            step and index basis.""")
-
     item = 'exetime'
     scparam(cfg, ['metric', step, index, item, group],
             sctype='float',
