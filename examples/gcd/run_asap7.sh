3178487cfe2128aa8c6cb112939b74c99d8fb351
Will <5217539+WRansohoff@users.noreply.github.com>
2022-03-29 15:18:29 -0400

    Fix recent daily test failures

--- examples/gcd/run_asap7.sh
+++ examples/gcd/run_asap7.sh
@@ -10,6 +10,7 @@ sc gcd.v \
    -asic_diearea "(100.13,100.8)" \
    -asic_corearea "(10.07,11.2)" \
    -asic_corearea "(90.25,91)" \
+   -pdk_wafersize "300.0" \
    -loglevel "INFO" \
    -quiet \
    -relax \
