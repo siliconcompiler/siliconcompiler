e3c56e589ff35964c840a5fa3c9047ce1afc2e0e
Noah Moroze <noah@zeroasic.com>
2022-04-10 23:58:20 -0400

    Add some steplist tests

---
+++ tests/flows/test_steplist.py
@@ -0,0 +1,35 @@
+import siliconcompiler
+
+import pytest
+
+@pytest.mark.eda
+def test_steplist(gcd_chip):
+    # Initial run
+    gcd_chip.set('steplist', ['import', 'syn'])
+    gcd_chip.run()
+
+    # Make sure we didn't finish
+    assert gcd_chip.find_result('gds', step='export') is None
+    # Make sure we ran syn
+    assert gcd_chip.find_result('vg', step='syn')
+    assert gcd_chip.get('flowstatus', 'import', '0', 'status') == siliconcompiler.TaskStatus.SUCCESS
+    assert gcd_chip.get('flowstatus', 'syn', '0', 'status') == siliconcompiler.TaskStatus.SUCCESS
+
+    # Re-run
+    gcd_chip.set('steplist', ['syn'])
+    gcd_chip.run()
+    assert gcd_chip.find_result('gds', step='export') is None
+    assert gcd_chip.find_result('vg', step='syn')
+
+    gcd_chip.set('steplist', ['floorplan'])
+    gcd_chip.run()
+    assert gcd_chip.find_result('def', step='floorplan')
+
+@pytest.mark.eda
+def test_invalid(gcd_chip):
+    # Invalid steplist, need to run import first
+    gcd_chip.set('steplist', 'syn')
+
+    with pytest.raises(siliconcompiler.SiliconCompilerError):
+        # Should be caught by check_manifest()
+        gcd_chip.run()
