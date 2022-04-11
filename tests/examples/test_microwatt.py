de5b4536bffd0335d0a2979c9f91458f4dc46990
Noah Moroze <noah@zeroasic.com>
2022-04-11 16:23:29 -0400

    Insert temporary print() to debug microwatt
    Failing on CI only

--- tests/examples/test_microwatt.py
+++ tests/examples/test_microwatt.py
@@ -1,4 +1,5 @@
 import pytest
+import sys
 
 # Only run daily -- this will probably be slowish if we make microwatt example
 # go from end-to-end, and we already have a quick GHDL test.
@@ -9,6 +10,7 @@ def test_py(setup_example_test, microwatt_dir):
     # ensure that the submodule is cloned.
 
     setup_example_test('microwatt')
+    print(sys.path)
 
     import build
     build.main()
