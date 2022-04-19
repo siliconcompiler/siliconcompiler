97dc4fe180ed8a5b7318f5b31660fff5ebfe5d21
aolofsson <andreas@zeroasic.com>
2022-04-18 21:17:28 -0400

    Creating basic skelecton for sup API

--- siliconcompiler/sup.py
+++ siliconcompiler/sup.py
@@ -1,40 +1,151 @@
-class Package:
+import os
+import shutil
+import siliconcompiler
 
-    ###########################################################################
-    def package(self, filename, prune=True):
+class Sup:
+    '''SiliconCompiler Unified Package (SUP) class.
+
+    This is the main object used to interact with the SiliconCompiler
+    Unified Packager API.
+
+     Args:
+       filename: Manifest to package.
+
+    '''
+
+    def __init__(self):
+
+        self.chip = siliconcompiler.Chip()
+
+        if 'SC_CACHE' in os.environ:
+            self.cachedir = os.environ['SC_CACHE']
+        else:
+            self.cachedir = os.path.join(os.environ['HOME'],'.sc','registry')
+
+    ############################################################################
+    def package(self, filename, doc=True, outputs=False, jobs='None'):
         '''
-        Create sanitized project package. (WIP)
+        Create a SUP package based on current chip object.
 
-        The SiliconCompiler project is filtered and exported as a JSON file.
-        If the prune option is set to True, then all metrics, records and
-        results are pruned from the package file.
+        The package is placed in the local cache $SC_HOME/registry. SC_HOME is
+        an environment variable that is set to $HOME/.sc by default.
 
-        Args:
-            filename (filepath): Output filepath
-            prune (bool): If True, only essential source parameters are
-                 included in the package.
+        Directory structure in $SC_HOME/registry.
 
-        Examples:
-            >>> chip.package('package.json')
-            Write project information to 'package.json'
+        <design>
+        └── <version>
+            ├── <jobname>
+            │   ├── import (sources)
+            │   └── export (results)
+            ├── <jobname>
+            │   ├── import
+            │   └── export
+            ├── <docs>/<design>-<version>.{pdf,html,rst}
+            └── <design>-<version>.sup
+
         '''
+
+        # read in json object
+        self.chip.read_manifest(filename, clobber=True)
+
+        # creating internal cache structure
+        design = self.chip.get('design')
+        version = self.chip.get('package', 'version')
+        packdir = os.path.join(self.cachedir, design, version)
+        os.makedirs(packdir, exist_ok=True)
+
+        # Write manifest
+        # TODO: prune
+        self.chip.write_manifest(os.path.join(packdir,f"{design}-{version}.sup"))
 
         return(0)
 
-    ###########################################################################
-    def publish(self, filename):
+    ############################################################################
+    def publish(self, registry=None):
         '''
-        Publishes package to registry. (WIP)
-
-        The filename is uploaed to a central package registry based on the
-        the user credentials found in ~/.sc/credentials.
-
-        Args:
-            filename (filepath): Package filename
-
-        Examples:
-            >>> chip.publish('hello.json')
-            Publish hello.json to central repository.
+        Publishes a package to the named registry.
         '''
+
+        design = self.chip.get('design')
+        version = self.chip.get('package', 'version')
+        packdir = os.path.join(self.cachedir, design, version)
+        ifile = os.path.join(packdir,f"{design}-{version}.sup")
+
+        if registry:
+            regdir=os.path.join(registry, design, version)
+            ofile = os.path.join(regdir,f"{design}-{version}.sup")
+            os.makedirs(regdir, exist_ok=True)
+            shutil.copyfile(ifile, ofile)
+
+        return(0)
+
+    ############################################################################
+    def remove(self, name=None):
+        '''
+        Removes a named package from the the local registry.
+
+        '''
+
+        if name is None:
+            name = self.chip.get('design')
+
+        return(0)
+
+    ############################################################################
+    def list(self):
+        '''
+        List installed packages.
+        '''
+
+        if name is None:
+            name = self.chip.get('design')
+
+        if alldeps:
+            print("fetch all deps")
+        else:
+            print("fetch one package only")
+
+        return(0)
+
+    ############################################################################
+    def install(self, name=None, alldeps=True):
+        '''
+        Fetch dependencies from the network to local registry cache.
+
+        The dependency packages are placed in $SC_HOME/registry.
+
+        If no name is specified, then all
+
+        '''
+
+        if name is None:
+            name = self.chip.get('design')
+
+        if alldeps:
+            print("fetch all deps")
+        else:
+            print("fetch one package only")
+
+        return(0)
+
+    ############################################################################
+    def build(self, name=None, job=None, alldeps=True, clean=True):
+        '''
+        Build the named package and its dependencies.
+
+        - All intermediate files are cleaned up by default.
+        - The order of the build is done bottom up.
+        - Assumes that it's actually possible
+        - Ideally the 'binaries' would be shipped with the package.
+
+        '''
+
+        if name is None:
+            name = self.chip.get('design')
+
+        if alldeps:
+            print("build dep")
+        else:
+            print("build ")
 
         return(0)
