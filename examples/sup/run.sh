41900d14015fbcb44c91982b61c7f07a846fe86d
aolofsson <andreas@zeroasic.com>
2022-04-20 21:15:30 -0400

    Fixing example for new sup api

--- examples/sup/run.sh
+++ examples/sup/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 export PACKAGE='gcd'
-export MANIFEST='gcd/job0/gcd.pkg.json'
+export MANIFEST='build/gcd/job0/gcd.pkg.json'
 export REGISTRY='test_registry'
 
 python siliconcompiler/apps/sup.py check $MANIFEST
 python siliconcompiler/apps/sup.py publish $MANIFEST -registry $REGISTRY
-python siliconcompiler/apps/sup.py show $PACKAGE -registry $REGISTRY
 python siliconcompiler/apps/sup.py index -registry $REGISTRY
 python siliconcompiler/apps/sup.py install $PACKAGE
+python siliconcompiler/apps/sup.py show $PACKAGE
 python siliconcompiler/apps/sup.py list
+python siliconcompiler/apps/sup.py uninstall $PACKAGE
