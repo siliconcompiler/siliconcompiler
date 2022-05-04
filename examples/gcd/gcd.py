2365fda2615e50d401f6c55cb0eb2678fcaaf0a2
aolofsson <andreas@zeroasic.com>
2022-05-03 23:24:38 -0400

    Updating basic gcd.py example for new schema

--- examples/gcd/gcd.py
+++ examples/gcd/gcd.py
@@ -2,15 +2,17 @@
 
 import siliconcompiler
 
-def main():
+def main(root='.'):
     '''Simple asicflow example.'''
 
-    chip = siliconcompiler.Chip(design='gcd', loglevel='INFO')
-    chip.add('source', 'gcd.v')
-    chip.add('constraint', 'gcd.sdc')
-    chip.set('relax', True)
-    chip.set('quiet', True)
-    chip.set('track', True)
+    chip = siliconcompiler.Chip('gcd')
+    chip.add('source', 'verilog', f"{root}/gcd.v")
+    chip.add('source', 'sdc', f"{root}/gcd.sdc")
+    chip.set('option', 'relax', True)
+    chip.set('option', 'quiet', True)
+    chip.set('option', 'track', True)
+    chip.set('option', 'skipcheck', True)
+    chip.set('option', 'novercheck', True)
     chip.set('asic', 'diearea', [(0,0), (100.13,100.8)])
     chip.set('asic', 'corearea', [(10.07,11.2), (90.25,91)])
     chip.load_target("freepdk45_demo")
@@ -18,4 +20,4 @@ def main():
     chip.summary()
 
 if __name__ == '__main__':
-    main()
+    main(root='examples/gcd')
