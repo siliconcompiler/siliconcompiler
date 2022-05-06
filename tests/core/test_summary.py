f1b5489272a182878cd2fb3afef033ef03f34dcc
aolofsson <andreas@zeroasic.com>
2022-05-05 21:33:01 -0400

    Reorg to move tests with run() command into flow tests - Core tests should be python only (no executables)

--- tests/core/test_summary.py
+++ tests/core/test_summary.py
@@ -67,23 +67,6 @@ def test_parallel_path(capfd):
     assert 'place2' not in stdout
     assert 'cts2' not in stdout
 
-@pytest.mark.eda
-def test_steplist_repeat(gcd_chip, capfd):
-    '''Regression test for #458.'''
-    with capfd.disabled():
-        gcd_chip.set('option', 'steplist', ['import', 'syn', 'floorplan'])
-        gcd_chip.run()
-        gcd_chip.set('option', 'steplist', ['syn'])
-        gcd_chip.run()
-
-    gcd_chip.summary()
-    stdout, _ = capfd.readouterr()
-    print(stdout)
-
-    assert 'import0' not in stdout
-    assert 'syn0' in stdout
-    assert 'floorplan0' not in stdout
-
 #########################
 if __name__ == "__main__":
     from tests.fixtures import datadir
