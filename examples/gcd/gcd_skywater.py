81cc27ce5e41bf3d1c19a6e449c1089f03d0ef28
aolofsson <andreas@zeroasic.com>
2022-05-07 11:54:17 -0400

    Partial update of examples

--- examples/gcd/gcd_skywater.py
+++ examples/gcd/gcd_skywater.py
@@ -9,30 +9,26 @@ def main():
     '''GCD example with custom floorplan and signoff steps.'''
 
     # Create instance of Chip class
-    chip = siliconcompiler.Chip()
+    chip = siliconcompiler.Chip("gcd")
 
-    chip.add('source', 'gcd.v')
-    chip.set('design', 'gcd')
-    chip.set('relax', True)
-    chip.set('quiet', True)
-    chip.set('clock', 'core_clock', 'pin', 'clk')
-    chip.set('clock', 'core_clock', 'period', 5)
+    chip.add('source', 'verilog', 'gcd.v')
+    chip.add('source', 'sdc', 'gcd.sdc')
+    chip.set('option', 'relax', True)
+    chip.set('option', 'quiet', True)
 
     chip.load_target("skywater130_demo")
 
-    chip.set('supply', 'vdd', 'pin', 'vdd')
-    # level unimportant, just needs to be >0 to indicate power
-    chip.set('supply', 'vdd', 'level', 1)
-
-    chip.set('supply', 'vss', 'pin', 'vss')
-    chip.set('supply', 'vss', 'level', 0)
+    #chip.set('supply', 'vdd', 'pin', 'vdd')
+    #chip.set('supply', 'vdd', 'level', 1)
+    #chip.set('supply', 'vss', 'pin', 'vss')
+    #chip.set('supply', 'vss', 'level', 0)
 
     # 1) RTL2GDS
 
     def_path = make_floorplan(chip)
-    chip.set('read', 'def', 'floorplan', '0', def_path)
+    chip.set('source', 'def', def_path)
 
-    chip.set('jobname', 'rtl2gds')
+    chip.set('option', 'jobname', 'rtl2gds')
     chip.run()
     chip.summary()
 
@@ -44,9 +40,8 @@ def main():
     chip.set('jobname', 'signoff')
     chip.set('flow', 'signoffflow')
 
-    chip.set('read', 'gds', 'drc', '0', gds_path)
-    chip.set('read', 'gds', 'extspice', '0', gds_path)
-    chip.set('read', 'netlist', 'lvs', '0', vg_path)
+    chip.set('source', 'gds', gds_path)
+    chip.set('source', 'netlist', vg_path)
 
     chip.run()
     chip.summary()
--- examples/gcd/run_asap7.sh
+++ examples/gcd/run_asap7.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-OPT=$1
-
-echo $OPT
-sc gcd.v \
-   -target "asap7_demo" \
-   -constraint "gcd.sdc" \
+sc -design gcd \
+   -source "verilog gcd.v" \
+   -source "sdc gcd.sdc" \
+   -package_version "0.0.0" \
+   -package_description "GCD test package" \
+   -package_license "MIT" \
+   -target "arap7_demo" \
    -asic_diearea "(0,0)" \
    -asic_diearea "(100.13,100.8)" \
    -asic_corearea "(10.07,11.2)" \
    -asic_corearea "(90.25,91)" \
    -loglevel "INFO" \
+   -novercheck \
    -quiet \
    -relax \
-   -design gcd $OPT
+   -track \
+   -clean \
