a044520f0a4f858f96a59c82b3b371f485cd1ef7
Noah Moroze <noah@zeroasic.com>
2022-05-10 18:54:45 -0400

    Fix up test_read_manifest.py
    - Update old keypaths
    - Update defaults.json for myriad of changes

--- tests/core/test_read_manifest.py
+++ tests/core/test_read_manifest.py
@@ -9,26 +9,26 @@ def test_read_manifest_fields():
     '''Ensure that changes to fields other than 'value' are reflected by read_manifest()'''
 
     chip = siliconcompiler.Chip('foo')
-    chip.set('source', 'verilog', False, field='copy')
-    chip.add('source', 'verilog', 'foo.v')
+    chip.set('input', 'verilog', False, field='copy')
+    chip.add('input', 'verilog', 'foo.v')
     chip.write_manifest('tmp.json')
 
     # fresh chip, so we don't retain anything from `chip` in-memory
     chip2 = siliconcompiler.Chip('foo')
     chip2.read_manifest('tmp.json')
-    assert chip2.get('source', 'verilog', field='copy') is False
+    assert chip2.get('input', 'verilog', field='copy') is False
 
 
 def test_read_sup():
     '''Test compressed read/write'''
 
     chip = siliconcompiler.Chip('foo')
-    chip.add('source', 'verilog', 'foo.v')
+    chip.add('input', 'verilog', 'foo.v')
     chip.write_manifest('tmp.sup.gz')
 
     chip2 = siliconcompiler.Chip('foo')
     chip2.read_manifest('tmp.sup.gz')
-    assert chip2.get('source','verilog') == ['foo.v']
+    assert chip2.get('input','verilog') == ['foo.v']
 
 
 def test_read_defaults(datadir):
