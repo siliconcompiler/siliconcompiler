a0cabc21294590c31ae426689cf190ba03951ff0
Noah Moroze <noah@zeroasic.com>
2022-05-12 14:52:28 -0400

    Use argparse for -version CLI arg handling
    Ensures that -version option is documented with -h/--help.

--- siliconcompiler/core.py
+++ siliconcompiler/core.py
@@ -325,11 +325,7 @@ class Chip:
             else:
                 scargs.append(item)
 
-
-        # exit on version check
-        if '-version' in scargs:
-            print(_metadata.version)
-            sys.exit(0)
+        parser.add_argument('-version', action='version', version=_metadata.version)
 
         #Grab argument from pre-process sysargs
         #print(scargs)
