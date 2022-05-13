2234f4e5a203b5cf0352c8a430e66de38e5d08fe
Noah Moroze <noah@zeroasic.com>
2022-05-13 15:40:07 -0400

    Skip Microwatt test: it causes CI to time out

--- tests/examples/test_microwatt.py
+++ tests/examples/test_microwatt.py
@@ -5,6 +5,7 @@ import sys
 # go from end-to-end, and we already have a quick GHDL test.
 
 @pytest.mark.eda
+@pytest.mark.skip(reason='Slow (takes half hour to run)')
 def test_py(setup_example_test, microwatt_dir):
     # Note: value of microwatt_dir is unused, but specifying it is important to
     # ensure that the submodule is cloned.
