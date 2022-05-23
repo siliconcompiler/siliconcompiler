c85406b5710c500d8e02c855237dd2ead2e0101d
Kimia <kimia.talaei97@gmail.com>
2022-05-23 18:26:53 +0000

    Add synthesis script to replace the synthesis flow in the tcl script

--- siliconcompiler/tools/yosys/syn_fpga.tcl
+++ siliconcompiler/tools/yosys/syn_fpga.tcl
@@ -9,24 +9,5 @@ yosys hierarchy -top $sc_design
 if {[string match {ice*} $sc_partname]} {
     yosys synth_ice40 -top $sc_design -json "${sc_design}_netlist.json"
 } else {
-
-    set lutsize 3
-    set output_blif "outputs/$topmodule.blif"
- 
-    yosys proc
-    
-    yosys techmap -map +/adff2dff.v
-    yosys techmap
-
-    # LUT mapping
-    yosys abc -lut $lutsize
-
-    #Flatten the design
-    yosys flatten
-
-    # Check
-    yosys synth -run check
-
-    # Clean and output blifver      
-    yosys opt_clean -purge
+    yosys script "/home/kimia/siliconcompiler/siliconcompiler/tools/yosys/synthesis.ys"
 }
