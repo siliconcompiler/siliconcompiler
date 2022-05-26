7c63859ebda3a519db318f3105862d46621e36cd
Noah Moroze <noah@zeroasic.com>
2022-05-26 17:08:29 -0400

    Make _print_tcl() more robust
    - Use {} to escape strings, so that we can have spaces and newlines
    - Dump tuples as lists to avoid parsing
    - Make sure envvar syntax gets subbed in anywhere in string

--- siliconcompiler/core.py
+++ siliconcompiler/core.py
@@ -1473,6 +1473,38 @@ class Chip:
                 fout.write(f"{keypath},{value}")
 
     ###########################################################################
+    def _escape_val_tcl(self, val, typestr):
+        '''Recursive helper function for converting Python values to safe TCL
+        values, based on the SC type string.'''
+        if val is None:
+            return ''
+        elif typestr.startswith('('):
+            subtypes = typestr.strip('()').split(',')
+            valstr = ' '.join(self._escape_val_tcl(v, subtype.strip())
+                              for v, subtype in zip(val, subtypes))
+            return f'[list {valstr}]'
+        elif typestr.startswith('['):
+            subtype = typestr.strip('[]')
+            valstr = ' '.join(self._escape_val_tcl(v, subtype) for v in val)
+            return f'[list {valstr}]'
+        elif typestr == 'bool':
+            return 'true' if val else 'false'
+        elif typestr == 'str':
+            # Surrounding a string with '{}' ensures that special characters
+            # inside the string are treated as-is.
+            # Source: https://datacadamia.com/lang/tcl/special_character#about
+            escaped_val = val.replace('{', '\\{').replace('}', '\\}')
+            return '{' + escaped_val + '}'
+        elif typestr in ('file', 'dir'):
+            # Replace $VAR with $env(VAR) for tcl
+            # Surround with quotes to escape whitespace. We don't want to use {}
+            # here, since that will break envvar expansion.
+            return '"' + re.sub(r'\$(\w+)', r'$env(\1)', val) + '"'
+        else:
+            # floats/ints just become strings
+            return str(val)
+
+    ###########################################################################
     def _print_tcl(self, cfg, fout=None, prefix=""):
         '''
         Prints out schema as TCL dictionary
@@ -1487,29 +1519,17 @@ class Chip:
         for key in allkeys:
             typestr = self.get(*key, cfg=cfg, field='type')
             value = self.get(*key, cfg=cfg)
-            # everything becomes a list
-            # convert None to empty list
-            if value is None:
-                alist = []
-            elif bool(re.match(r'\[', typestr)):
-                alist = value
-            elif typestr == "bool" and value:
-                alist = ["true"]
-            elif typestr == "bool" and not value:
-                alist = ["false"]
-            else:
-                alist = [value]
-
-            #replace $VAR with env(VAR) for tcl
-            for i, val in enumerate(alist):
-                m = re.match(r'\$(\w+)(.*)', str(val))
-                if m:
-                    alist[i] = ('$env(' + m.group(1) + ')' + m.group(2))
 
             #create a TCL dict
             keystr = ' '.join(key)
-            valstr = ' '.join(map(str, alist)).replace(';', '\\;')
-            outstr = f"{prefix} {keystr} [list {valstr}]\n"
+
+            valstr = self._escape_val_tcl(value, typestr)
+
+            if not (typestr.startswith('[') or typestr.startswith('(')):
+                # treat scalars as lists as well
+                valstr = f'[list {valstr}]'
+
+            outstr = f"{prefix} {keystr} {valstr}\n"
 
             #print out all non default values
             if 'default' not in key:
--- tests/core/test_write_manifest.py
+++ tests/core/test_write_manifest.py
@@ -1,4 +1,6 @@
 # Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import pytest
+
 import siliconcompiler
 
 def test_write_manifest():
@@ -14,6 +16,55 @@ def test_write_manifest():
     chip.write_manifest('top.csv')
     chip.write_manifest('top.tcl', prune=False)
     chip.write_manifest('top.yaml')
+
+def test_advanced_tcl(monkeypatch):
+    # Tkinter module is part of Python standard library, but may not be
+    # available depending on if the system has the python3-tk installed. This
+    # line will import tkinter if it's available, and skip the test otherwise.
+    tkinter = pytest.importorskip('tkinter')
+
+    chip = siliconcompiler.Chip('top')
+
+    # Test complex strings
+    desc = '''This description is potentially problematic since it includes
+multiple lines, spaces, and TCL special characters. This package costs $5 {for real}!'''
+    chip.set('package', 'description', desc)
+
+    # Test tuples
+    chip.add('asic', 'diearea', (0, 0))
+    chip.add('asic', 'diearea', (30, 40))
+
+    # Test bools
+    chip.set('option', 'quiet', True)
+
+    # Test envvars
+    chip.set('input', 'verilog', 'rtl/$TOPMOD.v')
+
+    chip.write_manifest('top.tcl')
+
+    # Read from config in TCL as test
+    tcl = tkinter.Tcl()
+
+    # Set env var to test ['input', 'verilog']
+    monkeypatch.setenv('TOPMOD', 'design')
+
+    def tcl_eval(expr):
+        script = f'''
+        source top.tcl
+        return {expr}'''
+        return tcl.eval(script)
+
+    # When we call puts on a multiline string, it does get mangled a bit. It
+    # stays surrounded by {}, and {} within the string are still escaped.
+    # TODO: is this problematic? I think it is okay for now since we don't
+    # really read these strings within TCL, they just need to not break sourcing
+    # the manifest.
+    expected_desc = '{' + desc.replace('{', '\\{').replace('}', '\\}') + '}'
+    assert tcl_eval('[dict get $sc_cfg package description]') == expected_desc
+
+    assert tcl_eval('[lindex [lindex [dict get $sc_cfg asic diearea] 1] 0]') == '30.0'
+    assert tcl_eval('[dict get $sc_cfg option quiet]') == 'true'
+    assert tcl_eval('[dict get $sc_cfg input verilog]') == 'rtl/design.v'
 
 #########################
 if __name__ == "__main__":
