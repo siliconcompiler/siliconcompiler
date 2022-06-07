0be3004543e92e8370b98b75e2ddf3324ce8039f
Noah Moroze <noah@zeroasic.com>
2022-06-07 17:24:56 -0400

    Add collapsable manifest tree view to SC report

--- siliconcompiler/core.py
+++ siliconcompiler/core.py
@@ -3147,9 +3147,13 @@ class Chip:
             env = Environment(loader=FileSystemLoader(templ_dir))
             results_page = os.path.join(web_dir, 'report.html')
             results_gds = self.find_result('gds', step='export')
+            pruned_cfg = self._prune(self.cfg)
+            del pruned_cfg['history']
+            del pruned_cfg['library']
             with open(results_page, 'w') as wf:
                 wf.write(env.get_template('sc_report.j2').render(
                     manifest = self.cfg,
+                    pruned_cfg = pruned_cfg,
                     metric_keys = metric_list,
                     metrics = self.cfg['metric'],
                     tasks = flow_tasks,
