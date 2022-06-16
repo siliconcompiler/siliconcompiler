0d5200e6069711f773c3a124108df69b1df389e8
Kimia <kimia.talaei97@gmail.com>
2022-06-16 04:28:17 +0000

    Moving the Arch class to utils.py

--- siliconcompiler/tools/yosys/yosys.py
+++ siliconcompiler/tools/yosys/yosys.py
@@ -2,8 +2,7 @@ import os
 import re
 import sys
 import shutil
-import xml.etree.ElementTree as ET
-
+import importlib
 from jinja2 import Template
 import siliconcompiler
 
@@ -71,7 +70,6 @@ def setup(chip):
         # represent a set of possible inputs where you must pick one?
         chip.set('tool', tool, 'input', step, index, chip.design + '.v')
         chip.set('tool', tool, 'output', step, index, chip.design + '.vg')
-        chip.add('tool', tool, 'output', step, index, "/home/kimia/vpr_example/adder.v")
         chip.add('tool', tool, 'output', step, index, chip.design + '_netlist.json')
         chip.add('tool', tool, 'output', step, index, chip.design + '.blif')
     elif step == 'lec':
@@ -198,6 +196,11 @@ def create_vpr_lib(chip):
     if os.path.exists(dst):
         shutil.rmtree(dst)
     shutil.copytree(src, dst)
+    spec = importlib.util.spec_from_file_location("utils", f"{chip.scroot}/utils.py")
+    imported = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(imported)
+
+    Arch = getattr(imported, 'Arch')
     arch = Arch(chip.get('fpga', 'arch')[0])
     max_lut_size = arch.find_max_lut_size()
     max_mem_addr_width = arch.find_memory_addr_width()
@@ -226,77 +229,5 @@ if __name__ == "__main__":
     chip = make_docs()
     chip.write_manifest("yosys.json")
 
-
-class PbPrimitive:
-     
-    def __init__ (self, name, blif_model):
-        self.name = name
-        self.blif_model = blif_model
-        self.ports = []    
-        
-    def add_port(self, port):
-        
-        port_type = port.tag
-        port_name = port.attrib['name']
-        num_pins = port.attrib['num_pins']
-        port_class = port.attrib.get('port_class')
-        
-        new_port = { 'port_type': port_type,
-                     'port_name': port_name,
-                     'num_pins': num_pins,
-                     'port_class': port_class }
-        
-        self.ports.append(new_port)
-        
-    def find_port(self, port_name):
-        print(port_name)
-        for port in self.ports:
-            print(port['port_name'] + "---------------------------------------------------" + port_name)
-            print(re.match(port_name, port['port_name']))
-            if re.match(port_name, port['port_name']):
-                return port
-        return None
-    
-class Arch:
-    
-    def __init__(self, arch_file_name):
-        self.arch_file = ET.parse(arch_file_name)
-        self.complexblocklist = self.arch_file.find("complexblocklist")
-        self.pb_primitives = []
-        self.find_pb_primitives(self.complexblocklist)
-    
-    def find_pb_primitives(self, root):  
-        for pb_type in root.iter('pb_type'):
-            if "blif_model" in pb_type.attrib:
-                self.add_pb_primitive(pb_type)
-                
-    def add_pb_primitive(self, pb_type):
-        new_pb = PbPrimitive(pb_type.tag, pb_type.attrib["blif_model"])
-        for port in pb_type.iter():
-            if port.tag in ['input', 'output', 'clock']:
-                new_pb.add_port(port)
-        self.pb_primitives.append(new_pb)
-        
-    def find_max_lut_size(self):
-        max_lut_size = 0
-        for pb_type in self.pb_primitives:
-            if pb_type.blif_model == ".names":
-                in_port = pb_type.find_port("in")
-                lut_size = in_port["num_pins"]
-                max_lut_size = max(max_lut_size, int(lut_size))
-                
-        return (max_lut_size)
-    
-    def find_memory_addr_width(self):
-        max_add_size = 0
-        for pb_type in self.pb_primitives:
-            print(pb_type.blif_model + " ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-            if "port_ram" in pb_type.blif_model:
-                print("###############################################")
-                add_port = pb_type.find_port("^addr")
-                add_size = add_port["num_pins"]
-                max_add_size = max(max_add_size, int(add_size))
-                
-        return (max_add_size)
-        
+       
         
