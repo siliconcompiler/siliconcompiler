c0cfc413ce155948391805be84a3e670d9363c92
Noah Moroze <noah@zeroasic.com>
2022-07-20 15:04:36 -0400

    Make Surelog install script consistent w/ OpenROAD
    - sudo on make install
    - same trick to make it runnable from anywhere

--- setup/install-surelog.sh
+++ setup/install-surelog.sh
@@ -1,6 +1,12 @@
+#!/bin/sh
+
+# Get directory of script
+src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
+
 sudo apt-get install -y build-essential cmake git pkg-config tclsh swig uuid-dev libgoogle-perftools-dev python3 python3-dev
 sudo apt-get install -y default-jre
 
+cd ${src_path}/..
 git submodule update --init --recursive third_party/tools/surelog
 cd third_party/tools/surelog
 
@@ -12,5 +18,5 @@ git config --file $(pwd)/.gitconfig url."https://".insteadOf git://
 export HOME=$(pwd)
 
 make
-make install
+sudo make install
 cd -
