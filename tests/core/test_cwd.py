c7bad16c33e1bf75e585dda52fc4d0802dc3a3b3
Will <5217539+WRansohoff@users.noreply.github.com>
2022-10-04 21:29:39 -0400

    Skip nonexistant working directory test on Windows

--- tests/core/test_cwd.py
+++ tests/core/test_cwd.py
@@ -5,6 +5,7 @@ import shutil
 import siliconcompiler
 
 @pytest.mark.quick
+@pytest.mark.skipif(sys.platform=='win32', reason='Windows throws a permission denied error if we try to delete a directory which a process is currently inhabiting.')
 def test_cwd():
     os.mkdir('tmp_test_cwd')
     os.chdir('tmp_test_cwd')
