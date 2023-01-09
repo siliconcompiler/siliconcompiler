164981423ec5399401bbc72dbca798c2fa2b3056
Peter Gadfort <gadfort@zeroasic.com>
2023-01-09 10:17:45 -0500

    tool: update OpenROAD tool to better mirror OpenROAD-flow-scripts flow and handle all metrics collected by OpenROAD

--- siliconcompiler/tools/openroad/sc_dfm.tcl
+++ siliconcompiler/tools/openroad/sc_dfm.tcl
@@ -1,6 +1,19 @@
-
 ######################
-# Place holder for fill
+# Do fill
 ######################
 
-#density_fill -rules "fill.json'
+set removed_obs 0
+foreach obstruction [[ord::get_db_block] getObstructions] {
+  odb::dbObstruction_destroy $obstruction
+  incr removed_obs
+}
+utl::info FLW 1 "Deleted $removed_obs routing obstructions"
+
+if { $openroad_fin_add_fill == "True" && \
+     [dict exists $sc_cfg pdk $sc_pdk aprtech openroad $sc_stackup $sc_libtype fill] } {
+  set sc_fillrules [lindex [dict get $sc_cfg pdk $sc_pdk aprtech openroad $sc_stackup $sc_libtype fill] 0]
+  density_fill -rules $sc_fillrules
+}
+
+# estimate for metrics
+estimate_parasitics -global_routing
