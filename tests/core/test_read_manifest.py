cfed8726486bff8eae77627c058daa769344c4c6
aolofsson <andreas@zeroasic.com>
2023-01-08 21:53:11 -0500

    Updating core tests for new input/output schema

--- tests/core/test_read_manifest.py
+++ tests/core/test_read_manifest.py
@@ -11,26 +11,26 @@ def test_read_manifest_fields():
     '''Ensure that changes to fields other than 'value' are reflected by read_manifest()'''
 
     chip = siliconcompiler.Chip('foo')
-    chip.set('input', 'verilog', False, field='copy')
-    chip.add('input', 'verilog', 'foo.v')
+    chip.set('input', 'rtl', 'verilog', False, field='copy')
+    chip.add('input', 'rtl', 'verilog', 'foo.v')
     chip.write_manifest('tmp.json')
 
     # fresh chip, so we don't retain anything from `chip` in-memory
     chip2 = siliconcompiler.Chip('foo')
     chip2.read_manifest('tmp.json')
-    assert chip2.get('input', 'verilog', field='copy') is False
+    assert chip2.get('input', 'rtl', 'verilog', field='copy') is False
 
 
 def test_read_sup():
     '''Test compressed read/write'''
 
     chip = siliconcompiler.Chip('foo')
-    chip.add('input', 'verilog', 'foo.v')
+    chip.add('input', 'rtl', 'verilog', 'foo.v')
     chip.write_manifest('tmp.sup.gz')
 
     chip2 = siliconcompiler.Chip('foo')
     chip2.read_manifest('tmp.sup.gz')
-    assert chip2.get('input','verilog') == ['foo.v']
+    assert chip2.get('input', 'rtl', 'verilog') == ['foo.v']
 
 def test_read_defaults(datadir):
     '''Make sure read/write operaton doesn't modify manifest'''
