b63c5ad3b8157da87b0a1cb619fa6dde1cef2896
Peter Gadfort <gadfort@zeroasic.com>
2023-01-12 13:04:05 -0500

    update surelog script to pull via git

--- setup/install-surelog.sh
+++ setup/install-surelog.sh
@@ -6,10 +6,14 @@ src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
 sudo apt-get install -y build-essential cmake git pkg-config tclsh swig uuid-dev libgoogle-perftools-dev python3 python3-dev
 sudo apt-get install -y default-jre
 
-cd ${src_path}/..
-git submodule update --init --recursive third_party/tools/surelog
-cd third_party/tools/surelog
+mkdir -p deps
+cd deps
+
+git clone https://github.com/chipsalliance/Surelog.git
+cd Surelog
+git checkout ad83eedc7de32ec15a3f7b4e271c6b45ddf547eb
 
 make
 sudo make install
+
 cd -
