4e0f7cc2e5787cad09480ac00d67819df5ea39f9
Peter Gadfort <gadfort@zeroasic.com>
2023-01-16 14:10:34 -0500

    add klayout to _tools

--- setup/install-klayout.sh
+++ setup/install-klayout.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Get directory of script
+src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
+
 mkdir -p deps
 cd deps
 
-pkg_version="0.27.8-1"
+pkg_version=$(python3 ${src_path}/_tools.py --tool klayout --field version)
 version=$(lsb_release -sr)
 
 if [ "$version" = "18.04" ]; then
@@ -17,12 +20,12 @@ else
 fi
 
 # Fetch package
-wget $url
+wget -O klayout.deb $url
 # Install dependencies
 sudo apt-get install -y libqt5core5a libqt5designer5 libqt5gui5 libqt5multimedia5 \
        libqt5multimediawidgets5 libqt5network5 libqt5opengl5 libqt5printsupport5 \
        libqt5sql5 libqt5svg5 libqt5widgets5 libqt5xml5 libqt5xmlpatterns5 libruby2.7
 # Install package
-sudo dpkg -i klayout_${pkg_version}_amd64.deb
+sudo dpkg -i klayout.deb
 
 cd -
