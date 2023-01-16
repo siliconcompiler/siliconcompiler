3dc0fa6228dc0daa0a17191e6798f54de6516fab
Peter Gadfort <gadfort@zeroasic.com>
2023-01-16 13:46:47 -0500

    add netgen to _tools

--- setup/install-netgen.sh
+++ setup/install-netgen.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+# Get directory of script
+src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
+
 sudo apt-get install -y build-essential tcl-dev tk-dev
 
 mkdir -p deps
 cd deps
 
-git clone https://github.com/RTimothyEdwards/netgen.git
+git clone $(python3 ${src_path}/_tools.py --tool netgen --field git-url) netgen
 cd netgen
-
-git checkout 1.5.210
+git checkout $(python3 ${src_path}/_tools.py --tool netgen --field git-commit)
 
 ./configure
 make
