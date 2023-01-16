682ebd93c6275e5d71d0afa7a82c0c0fef527ebe
Peter Gadfort <gadfort@zeroasic.com>
2023-01-16 14:15:56 -0500

    add sv2v to _tools

--- setup/install-sv2v-pkg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p deps
-cd deps
-wget -q https://github.com/zachjs/sv2v/releases/download/v0.0.9/sv2v-Linux.zip
-unzip sv2v-Linux
-sudo mv sv2v-Linux/sv2v /usr/bin
-rm -rf sv2v-Linux
