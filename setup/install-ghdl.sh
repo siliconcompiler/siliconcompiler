3dc89b070c82161ddc270e75696aa2765e8ac91b
Peter Gadfort <gadfort@zeroasic.com>
2023-01-16 13:51:03 -0500

    add ghdl to _tools

--- setup/install-ghdl.sh
+++ setup/install-ghdl.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# Get directory of script
+src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
+
 sudo apt-get install -y gnat libgnat-7
 
 mkdir -p deps
 cd deps
-git clone https://github.com/ghdl/ghdl.git
+
+git clone $(python3 ${src_path}/_tools.py --tool ghdl --field git-url) ghdl
 cd ghdl
+git checkout $(python3 ${src_path}/_tools.py --tool ghdl --field git-commit)
 
 ./configure
 make
