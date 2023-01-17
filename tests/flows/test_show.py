fa1609c66e13bb4d62c253b61b9eee694d09b447
aolofsson <andreas@zeroasic.com>
2023-01-16 20:41:20 -0500

    Updating show test for task/tool schema change

--- tests/flows/test_show.py
+++ tests/flows/test_show.py
@@ -15,7 +15,7 @@ def adjust_exe_options(chip, headless):
     for step in ('show', 'screenshot'):
         # adjust options to ensure programs exit
         for tool in ('klayout', 'openroad'):
-            chip.set('tool', tool, 'var', step, '0', 'show_exit', 'true')
+            chip.set('tool', tool, 'task', step, 'var', step, '0', 'show_exit', 'true')
 
 @pytest.fixture
 def display():
