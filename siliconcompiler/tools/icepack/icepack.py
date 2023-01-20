bfdb9e80c9e9e2d1a386cdc021393ffd2b728994
Will <5217539+WRansohoff@users.noreply.github.com>
2023-01-19 19:04:00 -0500

    split nextpnr and icepack

--- siliconcompiler/tools/icepack/icepack.py
+++ siliconcompiler/tools/icepack/icepack.py
@@ -25,28 +25,6 @@ def make_docs():
 
 
 ################################
-# Setup Tool (pre executable)
-################################
-
-def setup(chip):
-    ''' Sets up default settings on a per step basis
-    '''
-    tool = 'icepack'
-    step = chip.get('arg','step')
-    index = chip.get('arg','index')
-    #TODO: fix below
-    task = step
-
-    clobber = False
-    design = chip.top()
-
-    chip.set('tool', tool, 'exe', tool)
-
-    chip.set('tool', tool, 'task', task, 'option', step, index, "", clobber=clobber)
-    chip.set('tool', tool, 'task', task, 'input', step, index, f'{design}.asc')
-    chip.set('tool', tool, 'task', task, 'output', step, index, f'{design}.bit')
-
-################################
 #  Custom runtime options
 ################################
 
