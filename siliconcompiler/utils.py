9473015b0f114c4e799a0eca2c29d148d38b2df0
Peter Gadfort <gadfort@zeroasic.com>
2023-01-24 13:53:39 -0500

    fix typo in bvs->bsv

--- siliconcompiler/utils.py
+++ siliconcompiler/utils.py
@@ -252,7 +252,7 @@ def get_default_iomap():
     # Build default map with fileset and type
     default_iomap = {}
     default_iomap.update({ext: ('hll', 'c') for ext in hll_c})
-    default_iomap.update({ext: ('hll', 'bvs') for ext in hll_bsv})
+    default_iomap.update({ext: ('hll', 'bsv') for ext in hll_bsv})
     default_iomap.update({ext: ('hll', 'scala') for ext in hll_scala})
     default_iomap.update({ext: ('hll', 'python') for ext in hll_python})
 
