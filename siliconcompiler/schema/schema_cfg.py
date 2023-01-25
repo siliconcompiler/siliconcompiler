2eac54a26f062c5c0d0a3f3a06e9dc1033b9960f
Noah Moroze <noah@zeroasic.com>
2023-01-25 13:47:26 -0500

    schema: bump version and update defaults.json

--- siliconcompiler/schema/schema_cfg.py
+++ siliconcompiler/schema/schema_cfg.py
@@ -6,7 +6,7 @@ import re
 
 from siliconcompiler import utils
 
-SCHEMA_VERSION = '0.14.0'
+SCHEMA_VERSION = '0.15.0'
 
 #############################################################################
 # PARAM DEFINITION
