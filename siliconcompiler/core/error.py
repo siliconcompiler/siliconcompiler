c870db0ba1544dbf031a2684145756f337a5098b
Will <5217539+WRansohoff@users.noreply.github.com>
2023-01-30 20:48:50 -0500

    SiliconCompilerError shouldn't require arguments in its call to 'super', because we don't support Python2.

--- siliconcompiler/core/error.py
+++ siliconcompiler/core/error.py
@@ -3,4 +3,4 @@ class SiliconCompilerError(Exception):
     ''' Minimal Exception wrapper used to raise sc runtime errors.
     '''
     def __init__(self, message):
-        super(Exception, self).__init__(message)
+        super().__init__(message)
