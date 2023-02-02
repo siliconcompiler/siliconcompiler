2f791223c0cceeb190eae0335bd1ce3719915a3d
Peter Gadfort <gadfort@zeroasic.com>
2023-02-02 16:59:27 -0500

    switch checklists to use Checklist()

--- siliconcompiler/checklists/oh_tapeout/oh_tapeout.py
+++ siliconcompiler/checklists/oh_tapeout/oh_tapeout.py
@@ -1,35 +1,40 @@
 import siliconcompiler
 
+from siliconcompiler import Checklist
+
 def make_docs():
     '''Subset of OH! library tapeout checklist.
 
     https://github.com/aolofsson/oh/blob/master/docs/tapeout_checklist.md
     '''
     chip = siliconcompiler.Chip()
-    setup(chip)
-    return chip
+    return setup(chip)
 
 def setup(chip):
     standard = 'oh_tapeout'
 
+    checklist = Checklist(chip, standard)
+
     # Automated
-    chip.set('checklist', standard, 'drc_clean', 'description',
-             'Is block DRC clean?')
-    chip.set('checklist', standard, 'drc_clean', 'criteria', 'drvs==0')
+    checklist.set('checklist', standard, 'drc_clean', 'description',
+                  'Is block DRC clean?')
+    checklist.set('checklist', standard, 'drc_clean', 'criteria', 'drvs==0')
 
-    chip.set('checklist', standard, 'lvs_clean', 'description',
-             'Is block LVS clean?')
-    chip.set('checklist', standard, 'lvs_clean', 'criteria', 'drvs==0')
+    checklist.set('checklist', standard, 'lvs_clean', 'description',
+                  'Is block LVS clean?')
+    checklist.set('checklist', standard, 'lvs_clean', 'criteria', 'drvs==0')
 
-    chip.set('checklist', standard, 'setup_time', 'description',
-             'Setup time met?')
-    chip.set('checklist', standard, 'setup_time', 'criteria', 'setupslack>=0')
+    checklist.set('checklist', standard, 'setup_time', 'description',
+                  'Setup time met?')
+    checklist.set('checklist', standard, 'setup_time', 'criteria', 'setupslack>=0')
 
-    chip.set('checklist', standard, 'errors_warnings', 'description',
-             'Are all EDA warnings/errors acceptable?')
-    chip.set('checklist', standard, 'errors_warnings', 'criteria',
-              ['errors==0', 'warnings==0'])
+    checklist.set('checklist', standard, 'errors_warnings', 'description',
+                  'Are all EDA warnings/errors acceptable?')
+    checklist.set('checklist', standard, 'errors_warnings', 'criteria',
+                  ['errors==0', 'warnings==0'])
 
     # Manual
-    chip.set('checklist', standard, 'spec', 'description',
-        'Is there a written specification?')
+    checklist.set('checklist', standard, 'spec', 'description',
+                  'Is there a written specification?')
+
+    return checklist
