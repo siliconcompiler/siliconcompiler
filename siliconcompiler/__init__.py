f7f01ff34c01829be60021e93bef214fcdba6192
Peter Gadfort <gadfort@zeroasic.com>
2023-02-03 01:30:12 -0500

    cleanup __init__

--- siliconcompiler/__init__.py
+++ siliconcompiler/__init__.py
@@ -1,11 +1,5 @@
 from siliconcompiler.core import *
-#from siliconcompiler.floorplan import *
-#from siliconcompiler.leflib import *
-#from siliconcompiler.deflib import *
 
 from siliconcompiler._metadata import version as __version__
 
-from siliconcompiler.use import PDK
-from siliconcompiler.use import Library
-from siliconcompiler.use import Flow
-from siliconcompiler.use import Checklist
+from siliconcompiler.use import PDK, Library, Flow, Checklist
