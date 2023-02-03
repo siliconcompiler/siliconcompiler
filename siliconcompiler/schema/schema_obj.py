0800e6b8a74fccd84f7bcb2f9139d64257546c00
Will <5217539+WRansohoff@users.noreply.github.com>
2023-02-03 17:13:16 -0500

    'defvaule' could be None if a Schema with a default value is written out with prune=True and then re-read.

--- siliconcompiler/schema/schema_obj.py
+++ siliconcompiler/schema/schema_obj.py
@@ -98,6 +98,8 @@ class Schema:
                 return cfg['nodevalue'][step]['default']
             elif Schema._is_set(cfg):
                 return cfg['value']
+            elif ('value' in cfg) and (cfg['value']):
+                return cfg['value']
             else:
                 return cfg['defvalue']
         elif field in cfg:
