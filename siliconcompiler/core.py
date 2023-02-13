93748f90b095820d4c1c002efdee3732f4436296
Noah Moroze <noah@zeroasic.com>
2023-02-13 15:41:52 -0500

    core: fix up logic in _find_showable_output()

--- siliconcompiler/core.py
+++ siliconcompiler/core.py
@@ -4305,15 +4305,16 @@ If you are sure that your working directory is valid, try running `cd $(pwd)`.""
         views (and we probably want to prioritize final layouts like
         DEF/GDS/OAS).
         '''
-        step = chip.get('arg', 'step')
-        index = chip.get('arg', 'index')
+        step = self.get('arg', 'step')
+        index = self.get('arg', 'index')
 
-        for key in self.getkeys('output'):
-            for output in self.find_files('output', key, step=step, index=index):
-                file_ext = utils.get_file_ext(output)
-                if file_ext in self.getkeys('option', 'showtool'):
-                    if not tool or self.get('option', 'showtool', file_ext) == tool:
-                        return output
+        for fileset in self.getkeys('output'):
+            for filetype in self.getkeys('output', fileset):
+                for output in self.find_files('output', fileset, filetype, step=step, index=index):
+                    file_ext = utils.get_file_ext(output)
+                    if file_ext in self.getkeys('option', 'showtool'):
+                        if not tool or self.get('option', 'showtool', file_ext) == tool:
+                            return output
         return None
 
     ###########################################################################
