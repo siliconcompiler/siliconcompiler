aa15870ad55aeb9e4d31d8eb3c5c61ffa776f660
Peter Gadfort <gadfort@zeroasic.com>
2023-02-28 16:34:41 -0500

    ensure workflow scripts point at the base siliconcompiler directory

--- .github/workflows/bin/install_surelog_macos.sh
+++ .github/workflows/bin/install_surelog_macos.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Get directory of setup scripts
+src_path=$(cd -- "$(dirname "$0")/../../../" >/dev/null 2>&1 ; pwd -P)
+
 # Install dependencies
 pip3 install orderedmultidict
 
 # Install Surelog
-git clone $(python3 setup/_tools.py --tool surelog --field git-url) surelog
+git clone $(python3 ${src_path}/setup/_tools.py --tool surelog --field git-url) surelog
 cd surelog
-git checkout $(python3 setup/_tools.py --tool surelog --field git-commit)
+git checkout $(python3 ${src_path}/setup/_tools.py --tool surelog --field git-commit)
 git submodule update --init --recursive
 
 export ADDITIONAL_CMAKE_OPTIONS=-DPython3_ROOT_DIR=${pythonLocation}
