95103f5f455ebb22c1fc685f2e36aa5ff1934478
Noah Moroze <noah@zeroasic.com>
2023-03-01 21:04:36 -0500

    wheels: fix syntax, print cmake version

--- .github/workflows/bin/install_surelog_macos.sh
+++ .github/workflows/bin/install_surelog_macos.sh
@@ -9,7 +9,9 @@ cd surelog
 git checkout $(python3 setup/_tools.py --tool surelog --field git-commit)
 git submodule update --init --recursive
 
+cmake --version # must be >=3.19
+
 # Point to Python, build universal binary (supporting Intel and Apple Silicon-based Macs)
-export ADDITIONAL_CMAKE_OPTIONS="-DPython3_ROOT_DIR=${pythonLocation} -DCMAKE_OSX_ARCHITECTURES=x86_64;arm64"
+export ADDITIONAL_CMAKE_OPTIONS="-DPython3_ROOT_DIR=${pythonLocation} -DCMAKE_OSX_ARCHITECTURES='x86_64;arm64'"
 make
 make install PREFIX=$GITHUB_WORKSPACE/siliconcompiler/tools/surelog
