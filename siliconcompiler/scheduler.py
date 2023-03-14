15617bfd116ccf3663ff101e519eb8424e686f51
Will <5217539+WRansohoff@users.noreply.github.com>
2023-03-14 19:48:34 -0400

    Move more scheduler params into the schema, and attempt to add support for pre/post logic in HPC jobs

--- siliconcompiler/scheduler.py
+++ siliconcompiler/scheduler.py
@@ -19,10 +19,7 @@ def _deferstep(chip, step, index, status):
     scheduler_type = chip.get('option', 'scheduler', 'name', step=step, index=index)
 
     # Determine which cluster parititon to use. (Default value can be overridden on per-step basis)
-    if f'{step}_slurm_partition' in chip.status:
-        partition = chip.status[f'{step}_slurm_partition']
-    else:
-        partition = chip.status['slurm_partition']
+    partition = chip.get('option', 'scheduler', 'queue', step=step, index=index)
     # Get the temporary UID associated with this job run.
     job_hash = chip.status['jobhash']
 
@@ -39,6 +36,7 @@ def _deferstep(chip, step, index, status):
     if scheduler_type == 'slurm':
         # The script defining this Chip object may specify feature(s) to
         # ensure that the job runs on a specific subset of available nodes.
+        # TODO: Maybe we should add a Schema parameter for these values.
         if 'slurm_constraint' in chip.status:
             slurm_constraint = chip.status['slurm_constraint']
         else:
@@ -62,17 +60,34 @@ def _deferstep(chip, step, index, status):
         if 'slurm_account' in chip.status:
             username = chip.status['slurm_account']
             schedule_cmd.extend(['--acount', username])
+        # Only delay the starting time if the 'defer' Schema option is specified.
+        defer_time = self.get('option', 'scheduler', 'defer')
+        if defer_time:
+            schedule_cmd.extend(['--begin', defer_time])
     elif scheduler_type == 'lsf':
         # TODO: LSF support is untested and currently unsupported.
         schedule_cmd = ['lsrun']
+
+    # User-defined pre/post-processing steps for the Slurm script.
+    # Unlike most other scheduler parameters, we should keep these pre/post-process blocks in the
+    # ephemeral status dictionary, so that they cannot persist across remote connections.
+    # The best place to set these values would be a custom target created by the cluster administrator.
+    script_prefix, script_suffix = '\n', '\n'
+    for pfx in ['sched', f'{step}{index}_sched']:
+        if f'{pfx}_preprocess' in chip.status:
+            script_prefix += chip.status[f'{pfx}_preprocess']
+        if f'{pfx}_postprocess' in chip.status:
+            script_suffix += chip.status[f'{pfx}_postprocess']
 
     # Create a command to defer execution to a compute node.
     script_path = f'{cfg_dir}/{step}{index}.sh'
     with open(script_path, 'w') as sf:
         sf.write('#!/bin/bash\n')
+        sf.write(script_prefix)
         sf.write(f'sc -cfg {shlex.quote(cfg_file)} -builddir {shlex.quote(chip.get("option", "builddir"))} '\
                     f'-arg_step {shlex.quote(step)} -arg_index {shlex.quote(index)} '\
                     f"-design {shlex.quote(chip.top())}\n")
+        sf.write(script_suffix)
         # In case of error(s) which prevents the SC build script from completing, ensure the
         # file mutex for job completion is set in shared storage. This lockfile signals the server
         # to mark the job done, without putting load on the cluster reporting/accounting system.
--- tests/flows/test_slurm_local.py
+++ tests/flows/test_slurm_local.py
@@ -2,19 +2,67 @@ import os
 import pytest
 
 @pytest.mark.eda
-@pytest.mark.skip(reason='Running slurm relies on Slurm account info that is injected by server, so it does not run locally.')
 def test_slurm_local_py(gcd_chip):
     '''Basic Python API test: build the GCD example using only Python code.
+       Note: Requires that the test runner be connected to a cluster, or configured
+       as a single-machine "cluster".
     '''
 
     # Inserting value into configuration
-    gcd_chip.set('jobscheduler', 'slurm')
+    gcd_chip.set('option', 'scheduler', 'slurm')
 
     # Run the chip's build process synchronously.
     gcd_chip.run()
 
     # Verify that GDS file was generated.
     assert os.path.isfile('build/gcd/job0/export/0/outputs/gcd.gds')
+
+@pytest.mark.eda
+def test_slurm_local_py_prefix(gcd_chip):
+    '''Basic Python API test: build the GCD example using only Python code.
+       Note: Requires that the test runner be connected to a cluster, or configured
+       as a single-machine "cluster".
+    '''
+
+    # Inserting value into configuration
+    gcd_chip.set('option', 'scheduler', 'slurm')
+    gcd_chip.status['syn0_sched_preprocess'] = '''
+touch inputs/extrafile.txt
+'''
+
+    # Run the chip's build process synchronously.
+    gcd_chip.run()
+
+    # Verify that GDS file was generated.
+    assert os.path.isfile('build/gcd/job0/export/0/outputs/gcd.gds')
+    assert os.path.isfile('build/gcd/job0/syn/0/inputs/extrafile.txt')
+
+@pytest.mark.eda
+def test_slurm_local_py_suffix(gcd_chip):
+    '''Basic Python API test: build the GCD example using only Python code.
+       Note: Requires that the test runner be connected to a cluster, or configured
+       as a single-machine "cluster".
+    '''
+
+    # Inserting value into configuration
+    gcd_chip.set('option', 'scheduler', 'slurm')
+    gcd_chip.status['sched_postprocess'] = '''
+touch outputs/extrafile.txt
+'''
+    gcd_chip.status['place0_sched_postprocess'] = '''
+touch outputs/placefile.txt
+'''
+
+    # Run the chip's build process synchronously.
+    gcd_chip.run()
+
+    # Verify that GDS file was generated.
+    assert os.path.isfile('build/gcd/job0/export/0/outputs/gcd.gds')
+    # Verify that post-process blocks ran correctly.
+    assert os.path.isfile('build/gcd/job0/syn/0/outputs/extrafile.txt')
+    assert os.path.isfile('build/gcd/job0/floorplan/0/outputs/extrafile.txt')
+    assert os.path.isfile('build/gcd/job0/place/0/outputs/extrafile.txt')
+    assert os.path.isfile('build/gcd/job0/place/0/outputs/placefile.txt')
 
 if __name__ == "__main__":
     from tests.fixtures import gcd_chip
