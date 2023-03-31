2244edbc4f5300965cf3cbac40b352a3bc5376cd
Peter Gadfort <gadfort@zeroasic.com>
2023-03-31 17:30:31 -0400

    fix check_filepaths to handle single directories and leverage _find_files to avoid code duplication + unit tests

---
+++ tests/core/test_check_filepaths.py
@@ -0,0 +1,41 @@
+# Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import siliconcompiler
+
+import os
+
+import pytest
+
+@pytest.mark.quick
+def test_check_filepaths_pass():
+    chip = siliconcompiler.Chip('gcd')
+    chip.load_target("freepdk45_demo")
+    chip.input('gcd.v')
+
+    # Create build dir
+    os.makedirs('build')
+
+    with open('gcd.v', 'w') as f:
+        f.write('test')
+
+    assert chip.check_filepaths()
+
+@pytest.mark.quick
+def test_check_filepaths_faildir():
+    chip = siliconcompiler.Chip('gcd')
+    chip.load_target("freepdk45_demo")
+    chip.input('gcd.v')
+
+    with open('gcd.v', 'w') as f:
+        f.write('test')
+
+    assert not chip.check_filepaths()
+
+@pytest.mark.quick
+def test_check_filepaths_failfile():
+    chip = siliconcompiler.Chip('gcd')
+    chip.load_target("freepdk45_demo")
+    chip.input('examples/gcd/gcd1.v')
+
+    # Create build dir
+    os.makedirs('build')
+    assert not chip.check_filepaths()
