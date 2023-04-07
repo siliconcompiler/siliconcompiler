cf42b53653faf1d25e58ae56955e93fca077b560
Will <5217539+WRansohoff@users.noreply.github.com>
2023-04-07 10:18:24 -0400

    Another typo

--- siliconcompiler/scheduler.py
+++ siliconcompiler/scheduler.py
@@ -60,7 +60,7 @@ def _deferstep(chip, step, index, status):
         # Only specify an account if accounting is required for this cluster/run.
         if 'slurm_account' in chip.status:
             username = chip.status['slurm_account']
-            schedule_cmd.extend(['--acount', username])
+            schedule_cmd.extend(['--account', username])
         # Only delay the starting time if the 'defer' Schema option is specified.
         # TODO: Add step/index at next major release.
         defer_time = chip.get('option', 'scheduler', 'defer')
