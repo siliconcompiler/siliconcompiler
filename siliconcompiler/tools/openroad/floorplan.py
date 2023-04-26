d20937ac0507c3809caab15fc321ebf21f27ccbc
Peter Gadfort <gadfort@zeroasic.com>
2023-04-26 18:37:29 -0400

    implement x/y location snapping to OpenROAD driver to ensure ongrid placement of macros

--- siliconcompiler/tools/openroad/floorplan.py
+++ siliconcompiler/tools/openroad/floorplan.py
@@ -27,5 +27,10 @@ def setup(chip):
         chip.add('tool', tool, 'task', task, 'require', ','.join(['tool', tool, 'task', task, 'file', 'padring']))
     chip.set('tool', tool, 'task', task, 'file', 'padring', 'script to insert the padring', field='help')
 
+    snap = chip.get('tool', tool, 'task', task, 'var', 'ifp_snap_strategy', step=step, index=index)[0]
+    snaps_allowed =  ('none', 'site', 'manufacturing_grid')
+    if snap not in snaps_allowed:
+        chip.error(f'{snap} is not a supported snapping strategy. Allowed values: {snaps_allowed}')
+
 def pre_process(chip):
     build_pex_corners(chip)
