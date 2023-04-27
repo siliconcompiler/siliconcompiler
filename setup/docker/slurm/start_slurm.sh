9ee65df9f786e7a366459767b1d59820e0d4b5bd
Peter Gadfort <gadfort@zeroasic.com>
2023-04-27 13:46:17 -0400

    fix extra space in start_slurm script

--- setup/docker/slurm/start_slurm.sh
+++ setup/docker/slurm/start_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the container's hostname in slurm.conf
-sed "s^{{ hostname }}^`hostname`^g" /etc/slurm/slurm.conf.in >  /etc/slurm/slurm.conf
+sed "s^{{ hostname }}^`hostname`^g" /etc/slurm/slurm.conf.in > /etc/slurm/slurm.conf
 
 # Start munge and slurm daemons
 /etc/init.d/munge start
