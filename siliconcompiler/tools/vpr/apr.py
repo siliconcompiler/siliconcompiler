1c28facc0b01e10d92678b689510663085cbcb10
petergrossmann21 <peter@zeroasic.com>
2023-05-09 17:39:23 -0400

    Fix lint errors

--- siliconcompiler/tools/vpr/apr.py
+++ siliconcompiler/tools/vpr/apr.py
@@ -37,26 +37,26 @@ def setup(chip):
 
     if 'sdc' in chip.getkeys('input'):
         options.append(f"--sdc_file {chip.get('input', 'fpga', 'sdc', step=step, index=index)}")
-    else :
+    else:
         options.append(f"--timing_analysis off")
-        
-    #Routing graph XML:
-    rr_graph_files = chip.get('tool', 'vpr', 'task', 'apr', 'var', 'rr_graph')
-    #if (len(rr_graph_files) == 1) :
-    options.append(f"--read_rr_graph "+rr_graph_files[0])
 
-    #***NOTE:  For real FPGA chips you need to specify the routing channel
+    # Routing graph XML:
+    rr_graph_files = chip.get('tool', 'vpr', 'task', 'apr', 'var', 'rr_graph')
+    # if (len(rr_graph_files) == 1):
+    options.append(f"--read_rr_graph " + rr_graph_files[0])
+
+    # ***NOTE: For real FPGA chips you need to specify the routing channel
     #          width explicitly.  VPR requires an explicit routing channel
     #          with when --read_rr_graph is used (typically the case for
     #          real chips).  Otherwise VPR performs a binary search for
     #          the minimum routing channel width that the circuit fits in.
     #          -PG 1/13/2023
-    #Given the above, it may be appropriate to couple these variables somehow,
-    #but --route_chan_width CAN be used by itself.
+    # Given the above, it may be appropriate to couple these variables somehow,
+    # but --route_chan_width CAN be used by itself.
     num_routing_channels = chip.get('tool', 'vpr', 'task', 'apr', 'var', 'route_chan_width')
-    if (len(num_routing_channels) == 1) :
+    if (len(num_routing_channels) == 1):
         options.append(f'--route_chan_width {num_routing_channels[0]}')
-    
+
     threads = chip.get('tool', tool, 'task', task, 'threads', step=step, index=index)
     options.append(f"--num_workers {threads}")
 
