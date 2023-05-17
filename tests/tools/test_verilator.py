8f7943508b98bb25a3697f24f541fd9e216ddbab
Noah Moroze <noah@zeroasic.com>
2023-05-17 16:56:09 -0400

    Move Verilator compile test into tool tests

--- tests/tools/test_verilator.py
+++ tests/tools/test_verilator.py
@@ -4,19 +4,38 @@ import subprocess
 import pytest
 
 import siliconcompiler
-
 from siliconcompiler.tools.surelog import parse
-from siliconcompiler.tools.verilator import compile
+from siliconcompiler.tools.verilator import lint, compile
 
 
+@pytest.mark.quick
 @pytest.mark.eda
-def test_basic(scroot, datadir):
+def test_lint_post_surelog(scroot):
     chip = siliconcompiler.Chip('heartbeat')
 
     v_src = os.path.join(scroot, 'tests', 'data', 'heartbeat.v')
     chip.input(v_src)
-    c_src = os.path.join(datadir, 'heartbeat_tb.cpp')
+
+    flow = 'lint'
+    chip.node(flow, 'import', parse)
+    chip.node(flow, 'lint', lint)
+    chip.edge(flow, 'import', 'lint')
+    chip.set('option', 'flow', flow)
+
+    chip.run()
+
+
+@pytest.mark.quick
+@pytest.mark.eda
+def test_compile(scroot, datadir):
+    chip = siliconcompiler.Chip('heartbeat')
+
+    v_src = os.path.join(scroot, 'tests', 'data', 'heartbeat.v')
+    chip.input(v_src)
+    c_src = os.path.join(datadir, 'verilator', 'heartbeat_tb.cpp')
     chip.input(c_src)
+    vlt_cfg = os.path.join(datadir, 'verilator', 'config.vlt')
+    chip.set('tool', 'verilator', 'task', 'compile', 'file', 'config', vlt_cfg)
 
     chip.set('option', 'mode', 'sim')
 
