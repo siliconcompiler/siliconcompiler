6dc5744c69d68c919461263f5ba3fd1b63d94a08
aolofsson <andreas@zeroasic.com>
2023-05-20 22:43:31 -0400

    Adding basic usability test for datasheet

---
+++ tests/core/test_datasheet.py
@@ -0,0 +1,41 @@
+# Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import siliconcompiler
+
+
+def test_datasheet():
+    '''API test for help method
+    '''
+
+    chip = siliconcompiler.Chip('nand2')
+
+    mode = 'default'
+    top = chip.top()
+
+    # inputs
+    chip.set('datasheet', top, 'pin', 'a', 'dir', mode, 'input');
+    chip.set('datasheet', top, 'pin', 'b', 'dir', mode, 'input');
+    chip.set('datasheet', top, 'pin', 'a', 'cap', mode, (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'b', 'cap', mode, (0.9,0.9,0.9));
+
+    # output
+    chip.set('datasheet', top, 'pin', 'z', 'func', mode, '~(a&b)');
+    chip.set('datasheet', top, 'pin', 'z', 'dir', mode, 'output');
+    chip.set('datasheet', top, 'pin', 'z', 'trise', mode, 'a', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'tfall', mode, 'a', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'trise', mode, 'b', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'tfall', mode, 'b', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayr', mode, 'a', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayf', mode, 'a', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayr', mode, 'b', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayf', mode, 'b', (0.9,0.9,0.9));
+
+    # print outputs
+    for key in chip.allkeys():
+        if key[0]=='datasheet':
+            val = chip.get(*key)
+            if val:
+                print(key, chip.get(*key))
+
+#########################
+if __name__ == "__main__":
+    test_datasheet()
