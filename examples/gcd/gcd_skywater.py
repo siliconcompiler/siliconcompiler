7444d7158a45b07e581c24b3a9033d9b2c4fe6b9
aolofsson <andreas@zeroasic.com>
2023-05-20 22:07:01 -0400

    Fixing name change from power to supply

--- examples/gcd/gcd_skywater.py
+++ examples/gcd/gcd_skywater.py
@@ -20,7 +20,7 @@ def main():
 
     chip.load_target("skywater130_demo")
 
-    chip.set('datasheet', chip.top(), 'pin', 'vdd', 'type', 'global', 'power')
+    chip.set('datasheet', chip.top(), 'pin', 'vdd', 'type', 'global', 'supply')
     chip.set('datasheet', chip.top(), 'pin', 'vss', 'type', 'global', 'ground')
 
     # 1) RTL2GDS
