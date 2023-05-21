83c63fa472b0cc9dde19846de5e8f1b98de83330
aolofsson <andreas@zeroasic.com>
2023-05-20 22:49:13 -0400

    Fixing basic lint errors

--- tests/core/test_datasheet.py
+++ tests/core/test_datasheet.py
@@ -12,29 +12,30 @@ def test_datasheet():
     top = chip.top()
 
     # inputs
-    chip.set('datasheet', top, 'pin', 'a', 'dir', mode, 'input');
-    chip.set('datasheet', top, 'pin', 'b', 'dir', mode, 'input');
-    chip.set('datasheet', top, 'pin', 'a', 'cap', mode, (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'b', 'cap', mode, (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'a', 'dir', mode, 'input')
+    chip.set('datasheet', top, 'pin', 'b', 'dir', mode, 'input')
+    chip.set('datasheet', top, 'pin', 'a', 'cap', mode, (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'b', 'cap', mode, (0.9, 0.9, 0.9))
 
     # output
-    chip.set('datasheet', top, 'pin', 'z', 'func', mode, '~(a&b)');
-    chip.set('datasheet', top, 'pin', 'z', 'dir', mode, 'output');
-    chip.set('datasheet', top, 'pin', 'z', 'trise', mode, 'a', (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'z', 'tfall', mode, 'a', (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'z', 'trise', mode, 'b', (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'z', 'tfall', mode, 'b', (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'z', 'tdelayr', mode, 'a', (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'z', 'tdelayf', mode, 'a', (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'z', 'tdelayr', mode, 'b', (0.9,0.9,0.9));
-    chip.set('datasheet', top, 'pin', 'z', 'tdelayf', mode, 'b', (0.9,0.9,0.9));
+    chip.set('datasheet', top, 'pin', 'z', 'func', mode, '~(a&b)')
+    chip.set('datasheet', top, 'pin', 'z', 'dir', mode, 'output')
+    chip.set('datasheet', top, 'pin', 'z', 'trise', mode, 'a', (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'z', 'tfall', mode, 'a', (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'z', 'trise', mode, 'b', (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'z', 'tfall', mode, 'b', (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayr', mode, 'a', (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayf', mode, 'a', (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayr', mode, 'b', (0.9, 0.9, 0.9))
+    chip.set('datasheet', top, 'pin', 'z', 'tdelayf', mode, 'b', (0.9, 0.9, 0.9))
 
     # print outputs
     for key in chip.allkeys():
-        if key[0]=='datasheet':
+        if key[0] == 'datasheet':
             val = chip.get(*key)
             if val:
                 print(key, chip.get(*key))
+
 
 #########################
 if __name__ == "__main__":
