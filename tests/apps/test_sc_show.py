488d91b04660a35abcd672c9eaf54d9e666ab97e
Peter Gadfort <gadfort@zeroasic.com>
2023-05-25 12:49:35 -0400

    add additional timeout to some tests to allow for variations in runtime

--- tests/apps/test_sc_show.py
+++ tests/apps/test_sc_show.py
@@ -36,6 +36,7 @@ def heartbeat_dir(tmpdir_factory):
 ])
 @pytest.mark.eda
 @pytest.mark.quick
+@pytest.mark.timeout(120)
 def test_sc_show_design_only(flags, monkeypatch, heartbeat_dir):
     '''Test sc-show app on a few sets of flags.'''
 
@@ -71,6 +72,7 @@ def test_sc_show_design_only(flags, monkeypatch, heartbeat_dir):
 ])
 @pytest.mark.eda
 @pytest.mark.quick
+@pytest.mark.timeout(120)
 def test_sc_show(flags, monkeypatch, heartbeat_dir):
     '''Test sc-show app on a few sets of flags.'''
 
