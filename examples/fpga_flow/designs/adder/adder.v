e7aad19683ee09a148e6a23ce18740d168da90f1
petergrossmann21 <peter@zeroasic.com>
2023-05-26 17:37:27 -0400

    Add additional HDL designs for FPGA flow testing

---
+++ examples/fpga_flow/designs/adder/adder.v
@@ -0,0 +1,19 @@
+//adder.v
+//Peter Grossmann
+//9 August 2022
+//$Id$
+//$Log$
+
+module adder
+  # (
+     parameter DATA_WIDTH = 8
+     )
+   (
+    input [(DATA_WIDTH-1):0] a,
+    input [(DATA_WIDTH-1):0] b,
+    output [(DATA_WIDTH):0] y
+    );
+
+   assign y = a + b;
+
+endmodule // adder
---
+++ examples/fpga_flow/designs/adder/adder.vhd
@@ -0,0 +1,33 @@
+--adder.vhd
+--Peter Grossmann
+--24 May 2023
+--$Id$
+--$Log$
+
+--Multiply accumulate circuit
+--Adapted from example code found here:
+--https://surf-vhdl.com/how-to-implement-pipeline-multiplier-vhdl/
+
+
+library ieee ;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity adder is
+  generic (
+    WIDTH   : integer:= 8
+  );
+  port ( 
+    a        : in  unsigned(WIDTH downto 0);
+    b        : in  unsigned(WIDTH downto 0);
+    y        : out unsigned(WIDTH downto 0)
+  );
+end adder;
+
+architecture behavioral of adder is
+
+begin
+  
+  y <= a + b;
+    
+end behavioral;
