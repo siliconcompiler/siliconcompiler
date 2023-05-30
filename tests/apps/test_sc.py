c3feb4f25d03e4e56149dde52bf1532c59a8a239
Peter Gadfort <gadfort@zeroasic.com>
2023-05-30 11:26:37 -0400

    remove calls to subprocess where direct python access is possible

--- tests/apps/test_sc.py
+++ tests/apps/test_sc.py
@@ -1,15 +1,17 @@
 import os
-import subprocess
+from siliconcompiler.apps import sc
 
 import pytest
 
 
 @pytest.mark.eda
 @pytest.mark.quick
-def test_design_inference(scroot):
+def test_design_inference(scroot, monkeypatch):
     '''Tests that sc CLI app can infer design name from filename.'''
     source = os.path.join(scroot, 'tests', 'data', 'heartbeat.v')
     # only run import, makes this quicker
-    subprocess.run(['sc', source, '-steplist', 'import', '-strict'])
+
+    monkeypatch.setattr('sys.argv', ['sc', source, '-steplist', 'import', '-strict'])
+    assert sc.main() == 0
 
     assert os.path.isfile('build/heartbeat/job0/heartbeat.pkg.json')
