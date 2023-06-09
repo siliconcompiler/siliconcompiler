5a6dc8ccdd5a3a727310710eda7fa29f30f480ff
Peter Gadfort <gadfort@zeroasic.com>
2023-06-09 13:10:36 -0400

    make tool/task/format an enum

--- siliconcompiler/schema/schema_cfg.py
+++ siliconcompiler/schema/schema_cfg.py
@@ -11,7 +11,7 @@ try:
 except ImportError:
     from siliconcompiler.schema.utils import trim
 
-SCHEMA_VERSION = '0.34.1'
+SCHEMA_VERSION = '0.34.2'
 
 
 #############################################################################
@@ -1275,14 +1275,14 @@ def schema_tool(cfg, tool='default'):
             setting 'novercheck' to True.""")
 
     scparam(cfg, ['tool', tool, 'format'],
-            sctype='str',
+            sctype='enum',
+            enum=["json", "tcl", "yaml"],
             shorthelp="Tool: file format",
             switch="-tool_format 'tool <file>'",
             example=["cli: -tool_format 'yosys tcl'",
                      "api: chip.set('tool', 'yosys', 'format', 'tcl')"],
             schelp="""
-            File format for tool manifest handoff. Supported formats are tcl,
-            yaml, and json.""")
+            File format for tool manifest handoff.""")
 
     key = 'default'
     scparam(cfg, ['tool', tool, 'licenseserver', key],
