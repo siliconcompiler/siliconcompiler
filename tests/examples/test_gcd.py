9e7d2054335e7ef86be89a8b43d0c79531863e5b
Peter Gadfort <gadfort@zeroasic.com>
2023-06-12 20:43:26 -0400

    update gcd test

--- tests/examples/test_gcd.py
+++ tests/examples/test_gcd.py
@@ -43,12 +43,12 @@ def __check_gcd(chip):
     assert chip.get('metric', 'warnings', step='cts', index='0') == 5
 
     # Warning: *. (x3)
-    # Missing route to pin (x69)
-    assert chip.get('metric', 'warnings', step='route', index='0') == 72
+    # Missing route to pin (x63)
+    assert chip.get('metric', 'warnings', step='route', index='0') == 66
 
     # Warning: *. (x3)
-    # Missing route to pin (x235)
-    assert chip.get('metric', 'warnings', step='dfm', index='0') == 238
+    # Missing route to pin (x224)
+    assert chip.get('metric', 'warnings', step='dfm', index='0') == 227
 
     # "no fill config specified"
     assert chip.get('metric', 'warnings', step='export', index='0') == 1
