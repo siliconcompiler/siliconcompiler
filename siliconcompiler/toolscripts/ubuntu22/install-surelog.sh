55d086e6df1b2d64191fbc694f346ae40131b37f
Peter Gadfort <gadfort@zeroasic.com>
2023-06-25 09:45:04 -0400

    update to Ubuntu22 for tools builder
    fix daily tests running image to 22.04
    
    disable pushing name tags
    
    fix tools image for ubuntu22
    
    add venv to docker runner
    
    ensure local image also has env
    
    ensure venv for install for now
    
    fix local runner image

--- siliconcompiler/toolscripts/ubuntu22/install-surelog.sh
+++ siliconcompiler/toolscripts/ubuntu22/install-surelog.sh
@@ -9,10 +9,15 @@ src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)/..
 sudo apt-get install -y build-essential cmake git pkg-config \
     tclsh swig uuid-dev libgoogle-perftools-dev python3 \
     python3-orderedmultidict python3-psutil python3-dev \
-    default-jre lcov zlib1g-dev
+    default-jre lcov zlib1g-dev python3-venv
 
 mkdir -p deps
 cd deps
+
+python3 -m venv .surelog --clear
+. .surelog/bin/activate
+python3 -m pip install cmake
+python3 -m pip install orderedmultidict
 
 git clone $(python3 ${src_path}/_tools.py --tool surelog --field git-url) surelog
 cd surelog
@@ -20,6 +25,6 @@ git checkout $(python3 ${src_path}/_tools.py --tool surelog --field git-commit)
 git submodule update --init --recursive
 
 make -j$(nproc)
-sudo make install
+sudo -E PATH="$PATH" make install
 
 cd -
