bc5792abf8778d57bae4902d7bee78afc039e292
Peter Gadfort <gadfort@zeroasic.com>
2023-07-09 14:47:54 -0400

    correct slurm conf path

--- setup/docker/slurm/install-slurm.sh
+++ setup/docker/slurm/install-slurm.sh
@@ -4,3 +4,5 @@ useradd slurm
 # Configure Slurm
 mkdir -p /sc_tools/log/slurm /sc_tools/spool/slurm /sc_tools/spool/slurmd /sc_tools/etc
 chown -R slurm:slurm /sc_tools/log/slurm /sc_tools/spool/slurm /sc_tools/spool/slurmd /sc_tools/etc
+
+mv /sc_tools/slurm_cfg/cgroup.conf /sc_tools/etc/
