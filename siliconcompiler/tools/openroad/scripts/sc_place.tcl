7a093c0dfa9aa982eeb51e2acbce96fd141510f5
Peter Gadfort <gadfort@zeroasic.com>
2023-07-17 00:19:05 -0400

    wrap initial global placement in placeable_ios check with pin_placement

--- siliconcompiler/tools/openroad/scripts/sc_place.tcl
+++ siliconcompiler/tools/openroad/scripts/sc_place.tcl
@@ -1,17 +1,21 @@
 #######################
-# Global Placement (without considering IO placements)
+# Global Placement and Refinement of Pin Placement
 #######################
 
-if { $openroad_gpl_enable_skip_io && [sc_design_has_placeable_ios] } {
-  utl::info FLW 1 "Performing global placement without considering IO"
-  sc_global_placement -skip_io
-}
-
-###########################
-# Refine Automatic Pin Placement
-###########################
-
 if { [sc_design_has_placeable_ios] } {
+  #######################
+  # Global Placement (without considering IO placements)
+  #######################
+
+  if { $openroad_gpl_enable_skip_io } {
+    utl::info FLW 1 "Performing global placement without considering IO"
+    sc_global_placement -skip_io
+  }
+
+  ###########################
+  # Refine Automatic Pin Placement
+  ###########################
+
   sc_pin_placement
 }
 
