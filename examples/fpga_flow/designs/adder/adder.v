95f545d7736172df586c8c63258c0cada3005757
petergrossmann21 <peter@zeroasic.com>
2023-07-19 14:17:58 -0400

    macc lint fixes

--- examples/fpga_flow/designs/adder/adder.v
+++ examples/fpga_flow/designs/adder/adder.v
@@ -5,11 +5,11 @@
 module adder #(
     parameter DATA_WIDTH = 8
 ) (
-    input [(DATA_WIDTH-1):0] a,
-    input [(DATA_WIDTH-1):0] b,
-    output [(DATA_WIDTH):0] y
+    input  [(DATA_WIDTH-1):0] a,
+    input  [(DATA_WIDTH-1):0] b,
+    output [  (DATA_WIDTH):0] y
 );
 
     assign y = a + b;
 
-endmodule // adder
+endmodule  // adder
