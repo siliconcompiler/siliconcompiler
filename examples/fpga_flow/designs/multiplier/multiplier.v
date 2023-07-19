62a4cc7d561a16ccc3a578823ecb56b13894d1a4
petergrossmann21 <peter@zeroasic.com>
2023-07-19 14:01:21 -0400

    Fix verilog lint issues

--- examples/fpga_flow/designs/multiplier/multiplier.v
+++ examples/fpga_flow/designs/multiplier/multiplier.v
@@ -2,14 +2,14 @@
 //Peter Grossmann
 //13 September 2022
 
-module multiplier # (
-		     parameter DATA_WIDTH = 16
-		     ) (
-    input [(DATA_WIDTH-1):0] 	a,
-    input [(DATA_WIDTH-1):0] 	b,
-    output [(2*DATA_WIDTH-1):0] y
-    );
+module multiplier #(
+  parameter DATA_WIDTH = 16
+) (
+   input [(DATA_WIDTH-1):0] 	a,
+   input [(DATA_WIDTH-1):0] 	b,
+   output [(2*DATA_WIDTH-1):0] y
+);
 
-   assign y = a * b;
+    assign y = a * b;
 
 endmodule // multiplier
