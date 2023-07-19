dc23379e51e5d396d0abbf75d14bd88af6113b7f
petergrossmann21 <peter@zeroasic.com>
2023-07-19 14:25:17 -0400

    macc lint fixes

--- examples/fpga_flow/designs/macc/macc.v
+++ examples/fpga_flow/designs/macc/macc.v
@@ -6,16 +6,16 @@ module macc #(
     parameter INPUT_WIDTH = 8,
     parameter OUTPUT_WIDTH = 20
 ) (
-    input       [(INPUT_WIDTH-1):0] a,
-    input       [(INPUT_WIDTH-1):0] b,
-    input 			    clk,
-    input 			    reset,
+    input      [ (INPUT_WIDTH-1):0] a,
+    input      [ (INPUT_WIDTH-1):0] b,
+    input                           clk,
+    input                           reset,
     output reg [(OUTPUT_WIDTH-1):0] y
 );
 
     always @(posedge clk) begin
-       if (reset) y <= 'h0;
-       else y <= a * b + y;
+        if (reset) y <= 'h0;
+        else y <= a * b + y;
     end
-   
+
 endmodule  // macc
