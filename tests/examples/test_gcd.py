a412ccb39409b7bbc4827fa4facc051ec3255fa3
Peter Gadfort <gadfort@zeroasic.com>
2023-08-05 09:59:19 -0400

    bump warnings in gcd test

--- tests/examples/test_gcd.py
+++ tests/examples/test_gcd.py
@@ -43,12 +43,12 @@ def __check_gcd(chip):
     assert chip.get('metric', 'warnings', step='cts', index='0') == 5
 
     # Warning: *. (x3)
-    # Missing route to pin (x61)
-    assert chip.get('metric', 'warnings', step='route', index='0') == 64
+    # Missing route to pin (x60)
+    assert chip.get('metric', 'warnings', step='route', index='0') == 63
 
     # Warning: *. (x3)
-    # Missing route to pin (x182)
-    assert chip.get('metric', 'warnings', step='dfm', index='0') == 185
+    # Missing route to pin (x184)
+    assert chip.get('metric', 'warnings', step='dfm', index='0') == 187
 
     # "no fill config specified"
     assert chip.get('metric', 'warnings', step='export', index='0') == 1
