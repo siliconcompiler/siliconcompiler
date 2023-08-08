0a5a46c6ca0044855620bbb471b017bf711bcd44
GabrielAguirre1 <80078690+GabrielAguirre1@users.noreply.github.com>
2023-08-08 14:39:54 -0400

    found bugs, made syntax and display more consistent

--- siliconcompiler/report/streamlit_viewer.py
+++ siliconcompiler/report/streamlit_viewer.py
@@ -19,35 +19,31 @@ from siliconcompiler import __version__ as sc_version
 SUCCESS_COLOR = '#8EA604'  # green
 PENDING_COLOR = '#F5BB00'  # yellow, could use: #EC9F05
 FAILURE_COLOR = '#FF4E00'  # red
-
-sc_logo_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'logo.png')
-
-sc_font_path = \
+SC_LOGO_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'logo.png')
+SC_FONT_PATH = \
     os.path.join(os.path.dirname(__file__), '..', 'data', 'RobotoMono', 'RobotoMono-Regular.ttf')
-
-sc_about = [
+SC_ABOUT = [
     f"SiliconCompiler {sc_version}",
     '''A compiler framework that automates translation from source code to
      silicon.''',
     "https://www.siliconcompiler.com/",
     "https://github.com/siliconcompiler/siliconcompiler/"
 ]
-
-sc_menu = {"Get help": "https://docs.siliconcompiler.com/",
+SC_MENU = {"Get help": "https://docs.siliconcompiler.com/",
            "Report a Bug":
            '''https://github.com/siliconcompiler/siliconcompiler/issues''',
-           "About": "\n\n".join(sc_about)}
+           "About": "\n\n".join(SC_ABOUT)}
 
 # opened by running command in siliconcompiler/apps/sc_dashboard.py
-parser = argparse.ArgumentParser('dashboard')
-parser.add_argument('cfg', nargs='?')
-args = parser.parse_args()
+PARSER = argparse.ArgumentParser('dashboard')
+PARSER.add_argument('cfg', nargs='?')
+ARGS = PARSER.parse_args()
 
-if not args.cfg:
+if not ARGS.cfg:
     raise ValueError('configuration not provided')
 
 if 'job' not in streamlit.session_state:
-    with open(args.cfg, 'r') as f:
+    with open(ARGS.cfg, 'r') as f:
         config = json.load(f)
 
     chip = Chip(design='')
@@ -58,24 +54,24 @@ if 'job' not in streamlit.session_state:
         chip.schema.cfg['history'][os.path.basename(file_path)] = graph_chip.schema.cfg
 
     streamlit.set_page_config(page_title=f'{chip.design} dashboard',
-                              page_icon=Image.open(sc_logo_path), layout="wide",
-                              menu_items=sc_menu)
+                              page_icon=Image.open(SC_LOGO_PATH), layout="wide",
+                              menu_items=SC_MENU)
     streamlit.session_state['master chip'] = chip
     streamlit.session_state['job'] = 'default'
-    new_chip = chip
+    selected_chip = chip
     streamlit.session_state['transpose'] = False
 else:
     chip = streamlit.session_state['master chip']
     streamlit.set_page_config(page_title=f'{chip.design} dashboard',
-                              page_icon=Image.open(sc_logo_path), layout="wide",
-                              menu_items=sc_menu)
-    new_chip = Chip(design='')
+                              page_icon=Image.open(SC_LOGO_PATH), layout="wide",
+                              menu_items=SC_MENU)
+    selected_chip = Chip(design='')
     job = streamlit.session_state['job']
     if job == 'default':
-        new_chip = chip
+        selected_chip = chip
     else:
-        new_chip.schema = chip.schema.history(job)
-        new_chip.set('design', chip.design)
+        selected_chip.schema = chip.schema.history(job)
+        selected_chip.set('design', chip.design)
 
 
 def _convert_filepaths(logs_and_reports):
@@ -124,18 +120,15 @@ def get_nodes_and_edges(chip, node_dependencies, successful_path,
         chip (Chip) : The chip object that contains the schema read from.
         node_dependencies (dict) : Dictionary mapping nodes
             (as tuples of step/index) to their input nodes.
-        successful_path (set) : Contains all the nodes that are part of the
-            'winning' path.
-        succesful_path_node_opacity (float) : A number between 0 and 1
-            (inclusive) which represents the opacity for nodes on a successful
-            path.
+        successful_path (set) : Contains all the nodes that are part of the 'winning' path.
+        succesful_path_node_opacity (float) : A number between 0 and 1 (inclusive) which
+            represents the opacity for nodes on a successful path.
         succesful_path_node_border_width (int) : A number between 0 or greater
             which represents the width for nodes on a successful path.
         succesful_path_edge_width (int) : A number between 0 or greater which
             represents the width for edges on a successful path.
-        default_node_opacity (float) : A number between 0 and 1(inclusive)
-            which represents the opacity for nodes of a node not on a
-            successful path.
+        default_node_opacity (float) : A number between 0 and 1 (inclusive)
+            which represents the opacity for nodes of a node not on a successful path.
         default_node_border_width (int) : A number between 0 or greater
             which represents the width for nodes not on a successful path.
         default_edge_width (int) : A number between 0 or greater which
@@ -232,7 +225,7 @@ def show_files(chip, step, index):
         step (string) : step of node.
         index (string) : index of node.
     """
-    streamlit.caption('files')
+    streamlit.subheader(f'{step}{index} Files')
     logs_and_reports = report.get_files(chip, step, index)
     logs_and_reports = _convert_filepaths(logs_and_reports)
     if logs_and_reports == []:
@@ -291,15 +284,13 @@ def show_metrics_for_file(chip, step, index):
             streamlit.warning("This file does not include any metrics.")
 
 
-def manifest_module(chip, manifest, ui_width, max_num_of_keys_to_show=20,
-                    default_toggle_width_in_percent=0.2, default_toggle_width_in_pixels=200,
-                    header_col_width=0.85):
+def manifest_module(chip, ui_width, max_num_of_keys_to_show=20, default_toggle_width_in_percent=0.2,
+                    default_toggle_width_in_pixels=200, header_col_width=0.85):
     """
     Displays the manifest and a way to search through the manifest.
 
     Args:
         chip (Chip) : The chip object that contains the schema read from.
-        manifest (dict) : Represents the manifest json.
         ui_width (int) : The width of the screen of the web browser in pixels.
         max_num_of_keys_to_show (int) : The maximum number of keys that the
             manifest may have in order to be automatically expanded.
@@ -335,7 +326,7 @@ def manifest_module(chip, manifest, ui_width, max_num_of_keys_to_show=20,
                               help='Click here to see the JSON before it was made more readable'):
             manifest_to_show = chip.schema.cfg
         else:
-            manifest_to_show = manifest
+            manifest_to_show = MANIFEST
     with key_search_col:
         key = streamlit.text_input('Search Keys', '', placeholder="Keys")
         if key != '':
@@ -360,8 +351,7 @@ def select_nodes(metric_dataframe, node_from_flowgraph):
     displayed, the one clicked more recently will be displayed.
 
     Args:
-        metric_dataframe (Pandas.DataFrame) : Contains the metrics of all
-            nodes.
+        metric_dataframe (Pandas.DataFrame) : Contains the metrics of all nodes.
         node_from_flowgraph (string/None) : Contains a string of the node to
             display or None if none exists.
     """
@@ -370,7 +360,7 @@ def select_nodes(metric_dataframe, node_from_flowgraph):
         with streamlit.form("nodes"):
             option = streamlit.selectbox('Pick a node to inspect',
                                          metric_dataframe.columns.tolist())
-            params_submitted = streamlit.form_submit_button()
+            params_submitted = streamlit.form_submit_button('Apply')
             if not params_submitted and node_from_flowgraph is not None:
                 option = node_from_flowgraph
                 streamlit.session_state['selected'] = []
@@ -401,7 +391,7 @@ def metrics_dataframe_module(metric_dataframe):
     display_to_data = {}
     display_options = []
     for metric_unit in metrics_list:
-        metric = metric_to_metric_unit_map[metric_unit]
+        metric = METRIC_TO_METRIC_UNIT_MAP[metric_unit]
         display_to_data[metric] = metric_unit
         display_options.append(metric)
     options = {'metrics': [], 'nodes': []}
@@ -414,7 +404,7 @@ def metrics_dataframe_module(metric_dataframe):
             options['metrics'] = []
             for metric in metrics:
                 options['metrics'].append(display_to_data[metric])
-            streamlit.form_submit_button("Run")
+            streamlit.form_submit_button('Apply')
     if not options['nodes']:
         options['nodes'] = node_list
     if not options['metrics']:
@@ -442,7 +432,7 @@ def show_dataframe_header(header_col_width=0.7):
     header_col, transpose_col = streamlit.columns([header_col_width, 1 - header_col_width],
                                                   gap="large")
     with header_col:
-        streamlit.header('Data Metrics')
+        streamlit.header('Metrics')
     with transpose_col:
         streamlit.markdown('')
         streamlit.markdown('')
@@ -492,9 +482,8 @@ def show_title_and_runs(title_col_width=0.7):
     to inspect.
 
     Args:
-        title_col_width (float) : A number between 0 and 1 which is
-            the percentage of the width of the screen given to the title and
-            logo. The rest is given to selectbox.
+        title_col_width (float) : A number between 0 and 1 which is the percentage of the
+            width of the screen given to the title and logo. The rest is given to selectbox.
     """
     title_col, job_select_col = \
         streamlit.columns([title_col_width, 1 - title_col_width], gap="large")
@@ -506,7 +495,7 @@ def show_title_and_runs(title_col_width=0.7):
                     /* Define the @font-face rule */
                     @font-face {
                     font-family: 'Roboto Mono';
-                    src: url(sc_font_path) format('truetype');
+                    src: url(SC_FONT_PATH) format('truetype');
                     font-weight: normal;
                     font-style: normal;
                     }
@@ -552,7 +541,7 @@ def show_title_and_runs(title_col_width=0.7):
             f'''
             <body>
                 <div class="logo-container">
-                    <img src="data:image/png;base64,{base64.b64encode(open(sc_logo_path,
+                    <img src="data:image/png;base64,{base64.b64encode(open(SC_LOGO_PATH,
                     "rb").read()).decode()}" alt="Logo Image" class="logo-image" height="61">
                     <div class="logo-text">
                         <p class="text1">{streamlit.session_state['master chip'].design}</p>
@@ -573,7 +562,6 @@ def show_title_and_runs(title_col_width=0.7):
         if previous_job != job:
             streamlit.session_state['right after rerun'] = True
             streamlit.experimental_rerun()
-    return new_chip
 
 
 def show_metric_and_node_selection_for_graph(metrics, nodes, graph_number):
@@ -582,8 +570,7 @@ def show_metric_and_node_selection_for_graph(metrics, nodes, graph_number):
     to display.
 
     Args:
-        metrics (list) : A list of metrics that are set for all chips given in
-            chips.
+        metrics (list) : A list of metrics that are set for all chips given in chips.
         nodes (list) : A list of nodes given in the form f'{step}{index}'
         graph_number (int) : The number of graphs there are. Used to create
             keys to distinguish selectboxes from each other.
@@ -591,14 +578,18 @@ def show_metric_and_node_selection_for_graph(metrics, nodes, graph_number):
     metric_selector_col, node_selector_col = streamlit.columns(2, gap='small')
     with metric_selector_col:
         with streamlit.expander('Select a Metric'):
-            selected_metric = streamlit.selectbox('Select a Metric', metrics,
-                                                  label_visibility='collapsed',
-                                                  key=f'metric selection {graph_number}')
+            with streamlit.form(f'metrics{graph_number}'):
+                selected_metric = streamlit.selectbox('Select a Metric', metrics,
+                                                      label_visibility='collapsed',
+                                                      key=f'metric selection {graph_number}')
+                streamlit.form_submit_button('Apply')
     with node_selector_col:
         with streamlit.expander('Select Nodes'):
-            selected_nodes = \
-                streamlit.multiselect('Select a Node', nodes, label_visibility='collapsed',
-                                      key=f'node selection {graph_number}', default=nodes[0])
+            with streamlit.form(f'nodes{graph_number}'):
+                selected_nodes = \
+                    streamlit.multiselect('Select a Node', nodes, label_visibility='collapsed',
+                                          key=f'node selection {graph_number}', default=nodes[0])
+                streamlit.form_submit_button('Apply')
     return selected_metric, selected_nodes
 
 
@@ -769,7 +760,7 @@ def make_node_to_step_index_map(metric_dataframe):
         node_to_step_index_map[step + index] = (step, index)
     # concatenate step and index
     metric_dataframe.columns = metric_dataframe.columns.map(lambda x: f'{x[0]}{x[1]}')
-    return node_to_step_index_map
+    return node_to_step_index_map, metric_dataframe
 
 
 def make_metric_to_metric_unit_map(metric_dataframe):
@@ -790,7 +781,7 @@ def make_metric_to_metric_unit_map(metric_dataframe):
     # concatenate metric and unit
     metric_dataframe.index = metric_dataframe.index.map(lambda x: f'{x[0]} ({x[1]})'
                                                         if x[1] else x[0])
-    return metric_to_metric_unit_map
+    return metric_to_metric_unit_map, metric_dataframe
 
 
 def graphs_module(metric_dataframe):
@@ -801,15 +792,15 @@ def graphs_module(metric_dataframe):
         metric_dataframe (pandas.DataFrame) : A dataframe full of all metrics and all
             nodes of the selected chip
     '''
-    metrics = metric_dataframe.index.map(lambda x: metric_to_metric_unit_map[x])
+    metrics = metric_dataframe.index.map(lambda x: METRIC_TO_METRIC_UNIT_MAP[x])
     nodes = metric_dataframe.columns
     chips = []
     jobs = []
     for job in streamlit.session_state['master chip'].getkeys('history'):
-        new_chip = Chip(design='')
-        new_chip.schema = chip.schema.history(job)
-        new_chip.set('design', chip.design)
-        chips.append({'chip_object': new_chip, 'chip_name': job})
+        selected_chip = Chip(design='')
+        selected_chip.schema = chip.schema.history(job)
+        selected_chip.set('design', chip.design)
+        chips.append({'chip_object': selected_chip, 'chip_name': job})
         jobs.append(job)
     job_selector_col, graph_adder_col = streamlit.columns(2, gap='large')
     with job_selector_col:
@@ -828,11 +819,11 @@ def graphs_module(metric_dataframe):
         with graph_col:
             metric, selected_nodes = \
                 show_metric_and_node_selection_for_graph(metrics, nodes, graph_number)
-            nodes = []
-            for node in selected_nodes:
-                step, index = node_to_step_index_map[node]
-                nodes.append((step, index))
-            structure_graph_data(chips, metric, selected_jobs, nodes)
+            nodes_as_step_and_index = []
+            for selected_node in selected_nodes:
+                step, index = NODE_TO_STEP_INDEX_MAP[selected_node]
+                nodes_as_step_and_index.append((step, index))
+            structure_graph_data(chips, metric, selected_jobs, nodes_as_step_and_index)
             if not (graph_number == graphs or graph_number == graphs - 1):
                 streamlit.divider()
         graph_number += 1
@@ -843,7 +834,10 @@ def make_tabs(metric_dataframe, chip):
     Creates all the tabs. Displays the modules for the tabs that may or may not exist
     which include the graphs tab and design preview tab. Returns the rest of the tabs.
 
-    A
+    Args:
+        metric_dataframe (pandas.DataFrame) : A dataframe full of all metrics and all
+            nodes of the selected chip
+        chip (Chip) : The chip object that contains the schema read from.
     '''
     if 'flowgraph' not in streamlit.session_state:
         streamlit.session_state['flowgraph'] = True
@@ -872,32 +866,32 @@ def make_tabs(metric_dataframe, chip):
 
 # TODO find more descriptive way to describe layouts
 layout = 'vertical_flowgraph'
-new_chip = show_title_and_runs()
 # gathering data
-metric_dataframe = report.make_metric_dataframe(new_chip)
-node_to_step_index_map = make_node_to_step_index_map(metric_dataframe)
-metric_to_metric_unit_map = make_metric_to_metric_unit_map(metric_dataframe)
-manifest = report.make_manifest(new_chip)
+metric_dataframe = report.make_metric_dataframe(selected_chip)
+NODE_TO_STEP_INDEX_MAP, metric_dataframe = make_node_to_step_index_map(metric_dataframe)
+METRIC_TO_METRIC_UNIT_MAP, metric_dataframe = make_metric_to_metric_unit_map(metric_dataframe)
+MANIFEST = report.make_manifest(selected_chip)
 if layout == 'vertical_flowgraph':
-    metrics_tab, manifest_tab, file_viewer_tab = make_tabs(metric_dataframe, new_chip)
+    show_title_and_runs()
+    metrics_tab, manifest_tab, file_viewer_tab = make_tabs(metric_dataframe, selected_chip)
     ui_width = streamlit_javascript.st_javascript("window.innerWidth")
     with metrics_tab:
         node_from_flowgraph, datafram_and_node_info_col = \
-            flowgraph_layout_vertical_flowgraph(new_chip, ui_width)
+            flowgraph_layout_vertical_flowgraph(selected_chip, ui_width)
         with datafram_and_node_info_col:
             metrics_dataframe_module(metric_dataframe)
             streamlit.header('Node Information')
             metrics_col, records_col, logs_and_reports_col = streamlit.columns(3, gap='small')
             selected_node = select_nodes(metric_dataframe, node_from_flowgraph)
-            step, index = node_to_step_index_map[selected_node]
+            step, index = NODE_TO_STEP_INDEX_MAP[selected_node]
             with metrics_col:
                 node_metric_dataframe(selected_node, metric_dataframe[selected_node].dropna())
             with records_col:
-                node_details_dataframe(new_chip, step, index)
+                node_details_dataframe(selected_chip, step, index)
             with logs_and_reports_col:
-                display_file_content = show_files(new_chip, step, index)
-                show_metrics_for_file(new_chip, step, index)
+                display_file_content = show_files(selected_chip, step, index)
+                show_metrics_for_file(selected_chip, step, index)
     with manifest_tab:
-        manifest_module(new_chip, manifest, ui_width)
+        manifest_module(selected_chip, ui_width)
     with file_viewer_tab:
-        file_viewer_module(display_file_content, new_chip, step, index)
+        file_viewer_module(display_file_content, selected_chip, step, index)
