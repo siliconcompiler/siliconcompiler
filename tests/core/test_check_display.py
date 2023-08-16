7e0697b0f33e7a89fb4071d3f9f66cb1a0d71302
Martin Troiber <martin@zeroasic.com>
2023-08-16 16:09:50 +0100

    Make modified environment a proper fixture

--- tests/core/test_check_display.py
+++ tests/core/test_check_display.py
@@ -2,14 +2,17 @@ import siliconcompiler
 import os
 from siliconcompiler.tools.builtin import nop
 from unittest.mock import patch
+import pytest
 
 
-names_to_remove = {'DISPLAY', 'WAYLAND_DISPLAY'}
-modified_environ = {k: v for k, v in os.environ.items() if k not in names_to_remove}
+@pytest.fixture
+def modified_environ():
+    names_to_remove = {'DISPLAY', 'WAYLAND_DISPLAY'}
+    return {k: v for k, v in os.environ.items() if k not in names_to_remove}
 
 
 @patch('sys.platform', 'linux')
-def test_check_display_run():
+def test_check_display_run(modified_environ):
     # Checks if _check_display() is called during run()
     chip = siliconcompiler.Chip('test')
 
@@ -23,7 +26,7 @@ def test_check_display_run():
 
 
 @patch('sys.platform', 'linux')
-def test_check_display_nodisplay():
+def test_check_display_nodisplay(modified_environ):
     # Checks if the nodisplay option is set
     # On linux system without display
     with patch.dict(os.environ, modified_environ, clear=True):
@@ -53,7 +56,7 @@ def test_check_display_with_display_wayland():
 
 
 @patch('sys.platform', 'darwin')
-def test_check_display_with_display_macos():
+def test_check_display_with_display_macos(modified_environ):
     # Checks that the nodisplay option is not set
     # On macos system
     with patch.dict(os.environ, modified_environ, clear=True):
@@ -63,7 +66,7 @@ def test_check_display_with_display_macos():
 
 
 @patch('sys.platform', 'win32')
-def test_check_display_with_display_windows():
+def test_check_display_with_display_windows(modified_environ):
     # Checks that the nodisplay option is not set
     # On windows system
     with patch.dict(os.environ, modified_environ, clear=True):
