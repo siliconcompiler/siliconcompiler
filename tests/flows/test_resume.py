ffef21ff1d1d6a0b802258a87170a705947dc18c
Will <5217539+WRansohoff@users.noreply.github.com>
2023-09-04 22:02:27 -0400

    Add a test for resuming a job remotely.

--- tests/flows/test_resume.py
+++ tests/flows/test_resume.py
@@ -1,7 +1,8 @@
 import siliconcompiler
 
-import pytest
 import os
+import pytest
+import subprocess
 
 
 @pytest.mark.eda
@@ -36,3 +37,45 @@ def test_resume(gcd_chip):
     # Ensure flow finished successfully
     assert gcd_chip.find_result('def', step='place') is not None
     assert gcd_chip.find_result('gds', step='export') is not None
+
+@pytest.mark.eda
+@pytest.mark.timeout(600)
+def test_resume_remote(gcd_chip, unused_tcp_port):
+    '''Test that jobs can be resumed on a remote server.
+    '''
+
+    # Start running an sc-server instance.
+    os.mkdir('local_server_work')
+    srv_proc = subprocess.Popen(['sc-server',
+                                 '-port', str(unused_tcp_port),
+                                 '-nfs_mount', './local_server_work',
+                                 '-cluster', 'local'])
+
+    # Set a value that will cause place to break
+    gcd_chip.set('tool', 'openroad', 'task', 'place', 'var', 'place_density', 'asdf',
+                 step='place', index='0')
+
+    with pytest.raises(siliconcompiler.SiliconCompilerError):
+        gcd_chip.run()
+
+    # Ensure flow failed at placement, and store last modified time of floorplan
+    fp_result = gcd_chip.find_result('def', step='floorplan')
+    assert fp_result is not None
+    old_fp_mtime = os.path.getmtime(fp_result)
+
+    assert gcd_chip.find_result('def', step='place') is None
+    assert gcd_chip.find_result('gds', step='export') is None
+
+    # Fix place step and re-run remotely.
+    gcd_chip.set('tool', 'openroad', 'task', 'place', 'var', 'place_density', '0.40',
+                 step='place', index='0')
+    gcd_chip.set('option', 'remote', True)
+    gcd_chip.set('option', 'resume', True)
+    gcd_chip.run()
+
+    # Kill the server process.
+    srv_proc.kill()
+
+    # Ensure flow finished successfully
+    assert os.path.isfile('build/gcd/job0/place/0/outputs/gcd.pkg.json')
+    assert os.path.isfile('build/gcd/job0/export/0/outputs/gcd.pkg.json')
