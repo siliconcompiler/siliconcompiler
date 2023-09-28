c9e106c36b62db4afd8a7c11b4a48ca5d1d38026
Will <5217539+WRansohoff@users.noreply.github.com>
2023-09-28 15:08:59 -0400

    First pass at adding a Windows path test to daily CI.

---
+++ tests/schema/test_windows_paths.py
@@ -0,0 +1,31 @@
+import os
+
+from siliconcompiler import Chip
+
+@pytest.mark.win32
+def test_windows_path_compat():
+    '''
+    Test that SC stores POSIX-style paths internally, while still
+    providing Windows-style paths when necessary on a Windows system.
+    '''
+
+    # Create a test file using Windows file paths.
+    os.makedirs('d:\\\\windows_dir', exist_ok=True)
+    windows_path = 'd:\\\\windows_dir\\windows_file.v'
+    windows_content = '// Test file'
+    with open(windows_path, 'w') as wf:
+        wf.write(windows_content)
+
+    # Create a Schema, and set the file path using Windows notation.
+    chip = Chip('path_test')
+    chip.input(windows_path)
+
+    # Verify that the Schema path is POSIX-style.
+    path = chip.get('input', 'verilog')[0]
+    assert path == pathlib.Path(windows_path).as_posix()
+
+    # Verify that SC still uses the Windows path on a Windows system.
+    win_path = chip.find_files('input', 'verilog')[0]
+    assert os.path.isfile(win_path)
+    with open(win_path, 'r') as rf:
+        assert rf.read() == windows_content
