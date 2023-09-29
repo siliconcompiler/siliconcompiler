55e8db3f5cc2a199048860df947270fc3afaae34
Martin Troiber <martin@zeroasic.com>
2023-09-29 17:29:00 +0100

    Change schema patch version back

--- siliconcompiler/schema/schema_cfg.py
+++ siliconcompiler/schema/schema_cfg.py
@@ -11,7 +11,7 @@ try:
 except ImportError:
     from siliconcompiler.schema.utils import trim
 
-SCHEMA_VERSION = '0.36.1'
+SCHEMA_VERSION = '0.36.0'
 
 
 #############################################################################
