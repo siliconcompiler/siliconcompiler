89953b79a42f181e98bdb9db982ae731a2700caa
Peter Gadfort <gadfort@zeroasic.com>
2023-10-05 15:47:08 -0400

    testcases for nodes_to_execute

--- tests/core/test_nodes_to_execute.py
+++ tests/core/test_nodes_to_execute.py
@@ -1,6 +1,7 @@
 import siliconcompiler
 
 from siliconcompiler.tools.builtin import join
+from siliconcompiler.tools.builtin import nop
 
 
 def test_nodes_to_execute():
@@ -20,6 +21,208 @@ def test_nodes_to_execute():
 
     chip.set('option', 'flow', flow)
 
-    chip.write_flowgraph('test_list_steps.png')
-
     assert chip.nodes_to_execute() == [('A', '0'), ('B', '0'), ('C', '0'), ('D', '0')]
+
+
+def test_nodes_to_execute_to():
+    '''
+    Check to ensure forked graph is handled properly with to
+    '''
+    chip = siliconcompiler.Chip('test')
+    flow = 'test'
+
+    prev = None
+    for n in ('A', 'B', 'C', 'D'):
+        chip.node(flow, n, nop)
+        chip.node(flow, n + 'a', nop)
+        chip.edge(flow, n, n + 'a')
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    chip.set('option', 'flow', flow)
+    chip.set('option', 'to', 'Ba')
+
+    assert chip.nodes_to_execute() == [
+        ('A', '0'),
+        ('B', '0'),
+        ('Ba', '0')
+    ]
+
+
+def test_nodes_to_execute_to_multiple():
+    '''
+    Check to ensure forked graph is handled properly with to and multiple ends
+    '''
+    chip = siliconcompiler.Chip('test')
+    flow = 'test'
+
+    prev = None
+    for n in ('A', 'B', 'C', 'D'):
+        chip.node(flow, n, nop)
+        chip.node(flow, n + 'a', nop)
+        chip.edge(flow, n, n + 'a')
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    chip.set('option', 'flow', flow)
+    chip.set('option', 'to', ['Ba', 'Da'])
+
+    assert chip.nodes_to_execute() == [
+        ('A', '0'),
+        ('B', '0'),
+        ('Ba', '0'),
+        ('C', '0'),
+        ('D', '0'),
+        ('Da', '0')
+    ]
+
+
+def test_nodes_to_execute_from():
+    '''
+    Check to ensure forked graph is handled properly with from
+    '''
+    chip = siliconcompiler.Chip('test')
+    flow = 'test'
+
+    prev = None
+    for n in ('A', 'B', 'C', 'D'):
+        chip.node(flow, n, nop)
+        chip.node(flow, n + 'a', nop)
+        chip.edge(flow, n, n + 'a')
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    chip.set('option', 'flow', flow)
+    chip.set('option', 'from', 'B')
+
+    assert chip.nodes_to_execute() == [
+        ('B', '0'),
+        ('Ba', '0'),
+        ('C', '0'),
+        ('Ca', '0'),
+        ('D', '0'),
+        ('Da', '0')
+    ]
+
+
+def test_nodes_to_execute_from_to():
+    '''
+    Check to ensure forked graph is handled properly with from/to
+    '''
+    chip = siliconcompiler.Chip('test')
+    flow = 'test'
+
+    prev = None
+    for n in ('A', 'B', 'C', 'D'):
+        chip.node(flow, n, nop)
+        chip.node(flow, n + 'a', nop)
+        chip.edge(flow, n, n + 'a')
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    chip.set('option', 'flow', flow)
+    chip.set('option', 'from', 'B')
+    chip.set('option', 'to', 'Ca')
+
+    assert chip.nodes_to_execute() == [
+        ('B', '0'),
+        ('C', '0'),
+        ('Ca', '0')
+    ]
+
+
+def test_nodes_to_execute_different_flow():
+    '''
+    Check to ensure nodes_to_execute properly handles multiple flows
+    '''
+    chip = siliconcompiler.Chip('test')
+    flow = 'test'
+
+    prev = None
+    for n in ('A', 'B', 'C', 'D'):
+        chip.node(flow, n, nop)
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    flow = 'test2'
+
+    prev = None
+    for n in ('E', 'F', 'G', 'H'):
+        chip.node(flow, n, nop)
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    chip.set('option', 'flow', flow)
+    assert chip.nodes_to_execute(flow='test') == [
+        ('A', '0'),
+        ('B', '0'),
+        ('C', '0'),
+        ('D', '0')
+    ]
+    assert chip.nodes_to_execute(flow='test2') == [
+        ('E', '0'),
+        ('F', '0'),
+        ('G', '0'),
+        ('H', '0')
+    ]
+
+
+def test_nodes_to_execute_different_flow_from_to():
+    '''
+    Check to ensure nodes_to_execute only applies from/to/prune
+    to the correct flow
+    '''
+    chip = siliconcompiler.Chip('test')
+    flow = 'test'
+
+    prev = None
+    for n in ('A', 'B', 'C', 'D'):
+        chip.node(flow, n, nop)
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    flow = 'test2'
+
+    prev = None
+    for n in ('E', 'F', 'G', 'H'):
+        chip.node(flow, n, nop)
+
+        if prev:
+            chip.edge(flow, prev, n)
+
+        prev = n
+
+    chip.set('option', 'flow', 'test')
+    chip.set('option', 'from', 'A')
+    chip.set('option', 'to', 'B')
+    assert chip.nodes_to_execute(flow='test') == [
+        ('A', '0'),
+        ('B', '0')
+    ]
+    assert chip.nodes_to_execute(flow='test2') == [
+        ('E', '0'),
+        ('F', '0'),
+        ('G', '0'),
+        ('H', '0')
+    ]
