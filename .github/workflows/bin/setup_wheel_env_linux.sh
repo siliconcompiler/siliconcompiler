d5d504958b41ba651e686523a2f1d2fe5677cd65
Martin Troiber <martin@zeroasic.com>
2023-10-16 16:28:22 +0200

    Fix wget line break

--- .github/workflows/bin/setup_wheel_env_linux.sh
+++ .github/workflows/bin/setup_wheel_env_linux.sh
@@ -7,7 +7,7 @@ yum install -y zlib-devel graphviz xorg-x11-server-Xvfb wget
 # Install Klayout (for chip.show() test)
 klayout_version=$(python3 ${src_path}/setup/_tools.py --tool klayout --field version)
 wget --no-check-certificate \
-    -O klayout.rpm
+    -O klayout.rpm \
     "https://www.klayout.org/downloads/CentOS_7/klayout-${klayout_version}-0.x86_64.rpm"
 yum install -y python3 ruby qt-x11
 # This may fail on ARM64
