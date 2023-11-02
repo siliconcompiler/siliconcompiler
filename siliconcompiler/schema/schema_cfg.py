c7dc49f4f769ce3eb5aa75d23b862c5a6c2bb23f
Martin Troiber <martin@zeroasic.com>
2023-11-02 18:41:12 +0000

    Update schema

--- siliconcompiler/schema/schema_cfg.py
+++ siliconcompiler/schema/schema_cfg.py
@@ -11,7 +11,7 @@ try:
 except ImportError:
     from siliconcompiler.schema.utils import trim
 
-SCHEMA_VERSION = '0.37.2'
+SCHEMA_VERSION = '0.38.0'
 
 
 #############################################################################
