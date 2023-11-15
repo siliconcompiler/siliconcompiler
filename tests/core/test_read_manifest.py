c519c70a7e4f7e8e3001f7b86dede13214f9cf94
Peter Gadfort <gadfort@zeroasic.com>
2023-11-15 09:31:55 -0500

    avoid using Chip initializer for schema checking

--- tests/core/test_read_manifest.py
+++ tests/core/test_read_manifest.py
@@ -29,30 +29,21 @@ def test_modified_schema(datadir):
     '''Make sure schema has not been modified without updating defaults.json'''
 
     # gets default from schema
-    chip = siliconcompiler.Chip('test')
-    chip.schema._remove('dependency', 'siliconcompiler')
+    schema = siliconcompiler.Schema()
 
     # expected
     with open(os.path.join(datadir, 'defaults.json'), 'r') as f:
         expected = json.load(f)
 
-    # special case (initialized in constructor)
-    glbl_key = siliconcompiler.Schema.GLOBAL_KEY
-    expected['design']['node'][glbl_key] = {}
-    expected['design']['node'][glbl_key][glbl_key] = {
-        'value': 'test',
-        'signature': None
-    }
-
-    assert chip.schema.cfg == expected
+    assert schema.cfg == expected
 
 
 # Use nostrict mark to prevent changing default value of [option, strict]
 @pytest.mark.nostrict
 def test_last_schema(datadir):
-    chip = siliconcompiler.Chip('test')
-    chip.schema._remove('dependency', 'siliconcompiler')
-    current_version = packaging.version.Version(chip.get('schemaversion'))
+    schema = siliconcompiler.Schema()
+
+    current_version = packaging.version.Version(schema.get('schemaversion'))
     # Check last version of schema
     last_schema = Schema(manifest=os.path.join(datadir, 'last_minor.json'))
 
