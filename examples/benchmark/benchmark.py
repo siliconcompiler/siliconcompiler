db42c64037e3472fc02a3c6a8dc7790c87d4cca2
Felix Schneider <felix.schneider@fh-dortmund.de>
2024-01-17 17:33:11 +0100

    Fix benchmark example

--- examples/benchmark/benchmark.py
+++ examples/benchmark/benchmark.py
@@ -1,16 +1,12 @@
 import os
 import time
 import siliconcompiler
-from siliconcompiler.flows import asicflow
 
 # Setting up the experiment
-
-
 def main():
     rootdir = os.path.dirname(__file__)
 
     examples = [{'heartbeat': 1000},
-                {'picorv32': 1000},
                 {'aes': 1500}]
 
     results = {}
@@ -18,34 +14,33 @@ def main():
     for item in examples:
         design = list(item.keys())[0]
         size = list(item.values())[0]
-        parent = os.path.abspath("..")
+        parent = os.path.abspath(rootdir + "/..")
         rootdir = os.path.join(parent, design)
         results[design] = {}
-        for n in ['1', '2', '4', '8', '16']:
+        for n in [1, 2, 4, 8, 16]:
             wall_start = time.time()
             chip = siliconcompiler.Chip(design)
-            chip.set('jobname', f"job{n}")
-            chip.load_target("skywater130_demo")
-            chip.set('relax', True)
-            chip.set('quiet', True)
-            chip.set('remote', False)
+            chip.set('option', 'jobname', f"job{n}")
+            chip.set('option', 'relax', True)
+            chip.set('option', 'quiet', True)
+            chip.set('option', 'remote', False)
 
-            # load dsign
+            asic_flow_args = {
+                'syn_np': n,
+                'place_np': n,
+                'cts_np': n,
+                'route_np': n
+            }
+            chip.load_target("skywater130_demo", **asic_flow_args)
+            
+            # Set router to 1 thread to not interfere with measurement
+            chip.set('tool', 'openroad', 'task', 'route', 'threads', 1)
+
+            # load design
             chip.input(os.path.join(rootdir, f"{design}.v"))
             chip.input(os.path.join(rootdir, f"{design}.sdc"))
             chip.set('constraint', 'outline', [(0, 0), (size, size)])
             chip.set('constraint', 'corearea', [(10, 10), (size - 10, size - 10)])
-
-            # load flow
-            chip.set('flowarg', 'syn_np', n)
-            chip.set('flowarg', 'place_np', n)
-            chip.set('flowarg', 'cts_np', n)
-            chip.use(asicflow)
-            chip.set('flow', 'asicflow')
-
-            # Set router to 1 thread to not interfere with measurement
-            for i in range(int(n)):
-                chip.set('eda', 'openroad', 'threads', 'route', str(i), 1)
 
             # RUN
             chip.run()
@@ -55,8 +50,8 @@ def main():
             wall_end = time.time()
             walltime = round((wall_end - wall_start), 2)
             results[design][n] = walltime
-            with open("results.txt", 'w') as f:
-                f.write(results)
+            with open("results.txt", 'a') as f:
+                f.write(f"{design}, {n}, {walltime}\n")
 
 
 if __name__ == '__main__':
