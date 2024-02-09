2027adfab59e73bf30bbe9615705bbf2ee702c33
Peter Gadfort <gadfort@zeroasic.com>
2024-02-09 09:57:03 -0500

    fix verilog lint

--- examples/heartbeat_padring/heartbeat_top.v
+++ examples/heartbeat_padring/heartbeat_top.v
@@ -68,93 +68,96 @@ module heartbeat_top (
 
     `include "la_iopadring.vh"
 
-    localparam CELLMAP = { // GPIO SECTION
-                        {8'h0,PIN_NONE,LA_VSS},
-                        {8'h0,PIN_NONE,LA_VDD},
-                        {8'h0,PIN_NONE,LA_VDDIO},
-                        {8'h0,PIN_NONE,LA_VSSIO},
-                        {8'h0,PIN_IO0,LA_BIDIR},
-                        {8'h0,PIN_IO1,LA_BIDIR},
-                        {8'h0,PIN_IO2,LA_BIDIR},
-                        {8'h0,PIN_IO3,LA_BIDIR},
-                        {8'h0,PIN_IO4,LA_BIDIR},
-                        {8'h0,PIN_IO5,LA_BIDIR},
-                        {8'h0,PIN_IO6,LA_BIDIR},
-                        {8'h0,PIN_IO7,LA_BIDIR},
-                        {8'h0,PIN_IO8,LA_BIDIR}};
+    localparam CELLMAP = {  // GPIO SECTION
+        {8'h0,PIN_NONE,LA_VSS},
+        {8'h0,PIN_NONE,LA_VDD},
+        {8'h0,PIN_NONE,LA_VDDIO},
+        {8'h0,PIN_NONE,LA_VSSIO},
+        {8'h0,PIN_IO0,LA_BIDIR},
+        {8'h0,PIN_IO1,LA_BIDIR},
+        {8'h0,PIN_IO2,LA_BIDIR},
+        {8'h0,PIN_IO3,LA_BIDIR},
+        {8'h0,PIN_IO4,LA_BIDIR},
+        {8'h0,PIN_IO5,LA_BIDIR},
+        {8'h0,PIN_IO6,LA_BIDIR},
+        {8'h0,PIN_IO7,LA_BIDIR},
+        {8'h0,PIN_IO8,LA_BIDIR}
+    };
     wire [7:0] ioring;
 
-    la_iopadring #(.RINGW(8),
-                   .CFGW(18),
-                   //north
-                   .NO_NPINS(9),
-                   .NO_NCELLS(13),
-                   .NO_NSECTIONS(1),
-                   .NO_CELLMAP(CELLMAP),
-                   //east
-                   .EA_NPINS(9),
-                   .EA_NCELLS(13),
-                   .EA_NSECTIONS(1),
-                   .EA_CELLMAP(CELLMAP),
-                   //south
-                   .SO_NPINS(9),
-                   .SO_NCELLS(13),
-                   .SO_NSECTIONS(1),
-                   .SO_CELLMAP(CELLMAP),
-                   //west
-                   .WE_NPINS(9),
-                   .WE_NCELLS(13),
-                   .WE_NSECTIONS(1),
-                   .WE_CELLMAP(CELLMAP))
-    padring(// Inouts
-            .no_pad         (no_pad),
-            .ea_pad         (ea_pad),
-            .so_pad         (so_pad),
-            .we_pad         (we_pad),
-            .no_aio         (),
-            .ea_aio         (),
-            .so_aio         (),
-            .we_aio         (),
-            .no_vddio       (vddio),
-            .ea_vddio       (vddio),
-            .so_vddio       (vddio),
-            .we_vddio       (vddio),
-            /*AUTOINST*/
-            // Outputs
-            .no_z           (no_din),
-            .ea_z           (ea_din),
-            .so_z           (so_din),
-            .we_z           (we_din),
-            // Inouts
-            .vss            (vss),
-            .no_vdd         (vdd),
-            .no_vssio       (vssio),
-            .no_ioring      (ioring),
-            .ea_vdd         (vdd),
-            .ea_vssio       (vssio),
-            .ea_ioring      (ioring),
-            .so_vdd         (vdd),
-            .so_vssio       (vssio),
-            .so_ioring      (ioring),
-            .we_vdd         (vdd),
-            .we_vssio       (vssio),
-            .we_ioring      (ioring),
-            // Inputs
-            .no_a           (no_dout),
-            .no_ie          (no_ie),
-            .no_oe          (~no_oen),
-            .no_cfg         (no_tech_cfg),
-            .ea_a           (ea_dout),
-            .ea_ie          (ea_ie),
-            .ea_oe          (~ea_oen),
-            .ea_cfg         (ea_tech_cfg),
-            .so_a           (so_dout),
-            .so_ie          (so_ie),
-            .so_oe          (~so_oen),
-            .so_cfg         (so_tech_cfg),
-            .we_a           (we_dout),
-            .we_ie          (we_ie),
-            .we_oe          (~we_oen),
-            .we_cfg         (we_tech_cfg));
+    la_iopadring #(
+        .RINGW(8),
+        .CFGW(18),
+        //north
+        .NO_NPINS(9),
+        .NO_NCELLS(13),
+        .NO_NSECTIONS(1),
+        .NO_CELLMAP(CELLMAP),
+        //east
+        .EA_NPINS(9),
+        .EA_NCELLS(13),
+        .EA_NSECTIONS(1),
+        .EA_CELLMAP(CELLMAP),
+        //south
+        .SO_NPINS(9),
+        .SO_NCELLS(13),
+        .SO_NSECTIONS(1),
+        .SO_CELLMAP(CELLMAP),
+        //west
+        .WE_NPINS(9),
+        .WE_NCELLS(13),
+        .WE_NSECTIONS(1),
+        .WE_CELLMAP(CELLMAP)
+    ) padring (  // Inouts
+        .no_pad         (no_pad),
+        .ea_pad         (ea_pad),
+        .so_pad         (so_pad),
+        .we_pad         (we_pad),
+        .no_aio         (),
+        .ea_aio         (),
+        .so_aio         (),
+        .we_aio         (),
+        .no_vddio       (vddio),
+        .ea_vddio       (vddio),
+        .so_vddio       (vddio),
+        .we_vddio       (vddio),
+        /*AUTOINST*/
+        // Outputs
+        .no_z           (no_din),
+        .ea_z           (ea_din),
+        .so_z           (so_din),
+        .we_z           (we_din),
+        // Inouts
+        .vss            (vss),
+        .no_vdd         (vdd),
+        .no_vssio       (vssio),
+        .no_ioring      (ioring),
+        .ea_vdd         (vdd),
+        .ea_vssio       (vssio),
+        .ea_ioring      (ioring),
+        .so_vdd         (vdd),
+        .so_vssio       (vssio),
+        .so_ioring      (ioring),
+        .we_vdd         (vdd),
+        .we_vssio       (vssio),
+        .we_ioring      (ioring),
+        // Inputs
+        .no_a           (no_dout),
+        .no_ie          (no_ie),
+        .no_oe          (~no_oen),
+        .no_cfg         (no_tech_cfg),
+        .ea_a           (ea_dout),
+        .ea_ie          (ea_ie),
+        .ea_oe          (~ea_oen),
+        .ea_cfg         (ea_tech_cfg),
+        .so_a           (so_dout),
+        .so_ie          (so_ie),
+        .so_oe          (~so_oen),
+        .so_cfg         (so_tech_cfg),
+        .we_a           (we_dout),
+        .we_ie          (we_ie),
+        .we_oe          (~we_oen),
+        .we_cfg         (we_tech_cfg)
+    );
 
 endmodule
