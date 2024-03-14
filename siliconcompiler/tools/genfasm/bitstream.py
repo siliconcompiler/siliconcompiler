266d7c9c433faba4ab02a285ed4921dfb639e2a6
Peter Gadfort <gadfort@zeroasic.com>
2024-03-14 11:02:36 -0400

    add metrics to genfasm

--- siliconcompiler/tools/genfasm/bitstream.py
+++ siliconcompiler/tools/genfasm/bitstream.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from siliconcompiler.tools.vpr import vpr
 
 
 def setup(chip):
@@ -17,6 +18,11 @@ def setup(chip):
     chip.set('tool', tool, 'task', task, 'threads', os.cpu_count(),
              step=step, index=index, clobber=False)
 
+    chip.set('tool', tool, 'task', task, 'regex', 'warnings', "^Warning",
+             step=step, index=index, clobber=False)
+    chip.set('tool', tool, 'task', task, 'regex', 'errors', "^Error",
+             step=step, index=index, clobber=False)
+
 
 ################################
 # Post_process (post executable)
@@ -26,8 +32,7 @@ def setup(chip):
 def post_process(chip):
     ''' Tool specific function to run after step execution
     '''
+    vpr.vpr_post_process(chip)
 
     design = chip.top()
     shutil.move(f'{design}.fasm', 'outputs')
-    # TODO: return error code
-    return 0
