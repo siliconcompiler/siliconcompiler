1a9f410d821ab250bd46698d0d5d63c4261f0564
petergrossmann21 <peter@zeroasic.com>
2024-03-28 14:23:45 -0400

    Run code through format_verilog shell script

--- examples/fpga_flow/designs/register_file/register_file.v
+++ examples/fpga_flow/designs/register_file/register_file.v
@@ -1,27 +1,25 @@
-module register_file
-  # (
-     parameter DEPTH=32,
-     parameter DATA_WIDTH=32,
-     parameter ADDR_BITS=$clog2(DEPTH)
-     )
-   (
-    input 		      clk,
-    input 		      wen,
-    input [(ADDR_BITS-1):0]   wraddr,
-    input [(ADDR_BITS-1):0]   rdaddr,
-    input [(DATA_WIDTH-1):0]  datain,
+module register_file #(
+    parameter DEPTH = 32,
+    parameter DATA_WIDTH = 32,
+    parameter ADDR_BITS = $clog2(DEPTH)
+) (
+    input                     clk,
+    input                     wen,
+    input  [ (ADDR_BITS-1):0] wraddr,
+    input  [ (ADDR_BITS-1):0] rdaddr,
+    input  [(DATA_WIDTH-1):0] datain,
     output [(DATA_WIDTH-1):0] dataout
-    );
+);
 
-   reg [(DATA_WIDTH-1):0]       reg_file[0:(DEPTH-1)];
+    reg [(DATA_WIDTH-1):0] reg_file[0:(DEPTH-1)];
 
-   always @(posedge clk) begin
-      if (wen) begin
-	 reg_file[wraddr] <= datain;
-      end
-   end
+    always @(posedge clk) begin
+        if (wen) begin
+            reg_file[wraddr] <= datain;
+        end
+    end
 
-   assign dataout = reg_file[rdaddr];
-      
-   
-endmodule // register_file
+    assign dataout = reg_file[rdaddr];
+
+
+endmodule  // register_file
