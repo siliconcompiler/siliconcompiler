3b7b3d2af8493c231b4e8616afc268de7abc2bf4
petergrossmann21 <peter@zeroasic.com>
2024-03-30 16:20:58 -0400

    Fix lint errors

--- tests/flows/test_fpga_xml_constraints.py
+++ tests/flows/test_fpga_xml_constraints.py
@@ -10,9 +10,9 @@ from siliconcompiler.targets import fpgaflow_demo
 @pytest.mark.eda
 @pytest.mark.quick
 def test_fpga_xml_constraints(scroot,
-                          arch_name='example_arch_X014Y014',
-                          benchmark_name='adder',
-                          top_module='adder'):
+                              arch_name='example_arch_X014Y014',
+                              benchmark_name='adder',
+                              top_module='adder'):
 
     chip = siliconcompiler.Chip(f'{top_module}')
 
@@ -27,11 +27,11 @@ def test_fpga_xml_constraints(scroot,
 
     # 3. Set placement constraints
     xml_constraints = os.path.join(flow_root,
-                                    'designs',
-                                    benchmark_name,
-                                    'constraints',
-                                    f'pin_constraints_{arch_name}.xml')
-    
+                                   'designs',
+                                   benchmark_name,
+                                   'constraints',
+                                   f'pin_constraints_{arch_name}.xml')
+
     chip.add('input', 'constraint', 'pins', xml_constraints)
 
     # 4. Load target
@@ -46,4 +46,4 @@ def test_fpga_xml_constraints(scroot,
 
 if __name__ == "__main__":
     from tests.fixtures import scroot
-    test_fpga_constraints(scroot())
+    test_fpga_xml_constraints(scroot())
