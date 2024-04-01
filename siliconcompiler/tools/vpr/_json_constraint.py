aace908f8d04867fb3cf8b78c4c4af2624a43e91
petergrossmann21 <peter@zeroasic.com>
2024-04-01 11:49:41 -0400

    Relocate testing code

--- siliconcompiler/tools/vpr/_json_constraint.py
+++ siliconcompiler/tools/vpr/_json_constraint.py
@@ -4,35 +4,6 @@ import json
 from siliconcompiler.tools.vpr._xml_constraint import generate_vpr_constraints_xml_file
 
 
-def main():
-
-    option_parser = argparse.ArgumentParser()
-    option_parser.add_argument("-constraints_map",
-                               help="architecture-specific mapping of constraint pins "
-                                    "to FPGA core pins")
-    option_parser.add_argument("json_constraints",
-                               help="architecture-independent constraints file")
-    option_parser.add_argument("constraints_file_out",
-                               help="constraints XML file name")
-
-    options = option_parser.parse_args()
-
-    json_constraints_file = options.json_constraints
-    constraints_map_file = options.constraints_map
-    constraints_file_out = options.constraints_file_out
-
-    json_generic_constraints = load_json_constraints(json_constraints_file)
-    if (constraints_map_file):
-        constraints_map = load_constraints_map(constraints_map_file)
-    else:
-        constraints_map = {}
-
-    mapped_constraints = map_constraints(json_generic_constraints,
-                                         constraints_map)
-
-    generate_vpr_constraints_xml_file(mapped_constraints, constraints_file_out)
-
-
 def load_json_constraints(json_constraints_file):
 
     json_generic_constraints = {}
@@ -93,4 +64,28 @@ def map_constraints(json_generic_constraints,
 
 
 if __name__ == "__main__":
-    main()
+    option_parser = argparse.ArgumentParser()
+    option_parser.add_argument("-constraints_map",
+                               help="architecture-specific mapping of constraint pins "
+                                    "to FPGA core pins")
+    option_parser.add_argument("json_constraints",
+                               help="architecture-independent constraints file")
+    option_parser.add_argument("constraints_file_out",
+                               help="constraints XML file name")
+
+    options = option_parser.parse_args()
+
+    json_constraints_file = options.json_constraints
+    constraints_map_file = options.constraints_map
+    constraints_file_out = options.constraints_file_out
+
+    json_generic_constraints = load_json_constraints(json_constraints_file)
+    if (constraints_map_file):
+        constraints_map = load_constraints_map(constraints_map_file)
+    else:
+        constraints_map = {}
+
+    mapped_constraints = map_constraints(json_generic_constraints,
+                                         constraints_map)
+
+    generate_vpr_constraints_xml_file(mapped_constraints, constraints_file_out)
