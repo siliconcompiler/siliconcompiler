e2a34e4c223be82b76577d4f344382910f9c03ed
Peter Gadfort <gadfort@zeroasic.com>
2024-04-24 10:09:56 -0400

    fix collection test

--- tests/core/test_collect.py
+++ tests/core/test_collect.py
@@ -43,7 +43,7 @@ def test_collect_file_copyall():
     chip._collect()
 
     # check that all file are copied (input, library, and pdk)
-    assert len(os.listdir(chip._getcollectdir())) == 27
+    assert len(os.listdir(chip._getcollectdir())) == 40
 
 
 def test_collect_file_copyall_with_false():
@@ -54,7 +54,7 @@ def test_collect_file_copyall_with_false():
     chip._collect()
 
     # check that all file are copied (input, library, and pdk)
-    assert len(os.listdir(chip._getcollectdir())) == 27
+    assert len(os.listdir(chip._getcollectdir())) == 40
 
 
 def test_collect_file_with_false():
