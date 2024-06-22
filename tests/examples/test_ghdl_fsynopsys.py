eeebb42cb1912124e77f9cadeb70325449643c8f
Peter Gadfort <peter.gadfort@gmail.com>
2024-06-21 20:38:45 -0400

    run ghdl through whole flow

--- tests/examples/test_ghdl_fsynopsys.py
+++ tests/examples/test_ghdl_fsynopsys.py
@@ -9,6 +9,6 @@ def test_py():
     from ghdl_fsynopsys import build
     build.main()
 
-    outfile = 'build/binary_4_bit_adder_top/job0/syn/0/outputs/binary_4_bit_adder_top.vg'
+    outfile = 'build/binary_4_bit_adder_top/job0/write_gds/0/outputs/binary_4_bit_adder_top.gds'
 
     assert os.path.isfile(outfile)
