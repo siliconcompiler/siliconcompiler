8fa2d784c5431f0618457ab1b6e15ffe21311951
Peter Gadfort <gadfort@zeroasic.com>
2024-07-11 16:37:07 -0400

    version 0.24.1

--- siliconcompiler/_metadata.py
+++ siliconcompiler/_metadata.py
@@ -1,5 +1,5 @@
 # Version number following semver standard.
-version = '0.24.0'
+version = '0.24.1'
 
 # Default server address for remote runs, if unspecified.
 default_server = 'https://server.siliconcompiler.com'
