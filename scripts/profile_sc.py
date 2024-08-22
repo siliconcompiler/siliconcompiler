11463e9fa3e118060c100316f6eec6a08b97b35c
Peter Gadfort <gadfort@zeroasic.com>
2024-08-22 08:26:14 -0400

    add write_manifest to profiling

--- scripts/profile_sc.py
+++ scripts/profile_sc.py
@@ -13,7 +13,7 @@ import gprof2dot
 
 def print_stats(pr):
     ps = pstats.Stats(pr).sort_stats(SortKey.CUMULATIVE)
-    ps.print_stats()
+    ps.print_stats(20)
 
 
 def generate_graph(pr, name):
@@ -34,15 +34,53 @@ def generate_graph(pr, name):
         pass
 
 
-def run_read_manifest(pr):
-    chip = Chip('')
+def run_read_manifest(pr, extra):
+    remove_path = False
+    if not extra:
+        chip = Chip('write')
+        chip.load_target('freepdk45_demo')
+        chip.load_target('skywater130_demo')
+        chip.load_target('asap7_demo')
+
+        fd, path = tempfile.mkstemp(suffix='.json')
+        os.close(fd)
+
+        chip.write_manifest(path)
+        remove_path = True
+    else:
+        path = extra
+
+    chip = Chip('read')
 
     pr.enable()
-    chip.read_manifest('manifest.pkg.json')
+    chip.read_manifest(path)
+    pr.disable()
+
+    if remove_path:
+        os.remove(path)
+
+
+def run_write_manifest_abspath(pr, extra):
+    run_write_manifest(pr, extra, True)
+
+
+def run_write_manifest(pr, extra, abspath=False):
+    chip = Chip('write')
+    chip.load_target('freepdk45_demo')
+    chip.load_target('skywater130_demo')
+    chip.load_target('asap7_demo')
+    if extra:
+        chip.load_target(extra)
+
+    fd, path = tempfile.mkstemp(suffix='.json')
+    os.close(fd)
+
+    pr.enable()
+    chip.write_manifest(path, abspath=abspath)
     pr.disable()
 
 
-def run_asic_demo(pr):
+def run_asic_demo(pr, extra):
     chip = Chip('')
 
     pr.enable()
@@ -55,13 +93,27 @@ def run_asic_demo(pr):
 if __name__ == "__main__":
     tests = {
         'read_manifest': run_read_manifest,
-        'run_asic_demo': run_asic_demo,
+        'write_manifest': run_write_manifest,
+        'write_manifest_abspath': run_write_manifest_abspath,
+        'asic_demo': run_asic_demo,
         'all': None
     }
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--print', action='store_true')
-    parser.add_argument('--profile', choices=tests.keys(), default='all')
+    parser = argparse.ArgumentParser(
+        description='Utility tool to aide in profiling SiliconCompiler')
+    parser.add_argument(
+        '--print',
+        action='store_true',
+        help='Print the time stats from the profile')
+    parser.add_argument(
+        '--profile',
+        choices=tests.keys(),
+        default='all',
+        help='Profile test to run')
+    parser.add_argument(
+        '--extra',
+        metavar='<payload>',
+        help='extra payload information for test')
 
     args = parser.parse_args()
 
@@ -75,7 +127,7 @@ if __name__ == "__main__":
         print(f'Running {test}')
         func = tests[test]
         pr = cProfile.Profile()
-        func(pr)
+        func(pr, args.extra)
         generate_graph(pr, test)
         if args.print:
             print_stats(pr)
