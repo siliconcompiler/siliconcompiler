8ba590f565724a9a001ca97826c9f132c24991ad
Peter Gadfort <gadfort@zeroasic.com>
2024-08-25 11:19:26 -0400

    install klayout purely in yml file

--- .github/workflows/bin/install_klayout_win.bat
+++
@@ -1,5 +0,0 @@
-for /f "tokens=* USEBACKQ" %%i in (`python3 setup/_tools.py --tool klayout --field version`) do set KLAYOUTVERSION=%%i
-
-curl https://www.klayout.org/downloads/Windows/klayout-%KLAYOUTVERSION%-win64.zip --output klayout.zip
-7z x klayout.zip
-xcopy /E klayout-%KLAYOUTVERSION%-win64 "C:\Program Files (x86)\KLayout\"
