df98112effa3feb6079457288e9eda0382a28e2c
Andreas Olofsson <andreas@zeroasic.com>
2024-09-06 22:11:51 -0400

    Adding schematic capability to schema - I know this is going to be controversial, because it's a slipper slope, but there have been a number of times in the last 3 years that we have needed some way of keeping track of the instances in the design. As we move siliconcompiler into new areas, this need is only going to grow. We could punt and say that this functionality is going to be put in the derivative projects that make use of SC, but practically, those projects won't get as much care/resources. - This is a very simple data structure

---
+++ tests/core/test_schematic.py
@@ -0,0 +1,35 @@
+# Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import siliconcompiler
+
+
+def test_help():
+    '''API test for schematic entry
+    '''
+
+
+    chip = siliconcompiler.Chip('test')
+
+    # 4 input OR gate constructed from 2 input OR gates
+    chip.add('schematic', 'component', [('I0','OR2'),
+                                        ('I1','OR2'),
+                                        ('I2','OR2')])
+
+    # pins
+    chip.add('schematic', 'pin', [('IN0','input'),
+                                  ('IN1','input'),
+                                  ('IN2','input'),
+                                  ('IN3','input'),
+                                  ('OUT','output')])
+
+    # nets
+    chip.add('schematic', 'net', 'IN0', ('I0','A'))
+    chip.add('schematic', 'net', 'IN1', ('I0','B'))
+    chip.add('schematic', 'net', 'IN2', ('I1','A'))
+    chip.add('schematic', 'net', 'IN3', ('I1','B'))
+    chip.add('schematic', 'net', 'INTA', [('I0','Z'),('I2','A')])
+    chip.add('schematic', 'net', 'INTB', [('I1','Z'),('I2','B')])
+    chip.add('schematic', 'net', 'OUT', ('I2','Z'))
+
+#########################
+if __name__ == "__main__":
+    test_help()
