df5ac517d9151e3ed7d04179be2d824fe29852b2
Peter Gadfort <gadfort@zeroasic.com>
2024-09-19 09:17:00 -0400

    yupdate xyce enablement

---
+++ siliconcompiler/tools/xyce/__init__.py
@@ -0,0 +1,40 @@
+'''
+Xyce is a high performance SPICE-compatible circuit simulator
+capable capable of solving extremely large circuit problems by
+supporting large-scale parallel computing platforms. It also
+supports serial execution on all common desktop platforms,
+and small-scale parallel runs on Unix-like systems.
+
+Documentation: https://xyce.sandia.gov/documentation-tutorials/
+
+Sources: https://github.com/Xyce/Xyce
+
+Installation: https://xyce.sandia.gov/documentation-tutorials/building-guide/
+
+Status: SC integration WIP
+'''
+from siliconcompiler.tools._common import get_tool_task
+
+
+################################
+# Setup Tool (pre executable)
+################################
+def setup(chip):
+
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    tool, task = get_tool_task(chip, step, index)
+
+    chip.set('tool', tool, 'exe', 'Xyce')
+    chip.set('tool', tool, 'vswitch', '-v')
+    chip.set('tool', tool, 'version', '>=v7.8')
+
+    chip.set('tool', tool, 'task', task, 'regex', 'warnings', 'warning',
+             step=step, index=index, clobber=False)
+    chip.set('tool', tool, 'task', task, 'regex', 'errors', 'error',
+             step=step, index=index, clobber=False)
+
+
+def parse_version(stdout):
+    ver = stdout.split()[-1]
+    return ver.split('-')[0]
---
+++ siliconcompiler/tools/xyce/simulate.py
@@ -0,0 +1,70 @@
+from siliconcompiler.tools._common import input_provides, get_tool_task
+from siliconcompiler.tools.xyce import setup as tool_setup
+import os
+
+
+def setup(chip):
+    tool_setup(chip)
+
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    tool, task = get_tool_task(chip, step, index)
+
+    design = chip.top()
+
+    if f'{design}.xyce' in input_provides(chip, step, index):
+        chip.add('tool', tool, 'task', task, 'input', f'{design}.xyce',
+                 step=step, index=index)
+    elif f'{design}.cir' in input_provides(chip, step, index):
+        chip.add('tool', tool, 'task', task, 'input', f'{design}.cir',
+                 step=step, index=index)
+    else:
+        chip.add('tool', tool, 'task', task, 'require', 'input,netlist,spice',
+                 step=step, index=index)
+
+    chip.set('tool', tool, 'task', task, 'var', 'trace', 'false',
+             step=step, index=index, clobber=False)
+    chip.set('tool', tool, 'task', task, 'var', 'trace', 'true/false, enable dumping all signals',
+             field='help')
+
+    allowed_traced = ['ASCII', 'binary']
+    chip.set('tool', tool, 'task', task, 'var', 'trace_format', 'ASCII',
+             step=step, index=index, clobber=False)
+    chip.set('tool', tool, 'task', task, 'var', 'trace_format', 'Format to use for traces. '
+             f'Allowed values are {allowed_traced}',
+             field='help')
+
+    if chip.get('tool', tool, 'task', task, 'var', 'trace_format', step=step, index=index)[0] \
+            not in allowed_traced:
+        raise ValueError(f"{chip.get('tool', tool, 'task', task, 'var', 'trace_format')[0]} "
+                         "is not an accepted value")
+
+    if chip.get('tool', tool, 'task', task, 'var', 'trace_format', step=step, index=index) == \
+            ['ASCII']:
+        chip.add('tool', tool, 'task', task, 'option', '-a',
+                 step=step, index=index)
+
+    if chip.get('tool', tool, 'task', task, 'var', 'trace',
+                step=step, index=index) == ['true']:
+        chip.add('tool', tool, 'task', task, 'output', f'{design}.raw',
+                 step=step, index=index)
+        chip.add('tool', tool, 'task', task, 'option', f'-r outputs/{design}.raw',
+                 step=step, index=index)
+
+
+def runtime_options(chip):
+    design = chip.top()
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+
+    if f'{design}.xyce' in input_provides(chip, step, index):
+        if os.path.isfile(f'inputs/{design}.xyce'):
+            return [f'inputs/{design}.xyce']
+        elif os.path.isfile(f'inputs/{design}.xyce/{design}.cir'):
+            return [f'inputs/{design}.xyce/{design}.cir']
+        else:
+            return [f'inputs/{design}.xyce']
+    elif f'{design}.cir' in input_provides(chip, step, index):
+        return [f'inputs/{design}.cir']
+
+    return chip.find_files('input', 'netlist', 'spice', step=step, index=index)
