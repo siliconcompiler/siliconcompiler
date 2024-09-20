b238098476687630e42608870c42770a84e60829
Peter Gadfort <peter.gadfort@gmail.com>
2024-09-20 08:51:01 -0400

    version 0.28.1

--- siliconcompiler/_metadata.py
+++ siliconcompiler/_metadata.py
@@ -1,5 +1,5 @@
 # Version number following semver standard.
-version = '0.28.0'
+version = '0.28.1'
 
 # Default server address for remote runs, if unspecified.
 default_server = 'https://server.siliconcompiler.com'
