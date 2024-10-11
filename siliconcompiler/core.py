2b515809bf731a41c72db754e4243ed722dde89c
Peter Gadfort <gadfort@zeroasic.com>
2024-10-11 16:40:57 -0400

    disable the HTML generation by default

--- siliconcompiler/core.py
+++ siliconcompiler/core.py
@@ -2812,7 +2812,7 @@ class Chip:
         return self._dash
 
     ###########################################################################
-    def summary(self, show_all_indices=False, generate_image=True, generate_html=True):
+    def summary(self, show_all_indices=False, generate_image=True, generate_html=False):
         '''
         Prints a summary of the compilation manifest.
 
