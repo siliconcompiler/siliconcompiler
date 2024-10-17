dfddb8f986c2caeb7102cdef75f997855545d969
Peter Gadfort <peter.gadfort@gmail.com>
2024-10-17 10:50:46 -0400

    convert dfm and write data

---
+++ siliconcompiler/tools/openroad/fillmetal_insertion.py
@@ -0,0 +1,65 @@
+from siliconcompiler import NodeStatus
+
+from siliconcompiler.tools._common import get_tool_task
+from siliconcompiler.tools._common.asic import get_mainlib
+
+from siliconcompiler.tools.openroad._apr import setup as apr_setup
+from siliconcompiler.tools.openroad._apr import set_reports, set_pnr_inputs, set_pnr_outputs
+from siliconcompiler.tools.openroad._apr import \
+    define_ord_params, define_sta_params, define_sdc_params, \
+    define_fin_params
+from siliconcompiler.tools.openroad._apr import build_pex_corners, define_ord_files
+from siliconcompiler.tools.openroad._apr import extract_metrics
+
+
+def setup(chip):
+    '''
+    Perform fill metal insertion
+    '''
+
+    # Generic apr tool setup.
+    apr_setup(chip)
+
+    # Setup task IO
+    set_pnr_inputs(chip)
+    set_pnr_outputs(chip)
+
+    # set default values for openroad
+    define_ord_params(chip)
+    define_sta_params(chip)
+    define_sdc_params(chip)
+    define_fin_params(chip)
+
+    set_reports(chip, [
+        'setup',
+        'unconstrained',
+        'power'
+    ])
+
+
+def pre_process(chip):
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    tool, task = get_tool_task(chip, step, index)
+    if not chip.find_files('tool', tool, 'task', task, 'prescript',
+                           step=step, index=index) and \
+            not chip.find_files('tool', tool, 'task', task, 'postscript',
+                                step=step, index=index) and \
+            chip.get('tool', tool, 'task', task, 'var', 'fin_add_fill',
+                     step=step, index=index) == ["true"]:
+        pdk = chip.get('option', 'pdk')
+        stackup = chip.get('option', 'stackup')
+        mainlib = get_mainlib(chip)
+        libtype = chip.get('library', mainlib, 'asic', 'libarch', step=step, index=index)
+
+        if not chip.find_files('pdk', pdk, 'aprtech', tool, stackup, libtype, 'fill'):
+            chip.set('record', 'status', NodeStatus.SKIPPED, step=step, index=index)
+            chip.logger.warning(f'{step}{index} will be skipped since there is nothing to do.')
+            return
+
+    define_ord_files(chip)
+    build_pex_corners(chip)
+
+
+def post_process(chip):
+    extract_metrics(chip)
---
+++ siliconcompiler/tools/openroad/write_data.py
@@ -0,0 +1,121 @@
+from siliconcompiler.tools._common import get_tool_task
+from siliconcompiler.tools._common.asic import get_libraries, set_tool_task_var
+from siliconcompiler.tools.openroad._apr import setup as apr_setup
+from siliconcompiler.tools.openroad._apr import set_reports, set_pnr_inputs, set_pnr_outputs
+from siliconcompiler.tools.openroad._apr import \
+    define_ord_params, define_sta_params, define_sdc_params, \
+    define_pex_params, define_psm_params
+from siliconcompiler.tools.openroad._apr import build_pex_corners, define_ord_files
+from siliconcompiler.tools.openroad._apr import extract_metrics
+
+
+def setup(chip):
+    '''
+    Write output files
+    '''
+
+    # Generic apr tool setup.
+    apr_setup(chip)
+
+    # Task setup
+    design = chip.top()
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    tool, task = get_tool_task(chip, step, index)
+
+    # Setup task IO
+    set_pnr_inputs(chip)
+    set_pnr_outputs(chip)
+
+    # set default values for openroad
+    define_ord_params(chip)
+    define_sta_params(chip)
+    define_sdc_params(chip)
+    define_pex_params(chip)
+    define_psm_params(chip)
+
+    pdkname = chip.get('option', 'pdk')
+    targetlibs = get_libraries(chip, 'logic')
+    macrolibs = get_libraries(chip, 'macro')
+    stackup = chip.get('option', 'stackup')
+
+    # Determine if exporting the cdl
+    set_tool_task_var(chip, param_key='write_cdl',
+                      default_value='false',
+                      schelp='true/false, when true enables writing the CDL file for the design')
+    do_cdl = chip.get('tool', tool, 'task', task, 'var', 'write_cdl',
+                      step=step, index=index)[0] == 'true'
+
+    if do_cdl:
+        chip.add('tool', tool, 'task', task, 'output', design + '.cdl', step=step, index=index)
+        for lib in targetlibs + macrolibs:
+            chip.add('tool', tool, 'task', task, 'require',
+                     ",".join(['library', lib, 'output', stackup, 'cdl']),
+                     step=step, index=index)
+
+    set_tool_task_var(chip, param_key='write_spef',
+                      default_value='true',
+                      schelp='true/false, when true enables writing the SPEF file for the design')
+    do_spef = chip.get('tool', tool, 'task', task, 'var', 'write_spef',
+                       step=step, index=index)[0] == 'true'
+    set_tool_task_var(chip, param_key='use_spef',
+                      default_value=do_spef,
+                      schelp='true/false, when true enables reading in SPEF files.')
+
+    if do_spef:
+        # Require openrcx pex models
+        for corner in chip.get('tool', tool, 'task', task, 'var', 'pex_corners',
+                               step=step, index=index):
+            chip.add('tool', tool, 'task', task, 'require',
+                     ",".join(['pdk', pdkname, 'pexmodel', 'openroad-openrcx', stackup, corner]),
+                     step=step, index=index)
+
+        # Add outputs SPEF in the format {design}.{pexcorner}.spef
+        for corner in chip.get('tool', tool, 'task', task, 'var', 'pex_corners',
+                               step=step, index=index):
+            chip.add('tool', tool, 'task', task, 'output', design + '.' + corner + '.spef',
+                     step=step, index=index)
+
+    # Add outputs LEF
+    chip.add('tool', tool, 'task', task, 'output', design + '.lef', step=step, index=index)
+
+    set_tool_task_var(chip, param_key='write_liberty',
+                      default_value='true',
+                      schelp='true/false, when true enables writing the liberty '
+                             'timing model for the design')
+    do_liberty = chip.get('tool', tool, 'task', task, 'var', 'write_liberty',
+                          step=step, index=index)[0] == 'true'
+
+    if do_liberty:
+        # Add outputs liberty model in the format {design}.{libcorner}.lib
+        for corner in chip.getkeys('constraint', 'timing'):
+            chip.add('tool', tool, 'task', task, 'output', design + '.' + corner + '.lib',
+                     step=step, index=index)
+
+    set_tool_task_var(chip, param_key='write_sdf',
+                      default_value='true',
+                      schelp='true/false, when true enables writing the SDF timing model '
+                             'for the design')
+    do_sdf = chip.get('tool', tool, 'task', task, 'var', 'write_sdf',
+                      step=step, index=index)[0] == 'true'
+    if do_sdf:
+        # Add outputs liberty model in the format {design}.{libcorner}.sdf
+        for corner in chip.getkeys('constraint', 'timing'):
+            chip.add('tool', tool, 'task', task, 'output', design + '.' + corner + '.sdf',
+                     step=step, index=index)
+
+    set_reports(chip, [
+        'setup',
+        'hold',
+        'unconstrained',
+        'power'
+    ])
+
+
+def pre_process(chip):
+    define_ord_files(chip)
+    build_pex_corners(chip)
+
+
+def post_process(chip):
+    extract_metrics(chip)
