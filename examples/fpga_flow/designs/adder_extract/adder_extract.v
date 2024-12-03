4d4df53b2991aa47060c1174b9877529162325de
petergrossmann21 <peter@zeroasic.com>
2024-12-03 09:46:37 -0500

    Fix Verilog lint

--- examples/fpga_flow/designs/adder_extract/adder_extract.v
+++ examples/fpga_flow/designs/adder_extract/adder_extract.v
@@ -1,11 +1,11 @@
 
 module adder_extract #(
-    parameter INPUT_WIDTH = 18,
+    parameter INPUT_WIDTH  = 18,
     parameter OUTPUT_WIDTH = 40
 ) (
-    input  [(INPUT_WIDTH-1):0] a,
-    input  [(INPUT_WIDTH-1):0] b,
-    output [  (OUTPUT_WIDTH-1):0] y
+    input  [ (INPUT_WIDTH-1):0] a,
+    input  [ (INPUT_WIDTH-1):0] b,
+    output [(OUTPUT_WIDTH-1):0] y
 );
 
     assign y = a + b;
