df8ba49207b46086d3516224ec4290cbdc7caaf9
Peter Gadfort <peter.gadfort@gmail.com>
2024-12-06 15:17:51 -0700

    fix metrics task

--- siliconcompiler/tools/openroad/scripts/apr/sc_metrics.tcl
+++ siliconcompiler/tools/openroad/scripts/apr/sc_metrics.tcl
@@ -1,1 +1,22 @@
+###############################
+# Reading SC Schema
+###############################
 
+source ./sc_manifest.tcl > /dev/null
+
+###############################
+# Task Preamble
+###############################
+
+set sc_refdir [sc_cfg_tool_task_get refdir]
+source -echo "$sc_refdir/apr/preamble.tcl"
+
+###############################
+# Report Metrics
+###############################
+
+###############################
+# Task Postamble
+###############################
+
+source -echo "$sc_refdir/apr/postamble.tcl"
