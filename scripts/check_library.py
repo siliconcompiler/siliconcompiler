0387b4efebaf83afe8039ce55edd00b6ad44ce02
Peter Gadfort <peter.gadfort@gmail.com>
2024-12-14 09:59:13 -0700

    allow library to be specified

--- scripts/check_library.py
+++ scripts/check_library.py
@@ -1,17 +1,38 @@
+import argparse
+
 from siliconcompiler import Chip
 from siliconcompiler.tools.opensta import check_library
 
 
 def main():
-    chip = Chip('check_libs')
-    args = chip.create_cmdline(switchlist=['-target'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-target', help='Reference target', required=True)
+    parser.add_argument('-library', nargs='+', help='Library to check', required=True)
+    args = parser.parse_args()
 
-    if 'target' not in args or not args['target']:
-        raise ValueError('-target is required')
+    chip = Chip('check_libs')
 
     chip.node('check_lib', 'check', check_library)
     chip.set('option', 'flow', 'check_lib')
     chip.set('option', 'clean', True)
+
+    target = chip._load_module(args.target)
+    chip.use(target)
+
+    chip.unset('asic', 'logiclib')
+
+    for lib in args.library:
+        mod = chip._load_module(lib)
+
+        libraries = mod.setup()
+        if not isinstance(libraries, (list, tuple)):
+            libraries = [libraries]
+
+        for library in libraries:
+            if library.design.startswith('lambdalib_'):
+                continue
+            chip.use(library)
+            chip.add('asic', 'logiclib', library.design)
 
     chip.run()
 
