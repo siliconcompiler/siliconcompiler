b28d742773069f1d9d18b26827a3eaf3362db47f
Peter Gadfort <peter.gadfort@gmail.com>
2024-12-20 10:34:05 -0700

    add support for flist import handling

---
+++ tests/core/test_flist.py
@@ -0,0 +1,76 @@
+import os
+import pytest
+
+from siliconcompiler import Chip
+
+
+@pytest.mark.nostrict
+def test_import_flist_rel_paths(datadir):
+    chip = Chip('dummy')
+    chip.import_flist(os.path.join(datadir, 'flist', 'files.flist'))
+
+    assert "flist-files.flist" in chip.getkeys('package', 'source')
+
+    assert chip.get('input', 'rtl', 'verilog') == ["dummy.v"]
+    assert chip.get('input', 'rtl', 'verilog', field='package') == ['flist-files.flist']
+    assert None not in chip.find_files('input', 'rtl', 'verilog')
+
+    assert chip.get('option', 'idir') == ["."]
+    assert chip.get('option', 'idir', field='package') == ['flist-files.flist']
+
+    assert chip.get('option', 'define') == ["TEST=1"]
+
+
+@pytest.mark.nostrict
+def test_import_flist_abs_paths():
+    chip = Chip('dummy')
+
+    dummy_path = os.path.abspath('dummy.v')
+    with open(dummy_path, 'w') as f:
+        f.write("// test\n")
+
+    os.makedirs("flist", exist_ok=True)
+    os.makedirs("incs", exist_ok=True)
+
+    with open('flist/flist', 'w') as f:
+        f.write("// test\n")
+        f.write(dummy_path + "\n")
+        f.write("+incdir+" + os.path.abspath("incs") + "\n")
+
+    chip.import_flist('flist/flist')
+
+    assert "flist-flist" in chip.getkeys('package', 'source')
+
+    assert chip.get('input', 'rtl', 'verilog') == [os.path.abspath("dummy.v")]
+    assert chip.get('input', 'rtl', 'verilog', field='package') == [None]
+    assert None not in chip.find_files('input', 'rtl', 'verilog')
+
+    assert chip.get('option', 'idir') == [os.path.abspath("incs")]
+    assert chip.get('option', 'idir', field='package') == [None]
+
+
+@pytest.mark.nostrict
+def test_import_flist_abs_package_paths():
+    chip = Chip('dummy')
+
+    dummy_path = os.path.abspath('dummy.v')
+    with open(dummy_path, 'w') as f:
+        f.write("// test\n")
+
+    os.makedirs("incs", exist_ok=True)
+
+    with open('flist', 'w') as f:
+        f.write("// test\n")
+        f.write(dummy_path + "\n")
+        f.write("+incdir+" + os.path.abspath("incs") + "\n")
+
+    chip.import_flist('flist')
+
+    assert "flist-flist" in chip.getkeys('package', 'source')
+
+    assert chip.get('input', 'rtl', 'verilog') == ["dummy.v"]
+    assert chip.get('input', 'rtl', 'verilog', field='package') == ['flist-flist']
+    assert None not in chip.find_files('input', 'rtl', 'verilog')
+
+    assert chip.get('option', 'idir') == ["incs"]
+    assert chip.get('option', 'idir', field='package') == ['flist-flist']
