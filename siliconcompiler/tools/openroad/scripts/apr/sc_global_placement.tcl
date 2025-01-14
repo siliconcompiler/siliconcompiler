9e7987910bb26e06ffe5c5cd16fe1bab11869734
Peter Gadfort <gadfort@zeroasic.com>
2025-01-13 20:26:40 -0700

    fix clustering logic position

--- siliconcompiler/tools/openroad/scripts/apr/sc_global_placement.tcl
+++ siliconcompiler/tools/openroad/scripts/apr/sc_global_placement.tcl
@@ -12,18 +12,18 @@ set sc_refdir [sc_cfg_tool_task_get refdir]
 source -echo "$sc_refdir/apr/preamble.tcl"
 
 ###############################
-# Global Placement
-###############################
-
-sc_global_placement
-
-###############################
 # Perform multi-bit clustering
 ###############################
 
 if { [lindex [sc_cfg_tool_task_get var enable_multibit_clustering] 0] == "true" } {
     cluster_flops
 }
+
+###############################
+# Global Placement
+###############################
+
+sc_global_placement
 
 ###############################
 # Task Postamble
