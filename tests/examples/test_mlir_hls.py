d9242f4f4292be5db9225bb7aa3fd45e927408aa
Peter Gadfort <peter.gadfort@gmail.com>
2025-01-28 10:46:12 -0700

    add CI test for mlir_hls

---
+++ tests/examples/test_mlir_hls.py
@@ -0,0 +1,12 @@
+import os
+import pytest
+
+
+@pytest.mark.eda
+@pytest.mark.quick
+@pytest.mark.timeout(1200)
+def test_py_mlir_hls():
+    from mlir_hls import mlir_hls
+    mlir_hls.main()
+
+    assert os.path.isfile('build/main_kernel/job0/write.gds/0/outputs/main_kernel.gds')
