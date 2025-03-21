26314ff692f01100f1186ccb82a2b7b6363371ae
Peter Gadfort <peter.gadfort@gmail.com>
2025-03-21 11:34:35 -0400

    add support to build vpr on rhel9

---
+++ siliconcompiler/toolscripts/rhel9/install-vpr.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# Get directory of script
+src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)/..
+
+mkdir -p deps
+cd deps
+
+git clone $(python3 ${src_path}/_tools.py --tool vpr --field git-url) vpr
+cd vpr
+git checkout $(python3 ${src_path}/_tools.py --tool vpr --field git-commit)
+git submodule update --init --recursive
+
+sudo dnf config-manager --set-enabled devel || true
+./install_dnf_packages.sh
+
+sudo yum install -y tbb-devel
+sudo dnf config-manager --set-disabled devel || true
+
+args=
+if [ ! -z ${PREFIX} ]; then
+    args="-DCMAKE_INSTALL_PREFIX=$PREFIX"
+fi
+
+make CMAKE_PARAMS="$args -DWITH_PARMYS=OFF -DWITH_ABC=OFF -DYOSYS_F4PGA_PLUGINS=OFF" -j$(nproc)
+cd build
+sudo make install
