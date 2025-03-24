44509153b37d323eaff2ca29823dc7f3041a8b58
Peter Gadfort <peter.gadfort@gmail.com>
2025-03-24 18:00:12 -0400

    add no target test to report_library

--- tests/utils/test_report_library.py
+++ tests/utils/test_report_library.py
@@ -6,7 +6,9 @@ import pytest
 @pytest.mark.parametrize('target', [
     'asap7_demo',
     'skywater130_demo',
-    'gf180_demo'
+    'gf180_demo',
+    'ihp130_demo',
+    'freepdk45_demo'
 ])
 def test_report(target, scroot, monkeypatch):
     monkeypatch.syspath_prepend(scroot)
@@ -16,3 +18,14 @@ def test_report(target, scroot, monkeypatch):
     monkeypatch.setattr('sys.argv', ['report', '-target', target])
 
     assert report_library.main() == 0
+
+
+def test_report_no_target(scroot, monkeypatch):
+    monkeypatch.syspath_prepend(scroot)
+
+    from scripts import report_library
+
+    monkeypatch.setattr('sys.argv', ['report'])
+
+    with pytest.raises(ValueError, match='-target is required'):
+        report_library.main()
