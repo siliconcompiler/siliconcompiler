c027b31661800cc9584089f293dd02ff8bb044b1
Peter Gadfort <gadfort@zeroasic.com>
2025-05-05 17:06:43 -0400

    add record helper functions

--- siliconcompiler/record.py
+++ siliconcompiler/record.py
@@ -1,15 +1,154 @@
 # Copyright 2025 Silicon Compiler Authors. All Rights Reserved.
 
+import datetime
+import getpass
+import platform
+import psutil
+import socket
+import time
+
+from enum import Enum
+
 from siliconcompiler.schema import BaseSchema
 from siliconcompiler.schema import EditableSchema, Parameter, PerNode, Scope
 from siliconcompiler.schema.utils import trim
 
+from siliconcompiler import _metadata
+
+
+class RecordTime(Enum):
+    START = "starttime"
+    END = "endtime"
+
 
 class RecordSchema(BaseSchema):
+    __TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
+
     def __init__(self):
         super().__init__()
 
         schema_record(self)
+
+    def clear(self, step, index, keep=None):
+        if not keep:
+            keep = []
+
+        for record in self.getkeys():
+            if record in keep:
+                continue
+            param = self.get(record, field=None)
+
+            if param.get(field='pernode').is_never():
+                param.unset()
+            else:
+                param.unset(step=step, index=index)
+
+    def record_python_packages(self):
+        try:
+            from pip._internal.operations.freeze import freeze
+        except:  # noqa E722
+            freeze = None
+
+        if freeze:
+            # clear record
+            self.set('pythonpackage', [])
+
+            for pkg in freeze():
+                self.add('pythonpackage', pkg)
+
+    def record_version(self, step, index):
+        self.set('scversion', _metadata.version, step=step, index=index)
+        self.set('pythonversion', platform.python_version(), step=step, index=index)
+
+    def record_inputnodes(self, step, index, nodes):
+        self.set('inputnode', nodes, step=step, index=index)
+
+    def record_status(self, step, index, status):
+        self.set('status', status, step=step, index=index)
+
+    def __get_cloud_region(self):
+        # TODO: add logic to figure out if we're running on a remote cluster and
+        # extract the region in a provider-specific way.
+        return 'local'
+
+    def __record_ip_mac_information(self, step, index):
+        try:
+            for interface, addrs in psutil.net_if_addrs().items():
+                if interface == 'lo':
+                    # don't consider loopback device
+                    continue
+
+                if not addrs:
+                    # skip missing addrs
+                    continue
+
+                use_addr = False
+                for addr in addrs:
+                    if addr.family == socket.AF_INET:
+                        if not addr.address.startswith('127.'):
+                            use_addr = True
+                        break
+
+                if use_addr:
+                    ipaddr = None
+                    macaddr = None
+                    for addr in addrs:
+                        if not ipaddr and addr.family == socket.AF_INET:
+                            ipaddr = addr.address
+                        if not ipaddr and addr.family == socket.AF_INET6:
+                            ipaddr = addr.address
+                        if not macaddr and addr.family == psutil.AF_LINK:
+                            macaddr = addr.address
+
+                    self.set('ipaddr', ipaddr, step=step, index=index)
+                    self.set('macaddr', macaddr, step=step, index=index)
+                    break
+        except:  # noqa E722
+            pass
+
+    def record_userinformation(self, step, index):
+        from siliconcomiler.scheduler import _get_machine_info
+        machine_info = _get_machine_info()
+
+        self.set('platform', machine_info['system'], step=step, index=index)
+        if machine_info['distro']:
+            self.set('distro', machine_info['distro'], step=step, index=index)
+        self.set('osversion', machine_info['osversion'], step=step, index=index)
+        if machine_info['kernelversion']:
+            self.set('kernelversion', machine_info['kernelversion'], step=step, index=index)
+        self.set('arch', machine_info['arch'], step=step, index=index)
+        self.set('userid', getpass.getuser(), step=step, index=index)
+        self.set('machine', platform.node(), step=step, index=index)
+        self.set('region', self._get_cloud_region(), step=step, index=index)
+        self.__record_ip_mac_information(step, index)
+
+    def record_time(self, step, index, type):
+        type = RecordTime(type)
+
+        formatted_time = datetime.fromtimestamp(time.time()).strftime(RecordSchema.__TIMEFORMAT)
+
+        self.set('record', type.value, formatted_time, step=step, index=index)
+
+    def get_recorded_time(self, step, index, type):
+        type = RecordTime(type)
+        return datetime.strptime(
+            self.get('record', type.value, step=step, index=index),
+            RecordSchema.__TIMEFORMAT).timestamp()
+
+    def record_toolinformation(self, step, index, version=None, path=None):
+        if version:
+            self.set('toolversion', version, step=step, index=index)
+
+        if path:
+            self.set('toolpath', path, step=step, index=index)
+
+    def record_toolargs(self, step, index, args):
+        self.set('toolargs',
+                 ' '.join(f'"{arg}"' if ' ' in arg else arg for arg in args),
+                 step=step, index=index)
+
+    def record_toolexitcode(self, step, index, code):
+        self.set('toolexitcode', code, step=step, index=index)
 
 
 ###########################################################################
