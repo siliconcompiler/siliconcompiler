de70502df4beb60052aa22a734acc0923cf9c187
Peter Gadfort <peter.gadfort@gmail.com>
2025-05-22 16:37:13 -0400

    template use_sudo_install

--- siliconcompiler/toolscripts/ubuntu24/install-openroad.sh
+++ siliconcompiler/toolscripts/ubuntu24/install-openroad.sh
@@ -4,6 +4,13 @@ set -e
 
 src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)/..
 
+USE_SUDO_INSTALL="${USE_SUDO_INSTALL:-yes}"
+if [ "${USE_SUDO_INSTALL:-yes}" = "yes" ]; then
+    SUDO_INSTALL=sudo
+else
+    SUDO_INSTALL=""
+fi
+
 sudo apt-get install -y git
 
 mkdir -p deps
@@ -29,6 +36,6 @@ fi
 ./etc/Build.sh -cmake="$cmake_args"
 
 cd build
-sudo make install
+$SUDO_INSTALL make install
 
 cd -
