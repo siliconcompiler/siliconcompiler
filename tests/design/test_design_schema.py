3ba81c38153216a9faaaa5e399ee736ef8f8b94f
Andreas Olofsson <andreas@zeroasic.com>
2025-06-02 22:42:04 -0400

    object name cleanup

--- tests/design/test_design_schema.py
+++ tests/design/test_design_schema.py
@@ -15,49 +15,49 @@ def test_design_keys():
 
 
 def test_design_params():
-    schema = DesignSchema()
+    d = DesignSchema()
 
     # top module
     top = 'mytop'
-    assert schema.set('rtl', 'top', top)
-    assert schema.get('rtl', 'top') == top
+    assert d.set('rtl', 'top', top)
+    assert d.get('rtl', 'top') == top
 
     # files
     files = ['one.v', 'two.v']
-    assert schema.set('rtl', 'file', 'verilog', files)
-    assert schema.get('rtl', 'file', 'verilog') == files
+    assert d.set('rtl', 'file', 'verilog', files)
+    assert d.get('rtl', 'file', 'verilog') == files
 
     # idir
     idirs = ['/home/acme/incdir1', '/home/acme/incdir2']
-    assert schema.set('rtl', 'idir', idirs)
-    assert schema.get('rtl', 'idir', ) == idirs
+    assert d.set('rtl', 'idir', idirs)
+    assert d.get('rtl', 'idir', ) == idirs
 
     # libdirs
     libdirs = ['/usr/lib']
-    assert schema.set('hls', 'libdir', libdirs)
-    assert schema.get('hls', 'libdir', ) == libdirs
+    assert d.set('hls', 'libdir', libdirs)
+    assert d.get('hls', 'libdir', ) == libdirs
 
     # libs
     libs = ['lib1', 'lib2']
-    assert schema.set('rtl', 'lib', libs)
-    assert schema.get('rtl', 'lib', ) == libs
+    assert d.set('rtl', 'lib', libs)
+    assert d.get('rtl', 'lib', ) == libs
 
     # libext
     libexts = ['sv', 'v']
-    assert schema.set('rtl', 'lib', libexts)
-    assert schema.get('rtl', 'lib', ) == libexts
+    assert d.set('rtl', 'lib', libexts)
+    assert d.get('rtl', 'lib', ) == libexts
 
     # define
     defs = ['CFG_TARGET=FPGA']
-    assert schema.set('rtl', 'define', defs)
-    assert schema.get('rtl', 'define') == defs
+    assert d.set('rtl', 'define', defs)
+    assert d.get('rtl', 'define') == defs
 
     # undefine
     undefs = ['CFG_TARGET']
-    assert schema.set('rtl', 'undefine', undefs)
-    assert schema.get('rtl', 'undefine') == undefs
+    assert d.set('rtl', 'undefine', undefs)
+    assert d.get('rtl', 'undefine') == undefs
 
     # param
     val = '2'
-    assert schema.set('rtl', 'param', 'N', val)
-    assert schema.get('rtl', 'param', 'N') == val
+    assert d.set('rtl', 'param', 'N', val)
+    assert d.get('rtl', 'param', 'N') == val
