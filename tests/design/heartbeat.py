be02e2bb2e768a15e07d360c087ab6241c0a94e3
Andreas Olofsson <andreas@zeroasic.com>
2025-06-04 17:30:46 -0400

    Cleaning up design example, wip

--- tests/design/heartbeat.py
+++ tests/design/heartbeat.py
@@ -2,19 +2,29 @@ from siliconcompiler.design import DesignSchema
 
 class Heartbeat(DesignSchema):
 
-    def __init__(self, name='heartbeat'):
+    def __init__(self):
 
-        DesignSchema.__init__(self, name='heartbeat')
+        super().__init__('heartbeat')
 
         # files
         source = ['heartbeat.v']
         sdc = ['heartbeat.sdc']
         tb =  ['tb.v']
 
+        #self.package_root()
+        #self.package_home(path='git+https://github.com/acme')
+
+        #when is this needed? Is there a default?
+
+        # why do path concatenation at source? why not in find files/run?
+
         # rtl
         self.add_file(fileset='rtl', files=source)
         self.option(fileset='rtl', topmodule='heartbeat')
-
+        self.set('fileset', 'rtl', 'topmodule', 'heartbeat')
+        #self.set_libext()
+        #self.get_libext()
+        #TODO: study some clever APIs?
         # constraints
         self.add_file(fileset='constraint', files=sdc)
 
@@ -23,3 +33,6 @@ class Heartbeat(DesignSchema):
         self.option(fileset='testbench', topmodule='tb')
 
 #d = Heartbeat()
+
+#self.use(Heartbeat())<--!!!!
+#self.use(Heartbeat)
