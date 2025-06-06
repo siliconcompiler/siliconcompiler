60ccc2df3b4d34e28870b22e438b9e4e0f26f618
Andreas Olofsson <andreas@zeroasic.com>
2025-06-06 16:26:13 -0400

    Fixing more verible files

--- tests/design/data/tb.v
+++ tests/design/data/tb.v
@@ -1,40 +1,36 @@
-module tb();
+module tb ();
 
-   localparam PERIOD = 2;
-   localparam TIMEOUT = PERIOD * 64;
+    localparam PERIOD = 2;
+    localparam TIMEOUT = PERIOD * 64;
 
-   reg        nreset;
-   reg        clk;
+    reg nreset;
+    reg clk;
 
-   // control block
-   initial
-     begin
+    // control block
+    initial begin
         $dumpfile("dump.vcd");
         $dumpvars(0, tb);
-        #(TIMEOUT)
-        $finish;
-     end
+        #(TIMEOUT) $finish;
+    end
 
-   // test program
-   initial
-     begin
-        #(1)
-        nreset = 'b0;
+    // test program
+    initial begin
+        #(1) nreset = 'b0;
         clk = 'b0;
-        #(1)
-        nreset = 'b1;
-     end
+        #(1) nreset = 'b1;
+    end
 
-   // clk
-   always
-     #(PERIOD/2) clk = ~clk;
+    // clk
+    always #(PERIOD / 2) clk = ~clk;
 
-   heartbeat #(.N(8))
-   heartbeat(/*AUTOINST*/
-             // Outputs
-             .out             (out),
-             // Inputs
-             .clk             (clk),
-             .nreset          (nreset));
+    heartbeat #(
+        .N(8)
+    ) heartbeat (  /*AUTOINST*/
+        // Outputs
+        .out   (out),
+        // Inputs
+        .clk   (clk),
+        .nreset(nreset)
+    );
 
 endmodule
