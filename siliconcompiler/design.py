48eecf5e8b978b84acb047ce75a61056c20b32c9
Peter Gadfort <gadfort@zeroasic.com>
2025-07-14 10:33:38 -0400

    rename to dep_name in add_dep_fileset

--- siliconcompiler/design.py
+++ siliconcompiler/design.py
@@ -252,12 +252,12 @@ class DesignSchema(NamedSchema, DependencySchema):
             raise ValueError("fileset key must be a string")
         return self.get('fileset', fileset, 'param', name)
 
-    def add_dep_fileset(self, name: str, dep_fileset: str, fileset: str = None):
+    def add_dep_fileset(self, dep_name: str, dep_fileset: str, fileset: str = None):
         """
         Record a reference to an imported dependency's fileset.
 
         Args:
-           name (str): Dependency name.
+           dep_name (str): Dependency name.
            dep_fileset (str): Dependency fileset
            fileset (str): Fileset name.
 
@@ -267,7 +267,7 @@ class DesignSchema(NamedSchema, DependencySchema):
 
         if not isinstance(fileset, str):
             raise ValueError("fileset key must be a string")
-        return self.add("fileset", fileset, "depfileset", (name, dep_fileset))
+        return self.add("fileset", fileset, "depfileset", (dep_name, dep_fileset))
 
     def get_dep_fileset(self, fileset: str = None):
         """
