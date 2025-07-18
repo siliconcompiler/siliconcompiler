af0488dcfcc8349cfda1b654b35ddd3bb48e429d
Peter Gadfort <gadfort@zeroasic.com>
2025-07-18 11:03:46 -0400

    initial implementation of timing constraints

---
+++ siliconcompiler/constraint.py
@@ -0,0 +1,211 @@
+from typing import Union, Set, List, Tuple
+
+from siliconcompiler.schema import BaseSchema, NamedSchema, EditableSchema, Parameter, PerNode, Scope
+from siliconcompiler import DesignSchema
+
+
+class TimingScenarioSchema(NamedSchema):
+    def __init__(self, name: str = None):
+        super().__init__()
+        self.set_name(name)
+
+        schema = EditableSchema(self)
+        schema.insert(
+            'voltage', 'default',
+            Parameter(
+                "float",
+                pernode=PerNode.OPTIONAL,
+                unit='V',
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: pin voltage level",
+                switch="-constraint_timing_voltage 'scenario pin <float>'",
+                example=["api: chip.set('constraint', 'timing', 'worst', 'voltage', 'VDD', '0.9')"],
+                help="""Operating voltage applied to a specific pin in the scenario."""))
+
+        schema.insert(
+            'temperature',
+            Parameter(
+                'float',
+                pernode=PerNode.OPTIONAL,
+                unit='C',
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: temperature",
+                switch="-constraint_timing_temperature 'scenario <float>'",
+                example=["api: chip.set('constraint', 'timing', 'worst', 'temperature', '125')"],
+                help="""Chip temperature applied to the scenario specified in degrees C."""))
+
+        schema.insert(
+            'libcorner',
+            Parameter(
+                '[str]',
+                pernode=PerNode.OPTIONAL,
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: library corner",
+                switch="-constraint_timing_libcorner 'scenario <str>'",
+                example=["api: chip.set('constraint', 'timing', 'worst', 'libcorner', 'ttt')"],
+                help="""List of characterization corners used to select
+                timing files for all logiclibs and macrolibs."""))
+
+        schema.insert(
+            'pexcorner',
+            Parameter(
+                'str',
+                pernode=PerNode.OPTIONAL,
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: pex corner",
+                switch="-constraint_timing_pexcorner 'scenario <str>'",
+                example=["api: chip.set('constraint', 'timing', 'worst', 'pexcorner', 'max')"],
+                help="""Parasitic corner applied to the scenario. The
+                'pexcorner' string must match a corner found in :keypath:`pdk,<pdk>,pexmodel`."""))
+
+        schema.insert(
+            'opcond',
+            Parameter(
+                'str',
+                pernode=PerNode.OPTIONAL,
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: operating condition",
+                switch="-constraint_timing_opcond 'scenario <str>'",
+                example=["api: chip.set('constraint', 'timing', 'worst', 'opcond', 'typical_1.0')"],
+                help="""Operating condition applied to the scenario. The value
+                can be used to access specific conditions within the library
+                timing models from the :keypath:`asic,logiclib` timing models."""))
+
+        schema.insert(
+            'mode',
+            Parameter(
+                'str',
+                pernode=PerNode.OPTIONAL,
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: operating mode",
+                switch="-constraint_timing_mode 'scenario <str>'",
+                example=["api: chip.set('constraint', 'timing', 'worst', 'mode', 'test')"],
+                help="""Operating mode for the scenario. Operating mode strings
+                can be values such as test, functional, standby."""))
+
+        schema.insert(
+            'sdcfileset',
+            Parameter(
+                '[(str,str)]',
+                pernode=PerNode.OPTIONAL,
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: SDC files",
+                switch="-constraint_timing_file 'scenario <file>'",
+                example=["api: chip.set('constraint', 'timing', 'worst', 'file', 'hello.sdc')"],
+                help="""List of timing constraint files to use for the scenario. The
+                values are combined with any constraints specified by the design
+                'constraint' parameter. If no constraints are found, a default
+                constraint file is used based on the clock definitions."""))
+
+        schema.insert(
+            'check',
+            Parameter(
+                '[<setup,hold,maxtran,maxcap,mincap,power,leakagepower,dynamicpower,signalem>]',
+                pernode=PerNode.OPTIONAL,
+                scope=Scope.GLOBAL,
+                shorthelp="Constraint: timing checks",
+                switch="-constraint_timing_check 'scenario <str>'",
+                example=["api: chip.add('constraint', 'timing', 'worst', 'check', 'setup')"],
+                help="""
+                List of checks for to perform for the scenario. The checks must
+                align with the capabilities of the EDA tools and flow being used.
+                Checks generally include objectives like meeting setup and hold goals
+                and minimize power. Standard check names include setup, hold, power,
+                noise, reliability."""))
+
+    # TODO: libcorner and check should be a set
+
+    def set_pin_voltage(self, pin: str, voltage: float, step: str = None, index: Union[str, int] = None):
+        return self.set("voltage", pin, voltage, step=step, index=index)
+
+    def get_pin_voltage(self, pin: str, step: str = None, index: Union[str, int] = None) -> float:
+        if not self.valid("voltage", pin):
+            raise LookupError(f"{pin} does not have voltage")
+        return self.get("voltage", pin, step=step, index=index)
+
+    def add_libcorner(self, libcorner: str, clobber: bool = False, step: str = None, index: Union[str, int] = None):
+        if clobber:
+            return self.set("libcorner", libcorner, step=step, index=index)
+        else:
+            return self.add("libcorner", libcorner, step=step, index=index)
+
+    def get_libcorner(self, step: str = None, index: Union[str, int] = None) -> Set[str]:
+        return self.get("libcorner", step=step, index=index)
+
+    def set_pexcorner(self, pexcorner: str, step: str = None, index: Union[str, int] = None):
+        return self.set("pexcorner", pexcorner, step=step, index=index)
+
+    def get_pexcorner(self, step: str = None, index: Union[str, int] = None) -> str:
+        return self.get("pexcorner", step=step, index=index)
+
+    def set_mode(self, mode: str, step: str = None, index: Union[str, int] = None):
+        return self.set("mode", mode, step=step, index=index)
+
+    def get_mode(self, step: str = None, index: Union[str, int] = None) -> str:
+        return self.get("mode", step=step, index=index)
+
+    def set_opcond(self, opcond: str, step: str = None, index: Union[str, int] = None):
+        return self.set("opcond", opcond, step=step, index=index)
+
+    def get_opcond(self, step: str = None, index: Union[str, int] = None) -> str:
+        return self.get("opcond", step=step, index=index)
+
+    def set_temperature(self, temperature: float, step: str = None, index: Union[str, int] = None):
+        return self.set("temperature", temperature, step=step, index=index)
+
+    def get_temperature(self, step: str = None, index: Union[str, int] = None) -> float:
+        return self.get("temperature", step=step, index=index)
+
+    def add_sdcfileset(self, design: Union[DesignSchema, str], fileset: str, clobber: bool = False, step: str = None, index: Union[str, int] = None):
+        if isinstance(design, DesignSchema):
+            design = design.name()
+
+        if not isinstance(design, str):
+            raise TypeError("design must be a design object or string")
+
+        if not isinstance(fileset, str):
+            raise TypeError("fileset must be a string")
+
+        if clobber:
+            return self.set("sdcfileset", (design, fileset), step=step, index=index)
+        else:
+            return self.add("sdcfileset", (design, fileset), step=step, index=index)
+
+    def get_sdcfileset(self, step: str = None, index: Union[str, int] = None) -> List[Tuple[str, str]]:
+        return self.get("sdcfileset", step=step, index=index)
+
+    def add_check(self, check: str, clobber: bool = False, step: str = None, index: Union[str, int] = None):
+        if clobber:
+            return self.set("check", check, step=step, index=index)
+        else:
+            return self.add("check", check, step=step, index=index)
+
+    def get_check(self, step: str = None, index: Union[str, int] = None) -> Set[str]:
+        return self.get("check", step=step, index=index)
+
+
+class TimingConstraintSchema(BaseSchema):
+    def __init__(self):
+        super().__init__()
+
+        EditableSchema(self).insert("default", TimingScenarioSchema())
+
+    def add_scenario(self, scenario: TimingScenarioSchema):
+        if not isinstance(scenario, TimingScenarioSchema):
+            raise TypeError("scenario must be a timing scenario object")
+
+        if scenario.name() is None:
+            raise ValueError("scenario must have a name")
+
+        EditableSchema(self).insert(scenario.name(), scenario, clobber=True)
+
+    def get_scenario(self, scenario: str = None):
+        if scenario is None:
+            scenarios = {}
+            for scenario in self.getkeys():
+                scenarios[scenario] = self.get(scenario, field="schema")
+            return scenarios
+
+        if not self.valid(scenario):
+            raise LookupError(f"{scenario} is not defined")
+        return self.get(scenario, field="schema")
---
+++ tests/test_constraint.py
@@ -0,0 +1,246 @@
+import pytest
+
+from siliconcompiler import DesignSchema
+from siliconcompiler.schema import PerNode, Scope
+
+from siliconcompiler.constraint import TimingScenarioSchema, TimingConstraintSchema
+
+
+def test_timing_scanario_keys():
+    assert TimingScenarioSchema().allkeys() == set([
+        ('check',),
+        ('libcorner',),
+        ('mode',),
+        ('opcond',),
+        ('pexcorner',),
+        ('sdcfileset',),
+        ('temperature',),
+        ('voltage', 'default',),
+    ])
+
+
+@pytest.mark.parametrize("key", TimingScenarioSchema().allkeys())
+def test_timing_scanario_key_params(key):
+    param = TimingScenarioSchema().get(*key, field=None)
+    assert param.get(field="pernode") == PerNode.OPTIONAL
+    assert param.get(field="scope") == Scope.GLOBAL
+
+
+def test_timing_scanario_pin_voltage():
+    scene = TimingScenarioSchema()
+    assert scene.set_pin_voltage("VDD", 1.8)
+    assert scene.set_pin_voltage("VDD", 2.5, step="step0", index="1")
+    assert scene.get("voltage", "VDD") == 1.8
+    assert scene.get_pin_voltage("VDD") == 1.8
+    assert scene.get("voltage", "VDD", step="step0", index="1") == 2.5
+    assert scene.get_pin_voltage("VDD", step="step0", index="1") == 2.5
+
+
+def test_timing_scanario_pin_voltage_missing():
+    scene = TimingScenarioSchema()
+    with pytest.raises(LookupError, match="VDD does not have voltage"):
+        scene.get_pin_voltage("VDD")
+
+
+def test_timing_scanario_libcorner():
+    scene = TimingScenarioSchema()
+    assert scene.add_libcorner("slow0")
+    assert scene.add_libcorner("slow1")
+    assert scene.add_libcorner("slow2", step="step0", index="1")
+    assert scene.get("libcorner") == ["slow0", "slow1"]
+    assert scene.get_libcorner() == ["slow0", "slow1"]
+    assert scene.get("libcorner", step="step0", index="1") == ["slow2"]
+    assert scene.get_libcorner(step="step0", index="1") == ["slow2"]
+
+
+def test_timing_scanario_libcorner_clobber():
+    scene = TimingScenarioSchema()
+    assert scene.add_libcorner("slow0")
+    assert scene.get_libcorner() == ["slow0"]
+    assert scene.add_libcorner("slow1", clobber=True)
+    assert scene.get_libcorner() == ["slow1"]
+
+
+def test_timing_scanario_libcorner_clobber_step_index():
+    scene = TimingScenarioSchema()
+    assert scene.add_libcorner("slow0")
+    assert scene.add_libcorner("slow1", step="step0", index="1")
+    assert scene.get_libcorner() == ["slow0"]
+    assert scene.get_libcorner(step="step0", index="1") == ["slow1"]
+    assert scene.add_libcorner("slow2", step="step0", index="1", clobber=True)
+    assert scene.get_libcorner() == ["slow0"]
+    assert scene.get_libcorner(step="step0", index="1") == ["slow2"]
+
+
+def test_timing_scanario_pexcorner():
+    scene = TimingScenarioSchema()
+    assert scene.set_pexcorner("ff")
+    assert scene.set_pexcorner("ss", step="step0", index="1")
+    assert scene.get("pexcorner") == "ff"
+    assert scene.get_pexcorner() == "ff"
+    assert scene.get("pexcorner", step="step0", index="1") == "ss"
+    assert scene.get_pexcorner(step="step0", index="1") == "ss"
+
+
+def test_timing_scanario_mode():
+    scene = TimingScenarioSchema()
+    assert scene.set_mode("run")
+    assert scene.set_mode("test", step="step0", index="1")
+    assert scene.get("mode") == "run"
+    assert scene.get_mode() == "run"
+    assert scene.get("mode", step="step0", index="1") == "test"
+    assert scene.get_mode(step="step0", index="1") == "test"
+
+
+def test_timing_scanario_opcond():
+    scene = TimingScenarioSchema()
+    assert scene.set_opcond("op0")
+    assert scene.set_opcond("op1", step="step0", index="1")
+    assert scene.get("opcond") == "op0"
+    assert scene.get_opcond() == "op0"
+    assert scene.get("opcond", step="step0", index="1") == "op1"
+    assert scene.get_opcond(step="step0", index="1") == "op1"
+
+
+def test_timing_scanario_temperature():
+    scene = TimingScenarioSchema()
+    assert scene.set_temperature(125.0)
+    assert scene.set_temperature(-40, step="step0", index="1")
+    assert scene.get("temperature") == 125.0
+    assert scene.get_temperature() == 125.0
+    assert scene.get("temperature", step="step0", index="1") == -40.0
+    assert scene.get_temperature(step="step0", index="1") == -40.0
+
+
+def test_timing_scanario_check():
+    scene = TimingScenarioSchema()
+    assert scene.add_check("setup")
+    assert scene.add_check("hold", step="step0", index="1")
+    assert scene.get("check") == ["setup"]
+    assert scene.get_check() == ["setup"]
+    assert scene.get("check", step="step0", index="1") == ["hold"]
+    assert scene.get_check(step="step0", index="1") == ["hold"]
+
+
+def test_timing_scanario_check_clobber():
+    scene = TimingScenarioSchema()
+    assert scene.add_check("setup")
+    assert scene.get_check() == ["setup"]
+    assert scene.add_check("hold", clobber=True)
+    assert scene.get_check() == ["hold"]
+
+
+def test_timing_scanario_check_clobber_step_index():
+    scene = TimingScenarioSchema()
+    assert scene.add_check("setup")
+    assert scene.add_check("hold", step="step0", index="1")
+    assert scene.get_check() == ["setup"]
+    assert scene.get_check(step="step0", index="1") == ["hold"]
+    assert scene.add_check("power", step="step0", index="1", clobber=True)
+    assert scene.get_check() == ["setup"]
+    assert scene.get_check(step="step0", index="1") == ["power"]
+
+
+def test_timing_scanario_sdcfileset_invalid_type_design():
+    with pytest.raises(TypeError, match="design must be a design object or string"):
+        TimingScenarioSchema().add_sdcfileset(1.0, "fileset")
+
+
+def test_timing_scanario_sdcfileset_invalid_type_fileset():
+    with pytest.raises(TypeError, match="fileset must be a string"):
+        TimingScenarioSchema().add_sdcfileset("test", 1.0)
+
+
+def test_timing_scanario_sdcfileset_design_obj():
+    scene = TimingScenarioSchema()
+
+    design = DesignSchema("test")
+    assert scene.add_sdcfileset(design, "rtl")
+    assert scene.get("sdcfileset") == [("test", "rtl")]
+    assert scene.get_sdcfileset() == [("test", "rtl")]
+
+
+def test_timing_scanario_sdcfileset_design_str():
+    scene = TimingScenarioSchema()
+
+    assert scene.add_sdcfileset("test", "rtl")
+    assert scene.get("sdcfileset") == [("test", "rtl")]
+    assert scene.get_sdcfileset() == [("test", "rtl")]
+
+
+def test_timing_scanario_sdcfileset_design_step_index():
+    scene = TimingScenarioSchema()
+
+    assert scene.add_sdcfileset("test", "rtl")
+    assert scene.add_sdcfileset("test1", "rtl1", step="step0", index="1")
+    assert scene.get("sdcfileset") == [("test", "rtl")]
+    assert scene.get_sdcfileset() == [("test", "rtl")]
+    assert scene.get("sdcfileset", step="step0", index="1") == [("test1", "rtl1")]
+    assert scene.get_sdcfileset(step="step0", index="1") == [("test1", "rtl1")]
+
+
+def test_timing_scanario_sdcfileset_design_clobber():
+    scene = TimingScenarioSchema()
+
+    assert scene.add_sdcfileset("test", "rtl")
+    assert scene.add_sdcfileset("test1", "rtl1", step="step0", index="1")
+    assert scene.get_sdcfileset() == [("test", "rtl")]
+    assert scene.get_sdcfileset(step="step0", index="1") == [("test1", "rtl1")]
+    assert scene.add_sdcfileset("test1", "rtl", step="step0", index="1", clobber=True)
+    assert scene.get_sdcfileset() == [("test", "rtl")]
+    assert scene.get_sdcfileset(step="step0", index="1") == [("test1", "rtl")]
+
+
+def test_timing_constraint_keys():
+    assert TimingConstraintSchema().allkeys() == set([
+        ('default', 'check',),
+        ('default', 'libcorner',),
+        ('default', 'mode',),
+        ('default', 'opcond',),
+        ('default', 'pexcorner',),
+        ('default', 'sdcfileset',),
+        ('default', 'temperature',),
+        ('default', 'voltage', 'default',),
+    ])
+
+
+def test_timing_constraint_get_scenarios_empty():
+    assert TimingConstraintSchema().get_scenario() == {}
+
+
+def test_timing_constraint_get_scenarios_missing():
+    with pytest.raises(LookupError, match="notfound is not defined"):
+        TimingConstraintSchema().get_scenario("notfound")
+
+
+def test_timing_constraint_add_scenario_invalid_type():
+    with pytest.raises(TypeError, match="scenario must be a timing scenario object"):
+        TimingConstraintSchema().add_scenario(TimingConstraintSchema())
+
+
+def test_timing_constraint_add_scenario_unnamed():
+    with pytest.raises(ValueError, match="scenario must have a name"):
+        TimingConstraintSchema().add_scenario(TimingScenarioSchema())
+
+
+def test_timing_constraint_add_scenario():
+    scene = TimingScenarioSchema("slow")
+    schema = TimingConstraintSchema()
+
+    schema.add_scenario(scene)
+    assert schema.getkeys() == ("slow",)
+    assert schema.get_scenario("slow") is scene
+
+
+def test_timing_constraint_get_scenario():
+    scene_s = TimingScenarioSchema("slow")
+    scene_f = TimingScenarioSchema("fast")
+    schema = TimingConstraintSchema()
+
+    schema.add_scenario(scene_s)
+    schema.add_scenario(scene_f)
+    assert schema.getkeys() == ("fast", "slow")
+    assert schema.get_scenario() == {
+        "slow": scene_s,
+        "fast": scene_f
+    }
