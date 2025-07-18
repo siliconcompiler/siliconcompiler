3a8b75637e7a876c1fbb6f573cf8e6c7b3c729e4
Peter Gadfort <gadfort@zeroasic.com>
2025-07-18 09:43:18 -0400

    remove stray posix

--- tests/utils/test_issue.py
+++ tests/utils/test_issue.py
@@ -41,7 +41,6 @@ def test_automatic_issue(gcd_chip):
 
 def test_relpath(gcd_chip):
     path = os.path.abspath('test.file')
-    path = pathlib.PureWindowsPath(path).as_posix()
     with open(path, 'w') as f:
         f.write('test')
 
