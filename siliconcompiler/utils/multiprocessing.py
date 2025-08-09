eba2ad9d04b2c8a30e3c6e6f3267f7f7c43bb6a3
Peter Gadfort <peter.gadfort@gmail.com>
2025-08-09 13:13:03 -0400

    only keep logfile on errors

--- siliconcompiler/utils/multiprocessing.py
+++ siliconcompiler/utils/multiprocessing.py
@@ -27,34 +27,64 @@ class MPManager(metaclass=_ManagerSingleton):
         pass
 
     def _init_singleton(self):
+        self.__start = datetime.now()
+        self.__error = False
+
         # Parent logger
-        self.__logger = logging.getLogger("siliconcompiler")
-        self.__logger.propagate = False
-        if self.__ENABLE_LOGGER:
-            self.__logger.setLevel(logging.INFO)
-            try:
-                log_dir = os.path.join(tempfile.gettempdir(), "siliconcompiler")
-                os.makedirs(log_dir, exist_ok=True)
-                now = datetime.now()
-                now_file = now.strftime("%Y-%m-%d-%H-%M-%S-%f")
-                now_print = now.strftime("%Y-%m-%d %H:%M:%S.%f")
-                self.__logger.addHandler(logging.FileHandler(
-                    os.path.join(log_dir, f"{now_file}_{id(self)}.log")))
-                self.__logger.info(f"Log started at {now_print}")
-            except:  # noqa E722
-                pass
+        now_file = self.__start.strftime("%Y-%m-%d-%H-%M-%S-%f")
+        self.__logfile = os.path.join(tempfile.gettempdir(),
+                                      "siliconcompiler",
+                                      f"{now_file}_{id(self)}.log")
+        self._init_logger()
 
         # Manager to thread data
         self.__manager = multiprocessing.Manager()
 
         atexit.register(self.stop)
 
+    def _init_logger(self):
+        # Root logger
+        self.__logger = logging.getLogger("siliconcompiler")
+        self.__logger.propagate = False
+
+        if self.__ENABLE_LOGGER:
+            self.__logger.setLevel(logging.INFO)
+            try:
+                os.makedirs(os.path.dirname(self.__logfile), exist_ok=True)
+
+                handler = logging.FileHandler(self.__logfile)
+                handler.setFormatter(logging.Formatter(
+                    '%(asctime)s | %(name)s | %(levelname)s | %(message)s'))
+                handler.setLevel(logging.INFO)
+
+                self.__logger.addHandler(handler)
+
+                now_print = self.__start.strftime("%Y-%m-%d %H:%M:%S.%f")
+                self.__logger.info(f"Log started at {now_print}")
+
+                handler.setLevel(logging.WARNING)
+            except:  # noqa E722
+                pass
+
     def stop(self):
         # Remove all logger handlers
         for handler in list(self.__logger.handlers):
             self.__logger.removeHandler(handler)
+        if not self.__error:
+            try:
+                os.remove(self.__logfile)
+            except:  # noqa E722
+                pass
 
         self.__manager.shutdown()
+
+    @staticmethod
+    def error(msg: str = None):
+        if msg:
+            MPManager().logger().error(f"Error: {msg}")
+        else:
+            MPManager().logger().error("Error occurred")
+        MPManager().__error = True
 
     @staticmethod
     def get_manager():
