6bbe6472c7e52c46415657639d7a4b4ba1b912e9
Peter Gadfort <peter.gadfort@gmail.com>
2025-08-12 15:13:35 -0400

    remove unsed files

--- docs/user_guide/examples/setget.py
+++
@@ -1,4 +0,0 @@
-import siliconcompiler
-chip = siliconcompiler.Chip('hello_world')
-chip.set('input', 'rtl', 'verilog', 'hello_world.v')
-print(chip.get('input', 'rtl', 'verilog'))
--- tests/docs/examples/test_write_manifest.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def test_py(setup_docs_test):
-    import write_manifest  # noqa: F401
-
-    assert os.path.isfile('hello_world.json')
