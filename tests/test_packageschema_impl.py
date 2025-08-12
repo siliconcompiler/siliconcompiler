d6a19618cf6c13daec7ccbfdf883513ff9f01245
Peter Gadfort <peter.gadfort@gmail.com>
2025-08-12 09:56:32 -0400

    fix metric

--- tests/test_packageschema_impl.py
+++ tests/test_packageschema_impl.py
@@ -5,68 +5,6 @@ import os.path
 from pathlib import Path
 
 from siliconcompiler.packageschema import PackageSchema
-from siliconcompiler.packageschema import PackageSchemaTmp
-
-
-def test_register():
-    schema = PackageSchemaTmp()
-    assert schema.getkeys("source") == tuple()
-    assert schema.register("testpackage", "pathtosource") is True
-    assert schema.get("source", "testpackage", "path") == "pathtosource"
-    assert schema.get("source", "testpackage", "ref") is None
-
-
-def test_register_ref():
-    schema = PackageSchemaTmp()
-    assert schema.getkeys("source") == tuple()
-    assert schema.register("testpackage", "pathtosource", ref="123456") is True
-    assert schema.get("source", "testpackage", "path") == "pathtosource"
-    assert schema.get("source", "testpackage", "ref") == "123456"
-
-
-def test_register_ref_no_clobber():
-    schema = PackageSchemaTmp()
-    assert schema.getkeys("source") == tuple()
-    assert schema.register("testpackage", "pathtosource", ref="123456") is True
-    assert schema.get("source", "testpackage", "path") == "pathtosource"
-    assert schema.get("source", "testpackage", "ref") == "123456"
-    assert schema.register("testpackage", "pathtosource", ref="1234567", clobber=False) is False
-    assert schema.get("source", "testpackage", "ref") == "123456"
-
-
-def test_register_ref_with_clobber():
-    schema = PackageSchemaTmp()
-    assert schema.getkeys("source") == tuple()
-    assert schema.register("testpackage", "pathtosource", ref="123456") is True
-    assert schema.get("source", "testpackage", "path") == "pathtosource"
-    assert schema.get("source", "testpackage", "ref") == "123456"
-    assert schema.register("testpackage", "pathtosource", ref="1234567", clobber=True) is True
-    assert schema.get("source", "testpackage", "ref") == "1234567"
-
-
-def test_register_ref_file():
-    schema = PackageSchemaTmp()
-    assert schema.getkeys("source") == tuple()
-    with open("test.txt", "w") as f:
-        f.write("test")
-
-    assert schema.register("testpackage", "test.txt") is True
-    assert schema.get("source", "testpackage", "path") == os.path.abspath(".")
-    assert schema.get("source", "testpackage", "ref") is None
-
-
-def test_register_ref_dir():
-    schema = PackageSchemaTmp()
-    assert schema.getkeys("source") == tuple()
-
-    assert schema.register("testpackage", ".") is True
-    assert schema.get("source", "testpackage", "path") == os.path.abspath(".")
-    assert schema.get("source", "testpackage", "ref") is None
-
-
-def test_get_resolvers_empty():
-    schema = PackageSchemaTmp()
-    assert schema.get_resolvers() == {}
 
 
 def test_keys():
