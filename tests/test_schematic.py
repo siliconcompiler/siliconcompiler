19a2fb9a5c4cb61c24d821215afe62b917ac4c58
Andreas Olofsson <andreas@zeroasic.com>
2025-08-12 20:21:54 -0400

    Adding basic schematic tests

--- tests/test_schematic.py
+++ tests/test_schematic.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from siliconcompiler import SchematicSchema
 
-def test_design_keys():
+def test_schematic_keys():
     golden_keys = set([
         ('schematic', 'hierchar'),
         ('schematic', 'buschar'),
@@ -16,3 +16,50 @@ def test_design_keys():
     ])
 
     assert set(SchematicSchema("test").allkeys()) == golden_keys
+
+
+def test_addget_pin():
+    d = SchematicSchema("test")
+
+    # passing
+    assert d.add_pin("pin0", "output")
+    assert d.add_pin("pin1", "input")
+    assert d.add_pin("pin2", "inout")
+
+    # check that pin was set correctly
+    assert d.get_pindir("pin0") == "output"
+
+    # failing testcase
+    with pytest.raises(ValueError, match="error"):
+        d.add_pin("input", "pin1")
+
+def test_all_pins():
+    d = SchematicSchema("test")
+
+    assert d.add_pin("pin0", "output")
+    assert d.add_pin("pin1", "input")
+    assert d.add_pin("pin2", "inout")
+
+    assert d.all_pins() == ('pin0', 'pin1', 'pin2')
+
+def test_component():
+    d = SchematicSchema("test")
+
+    assert d.add_component("i0", "NAND2")
+    assert d.get_partname("i0") == "NAND2"
+
+def test_all_components():
+    d = SchematicSchema("test")
+
+    assert d.add_component("i0", "NAND2")
+    assert d.add_component("i1", "NOR2")
+
+    assert d.all_components() == ('i0', 'i1')
+
+def test_net():
+    d = SchematicSchema("test")
+
+    assert d.connect_net("pin0", "netA")
+    assert d.connect_net("pin1", "netA")
+
+    assert d.get_net("netA") == ["pin0", "pin1"]
