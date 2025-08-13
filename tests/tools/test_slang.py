7206439c351c624e2278389ad8af63ae558a9e66
Peter Gadfort <gadfort@zeroasic.com>
2025-08-13 09:18:07 -0400

    fix slang

--- tests/tools/test_slang.py
+++ tests/tools/test_slang.py
@@ -1,14 +1,13 @@
-import os
+import os.path
 
 from siliconcompiler import FlowgraphSchema
-from siliconcompiler.project import LintProject
+from siliconcompiler import Project
 from siliconcompiler.tools.slang import lint
 from siliconcompiler.tools.slang import elaborate
-from siliconcompiler.targets import freepdk45_demo
 
 
 def test_lint(heartbeat_design):
-    proj = LintProject(heartbeat_design)
+    proj = Project(heartbeat_design)
     proj.add_fileset("rtl")
 
     flow = FlowgraphSchema("lint")
@@ -21,135 +20,35 @@ def test_lint(heartbeat_design):
     assert proj.get('metric', 'warnings', step='lint', index='0') == 0
 
 
-def test_surelog(scroot):
-    gcd_src = os.path.join(scroot, 'examples', 'gcd', 'gcd.v')
-    design = "gcd"
-    step = "elaborate"
+def test_elaborate(heartbeat_design):
+    proj = Project(heartbeat_design)
+    proj.add_fileset("rtl")
 
-    chip = siliconcompiler.Chip(design)
-    chip.use(freepdk45_demo)
+    flow = FlowgraphSchema("elaborate")
+    flow.node("elaborate", elaborate.Elaborate())
+    proj.set_flow(flow)
 
-    chip.input(gcd_src)
-    chip.node('slang', step, elaborate)
-    chip.set('option', 'flow', 'slang')
+    assert proj.run()
 
-    assert chip.run()
+    assert proj.get('metric', 'errors', step='elaborate', index='0') == 0
+    assert proj.get('metric', 'warnings', step='elaborate', index='0') == 0
 
-    output = chip.find_result('v', step=step)
-    assert output is not None
-
-
-def test_surelog_duplicate_inputs(scroot):
-    gcd_src = os.path.join(scroot, 'examples', 'gcd', 'gcd.v')
-    design = "gcd"
-    step = "elaborate"
-
-    chip = siliconcompiler.Chip(design)
-    chip.use(freepdk45_demo)
-
-    # Set duplicate input files.
-    chip.input(gcd_src)
-    chip.input(gcd_src)
-
-    chip.set('option', 'clean', True)
-    chip.node('slang', step, elaborate)
-    chip.set('option', 'flow', 'slang')
-
-    assert chip.run()
-
-    output = chip.find_result('v', step=step)
-    assert output is not None
-
-    # Ensure only one module is written to the output Verilog. (Two will be written if dedup fails)
-    module_count = 0
-    with open(output, 'r') as rf:
-        for line in rf.readlines():
-            if line.startswith('module gcd'):
-                module_count += 1
-    assert module_count == 1
+    assert proj.find_result("v", step="elaborate") == \
+        os.path.abspath("build/heartbeat/job0/elaborate/0/outputs/heartbeat.v")
 
 
-def test_surelog_preproc_regression(datadir):
-    src = os.path.join(datadir, 'test_preproc.v')
-    design = 'test_preproc'
-    step = "elaborate"
+def test_slang_duplicate_inputs(heartbeat_design):
+    heartbeat_design.copy_fileset("rtl", "rtl_double")
+    heartbeat_design.add_file(heartbeat_design.get_file("rtl", "verilog"), "rtl_double")
 
-    chip = siliconcompiler.Chip(design)
-    chip.use(freepdk45_demo)
-    chip.node('slang', step, elaborate)
-    chip.input(src)
-    chip.add('option', 'define', 'MEM_ROOT=test')
-    chip.set('option', 'flow', 'slang')
+    proj = Project(heartbeat_design)
+    proj.add_fileset("rtl_double")
 
-    assert chip.run()
+    flow = FlowgraphSchema("lint")
+    flow.node("lint", lint.Lint())
+    proj.set_flow(flow)
 
-    result = chip.find_result('v', step=step)
+    assert proj.run()
 
-    assert result is not None
-
-    with open(result, 'r') as vlog:
-        assert "`MEM_ROOT" not in vlog.read()
-
-
-def test_github_issue_1789(datadir):
-    chip = siliconcompiler.Chip('encode_stream_sc_module_8')
-    chip.use(freepdk45_demo)
-
-    i_file = os.path.join(datadir,
-                          'gh1789',
-                          'encode_stream_sc_module_8.v')
-
-    chip.input(i_file)
-    chip.set('option', 'to', ['import.verilog'])
-    chip.node('slang', "import.verilog", elaborate)
-    chip.set('option', 'flow', 'slang')
-
-    assert chip.run()
-
-    i_file_data = None
-    with open(i_file, 'r') as f:
-        i_file_data = f.read()
-    i_file_data = "\n".join([line for line in i_file_data.splitlines() if line.strip()])
-
-    o_file_data = None
-    o_file = chip.find_result('v', step='import.verilog')
-    with open(o_file, 'r') as f:
-        o_file_data = f.read()
-
-    # Remove SC header and footer
-    o_file_data = "\n".join([line for line in o_file_data.splitlines() if line.strip()][3:-3])
-
-    assert i_file_data == o_file_data
-
-
-def test_github_issue_1789_no_source(datadir):
-    chip = siliconcompiler.Chip('encode_stream_sc_module_8')
-    chip.use(freepdk45_demo)
-
-    i_file = os.path.join(datadir,
-                          'gh1789',
-                          'encode_stream_sc_module_8.v')
-
-    chip.input(i_file)
-    chip.set('option', 'to', ['import.verilog'])
-    chip.node('slang', "import.verilog", elaborate)
-    chip.set('option', 'flow', 'slang')
-
-    chip.set('tool', 'slang', 'task', 'elaborate', 'var', 'include_source_paths', False)
-
-    chip.run()
-
-    i_file_data = None
-    with open(i_file, 'r') as f:
-        i_file_data = f.read()
-    i_file_data = "\n".join([line for line in i_file_data.splitlines() if line.strip()])
-
-    o_file_data = None
-    o_file = chip.find_result('v', step='import.verilog')
-    with open(o_file, 'r') as f:
-        o_file_data = f.read()
-
-    # Remove SC header and footer
-    o_file_data = "\n".join([line for line in o_file_data.splitlines() if line.strip()][2:-2])
-
-    assert i_file_data == o_file_data
+    assert proj.get('metric', 'errors', step='lint', index='0') == 0
+    assert proj.get('metric', 'warnings', step='lint', index='0') == 0
