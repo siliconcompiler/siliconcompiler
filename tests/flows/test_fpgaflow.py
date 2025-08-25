1b0e0ce2a483b0766c9c4b1440a4b9587f7b27a8
Peter Gadfort <peter.gadfort@gmail.com>
2025-08-25 14:43:07 -0400

    fix fpgaflow file

--- tests/flows/test_fpgaflow.py
+++ tests/flows/test_fpgaflow.py
@@ -1,9 +1,9 @@
 import os
 import json
 import pytest
-from siliconcompiler.scheduler import SchedulerNode
-from siliconcompiler.flows import fpgaflow
-from siliconcompiler.tools.vpr import route, place
+# from siliconcompiler.scheduler import SchedulerNode
+# from siliconcompiler.flows import fpgaflow
+# from siliconcompiler.tools.vpr import route, place
 # from logiklib.demo.K4_N8_6x6 import K4_N8_6x6
 # from logiklib.demo.K6_N8_3x3 import K6_N8_3x3
 # from logiklib.demo.K6_N8_12x12_BD import K6_N8_12x12_BD
