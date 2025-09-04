0c2afc964625e1ec557464d4b66768bdfe112761
Andreas Olofsson <andreas@zeroasic.com>
2025-09-04 11:32:52 -0400

    Resolving schematic merge conflict

--- siliconcompiler/__init__.py
+++ siliconcompiler/__init__.py
@@ -6,9 +6,6 @@ from siliconcompiler.packageschema import PackageSchema
 
 from siliconcompiler.library import LibrarySchema, ToolLibrarySchema, StdCellLibrarySchema
 
-from siliconcompiler.schematic import Schematic
-
-from siliconcompiler.fpga import FPGASchema
 from siliconcompiler.design import DesignSchema
 from siliconcompiler.record import RecordSchema
 from siliconcompiler.metric import MetricSchema
@@ -22,6 +19,8 @@ from siliconcompiler.option import OptionSchema
 from siliconcompiler.project import Project
 from siliconcompiler.asic import ASICProject, ASICTaskSchema
 from siliconcompiler.fpga import FPGASchema, FPGAProject
+
+from siliconcompiler.schematic import Schematic
 
 from siliconcompiler._metadata import version as __version__
 
@@ -42,9 +41,7 @@ __all__ = [
     "FPGASchema",
     "PackageSchema",
     "OptionSchema",
-
     "Schematic",
-
     "Project",
     "ASICProject",
     "FPGAProject",
