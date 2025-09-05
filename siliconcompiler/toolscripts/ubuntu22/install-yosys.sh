f1553204a2ee339c92f2a570d65b09d852817783
Peter Gadfort <gadfort@zeroasic.com>
2025-09-05 13:57:37 -0400

    update yosys dependencies

--- siliconcompiler/toolscripts/ubuntu22/install-yosys.sh
+++ siliconcompiler/toolscripts/ubuntu22/install-yosys.sh
@@ -14,7 +14,7 @@ fi
 
 # From: https://github.com/YosysHQ/yosys/blob/f2c689403ace0637b7455bac8f1e8d4bc312e74f/README.md
 sudo apt-get install -y build-essential clang bison flex \
-	libreadline-dev gawk tcl-dev libffi-dev git \
+	libreadline-dev gawk tcl-dev libffi-dev git libfl-dev \
 	graphviz xdot pkg-config python3 libboost-system-dev \
 	libboost-python-dev libboost-filesystem-dev zlib1g-dev
 
