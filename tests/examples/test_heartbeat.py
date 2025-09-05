178706bf3d6f265c858bdc2f82c1c94103ba5f6b
Peter Gadfort <peter.gadfort@gmail.com>
2025-09-05 17:44:27 -0400

    fix heartbeat tests

--- tests/examples/test_heartbeat.py
+++ tests/examples/test_heartbeat.py
@@ -36,12 +36,12 @@ def test_py_make_syn(pdk):
     make.syn(pdk=pdk)
 
     assert os.path.isfile('build/heartbeat/job0/heartbeat.pkg.json')
-    assert os.path.exists('build/heartbeat/job0/write.gds/0/outputs/heartbeat.gds')
 
 
 @pytest.mark.eda
 @pytest.mark.quick
 @pytest.mark.ready
+@pytest.mark.timeout(300)
 @pytest.mark.parametrize("pdk", ("freepdk45", "asap7"))
 def test_py_make_asic(pdk):
     from heartbeat import make
@@ -54,22 +54,22 @@ def test_py_make_asic(pdk):
 @pytest.mark.eda
 @pytest.mark.quick
 @pytest.mark.ready
-def test_py_make_sim_verilator():
+def test_py_make_sim_icarus():
     from heartbeat import make
     make.sim(tool="icarus")
 
-    assert os.path.isfile('build/heartbeat/job0/heartbeat_tb.pkg.json')
-    assert os.path.isfile('build/heartbeat/job0/sim/0/reports/heartbeat_tb.vcd')
+    assert os.path.isfile('build/heartbeat/job0/heartbeat.pkg.json')
+    assert os.path.isfile('build/heartbeat/job0/simulate/0/reports/heartbeat_tb.vcd')
 
 
 @pytest.mark.eda
 @pytest.mark.quick
 @pytest.mark.ready
-def test_py_make_sim_icarus():
+def test_py_make_sim_verilator():
     from heartbeat import make
     make.sim()
 
-    assert os.path.isfile('build/heartbeat/job0/heartbeat_tb.pkg.json')
+    assert os.path.isfile('build/heartbeat/job0/heartbeat.pkg.json')
 
 
 @pytest.mark.eda
