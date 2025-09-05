5ce917cd0f7c306d60ca5f0310c20ab45da73761
Peter Gadfort <gadfort@zeroasic.com>
2025-09-05 17:06:12 -0400

    update heartbeat example CI

--- tests/examples/test_heartbeat.py
+++ tests/examples/test_heartbeat.py
@@ -1,10 +1,12 @@
-import os
 import pytest
 import shutil
+
+import os.path
 
 
 @pytest.mark.eda
 @pytest.mark.quick
+@pytest.mark.ready
 @pytest.mark.timeout(300)
 def test_py_heartbeat():
     from heartbeat import heartbeat
@@ -13,76 +15,68 @@ def test_py_heartbeat():
     assert os.path.exists('build/heartbeat/job0/write.gds/0/outputs/heartbeat.gds')
 
 
-@pytest.mark.eda
-@pytest.mark.quick
-def test_py_heartbeat_sim():
-    from heartbeat import heartbeat_sim
-    heartbeat_sim.main()
-
-    assert os.path.isfile('build/heartbeat_tb/job0/heartbeat_tb.pkg.json')
-    assert os.path.isfile('build/heartbeat_tb/job0/sim/0/reports/heartbeat_tb.vcd')
+def test_py_make_check():
+    from heartbeat import make
+    make.check()
 
 
-@pytest.mark.eda
-@pytest.mark.quick
-def test_py_heartbeat_sim_verilator():
-    from heartbeat import heartbeat_sim_verilator
-    heartbeat_sim_verilator.main()
+def test_py_make_lint():
+    from heartbeat import make
+    make.lint()
 
     assert os.path.isfile('build/heartbeat/job0/heartbeat.pkg.json')
 
 
 @pytest.mark.eda
 @pytest.mark.quick
-@pytest.mark.timeout(300)
-def test_sh_run(examples_root, run_cli):
-    run_cli(os.path.join(examples_root, 'heartbeat', 'run.sh'),
-            'build/heartbeat/job0/write.gds/0/outputs/heartbeat.gds')
+@pytest.mark.ready
+@pytest.mark.parametrize("pdk", ("freepdk45", "asap7"))
+def test_py_make_syn(pdk):
+    from heartbeat import make
+    make.syn(pdk=pdk)
+
+    assert os.path.isfile('build/heartbeat/job0/heartbeat.pkg.json')
+    assert os.path.exists('build/heartbeat/job0/write.gds/0/outputs/heartbeat.gds')
 
 
 @pytest.mark.eda
-@pytest.mark.timeout(600)
-def test_parallel_all_serial():
-    from heartbeat import parallel
-    parallel.all_serial()
+@pytest.mark.quick
+@pytest.mark.ready
+@pytest.mark.parametrize("pdk", ("freepdk45", "asap7"))
+def test_py_make_asic(pdk):
+    from heartbeat import make
+    make.asic(pdk=pdk)
 
-    assert os.path.isdir('build/heartbeat/serial_0_0')
-    assert os.path.isdir('build/heartbeat/serial_0_1')
-    assert os.path.isdir('build/heartbeat/serial_1_0')
-    assert os.path.isdir('build/heartbeat/serial_1_1')
-
-
-@pytest.mark.eda
-@pytest.mark.timeout(600)
-def test_parallel_steps():
-    from heartbeat import parallel
-    parallel.parallel_steps()
-
-    assert os.path.isdir('build/heartbeat/parasteps_0')
-    assert os.path.isdir('build/heartbeat/parasteps_1')
+    assert os.path.isfile('build/heartbeat/job0/heartbeat.pkg.json')
+    assert os.path.exists('build/heartbeat/job0/write.gds/0/outputs/heartbeat.gds')
 
 
 @pytest.mark.eda
-@pytest.mark.timeout(300)
-def test_parallel_flows():
-    from heartbeat import parallel
-    parallel.parallel_flows()
+@pytest.mark.quick
+@pytest.mark.ready
+def test_py_make_sim_verilator():
+    from heartbeat import make
+    make.sim(tool="icarus")
 
-    assert os.path.isdir('build/heartbeat/paraflows_0')
-    assert os.path.isdir('build/heartbeat/paraflows_1')
+    assert os.path.isfile('build/heartbeat/job0/heartbeat_tb.pkg.json')
+    assert os.path.isfile('build/heartbeat/job0/sim/0/reports/heartbeat_tb.vcd')
 
 
-def test_py_parallel():
-    # Dummy test to simplify test checking
-    # The parallel examples needs to be split into separate file
-    assert True
+@pytest.mark.eda
+@pytest.mark.quick
+@pytest.mark.ready
+def test_py_make_sim_icarus():
+    from heartbeat import make
+    make.sim()
+
+    assert os.path.isfile('build/heartbeat/job0/heartbeat_tb.pkg.json')
 
 
 @pytest.mark.eda
 @pytest.mark.timeout(300)
 @pytest.mark.skipif(shutil.which("vivado") is None, reason="Vivado is not available in CI")
-def test_py_heartbeat_fpga():
-    from heartbeat import heartbeat_fpga
-    heartbeat_fpga.main()
+def test_py_make_fpga():
+    from heartbeat import make
+    make.make()
 
     assert os.path.isfile('build/heartbeat/job0/bitstream/0/outputs/heartbeat.bit')
