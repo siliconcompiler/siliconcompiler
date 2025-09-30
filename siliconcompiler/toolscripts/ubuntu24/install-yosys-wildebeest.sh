c00a476a953ca13b6edfb14d52a32c8630f61acf
Peter Gadfort <peter.gadfort@gmail.com>
2025-09-30 16:02:43 -0400

    add wildebeest plugin

---
+++ siliconcompiler/toolscripts/ubuntu24/install-yosys-wildebeest.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+# Get directory of script
+src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)/..
+
+sudo apt-get update
+sudo apt-get install -y git
+
+mkdir -p deps
+cd deps
+
+python3 -m venv .yosys-wildebeest --clear
+. .yosys-wildebeest/bin/activate
+python3 -m pip install cmake==3.31.6
+
+git clone $(python3 ${src_path}/_tools.py --tool yosys-wildebeest --field git-url) yosys-wildebeest
+cd yosys-wildebeest
+git checkout $(python3 ${src_path}/_tools.py --tool yosys-wildebeest --field git-commit)
+
+cmake -S . -B build
+cmake --build build
+
+USE_SUDO_INSTALL="${USE_SUDO_INSTALL:-yes}"
+if [ "${USE_SUDO_INSTALL:-yes}" = "yes" ]; then
+    SUDO_INSTALL="sudo -E PATH=$PATH"
+else
+    SUDO_INSTALL=""
+fi
+
+cd build
+$SUDO_INSTALL make install
+
+cd -
