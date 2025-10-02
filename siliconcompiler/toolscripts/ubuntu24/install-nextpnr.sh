5e437e388280cb4a6c36cc66dc500b1efff9bb14
Peter Gadfort <peter.gadfort@gmail.com>
2025-10-02 09:03:04 -0400

    update nextpnr

--- siliconcompiler/toolscripts/ubuntu24/install-nextpnr.sh
+++ siliconcompiler/toolscripts/ubuntu24/install-nextpnr.sh
@@ -14,15 +14,13 @@ sudo apt-get install -y git
 # Get directory of script
 src_path=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)/..
 
-USE_SUDO_INSTALL="${USE_SUDO_INSTALL:-yes}"
-if [ "${USE_SUDO_INSTALL:-yes}" = "yes" ]; then
-    SUDO_INSTALL=sudo
-else
-    SUDO_INSTALL=""
-fi
-
 mkdir -p deps
 cd deps
+
+python3 -m venv .nextpnr --clear
+. .nextpnr/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install cmake==3.28.4
 
 git clone $(python3 ${src_path}/_tools.py --tool nextpnr --field git-url) nextpnr
 cd nextpnr
@@ -33,7 +31,17 @@ if [ ! -z ${PREFIX} ]; then
     args=-DCMAKE_INSTALL_PREFIX="$PREFIX"
 fi
 
-cmake -DARCH=ice40 $args .
-make -j$(nproc)
+cmake -S . -B build -DARCH=ice40 $args
+cmake --build build
+
+USE_SUDO_INSTALL="${USE_SUDO_INSTALL:-yes}"
+if [ "${USE_SUDO_INSTALL:-yes}" = "yes" ]; then
+    SUDO_INSTALL="sudo -E PATH=$PATH"
+else
+    SUDO_INSTALL=""
+fi
+
+cd build
 $SUDO_INSTALL make install
+
 cd -
