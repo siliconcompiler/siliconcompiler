ba49d48887c45479d9b7418b085c1554539338bc
Fred Tombs <fred@zeroasic.com>
2025-10-10 15:35:52 -0400

    trim

--- tests/tools/test_wildbeest.py
+++ tests/tools/test_wildbeest.py
@@ -1,40 +1,25 @@
 import os
 import pytest
-from siliconcompiler.flows.fpgaflow import FPGAVPRFlow
-from siliconcompiler import Flowgraph
-from siliconcompiler.tools import get_task
-from siliconcompiler import FPGA
-from siliconcompiler.tools.yosys.syn_fpga import FPGASynthesis
+
+from siliconcompiler import Flowgraph, FPGA
+
 from siliconcompiler.scheduler import SchedulerNode
+
 from siliconcompiler.tools.slang import elaborate
-from siliconcompiler import FPGADevice, FPGA
-
-from siliconcompiler import FPGA, Design
-from siliconcompiler.flows.fpgaflow import FPGAVPROpenSTAFlow
-
-from siliconcompiler.tools.vpr import VPRFPGA
 from siliconcompiler.tools.yosys import YosysFPGA
-from siliconcompiler.tools.opensta import OpenSTAFPGA
+from siliconcompiler.tools.yosys.syn_fpga import FPGASynthesis
+
 from siliconcompiler.utils import sc_open
 
+
 class TestYosysFPGA(YosysFPGA):
-    '''
-    z1000 is the first in a series of open FPGA architectures.
-    The baseline z1000 part is an architecture with 2K LUTs
-    and no hard macros.
-    '''
     def __init__(self):
         super().__init__()
         self.set_name("z1000")
 
         self.set_dataroot("siliconcompiler", "python://siliconcompiler")
-        self.package.set_vendor("ZeroASIC")
-        self.set_lutsize(4)
 
-        # self.add_yosys_registertype(["dff", "dffr", "dffe", "dffer"])
-        # self.add_yosys_featureset(["async_reset", "enable"])
         with self.active_dataroot("siliconcompiler"):
-            # self.set_yosys_flipfloptechmap("data/demo_fpga/tech_flops.v")
             self.set_yosys_config('data/demo_fpga/z1000_yosys_config.json')
 
 
