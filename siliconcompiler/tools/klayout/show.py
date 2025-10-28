c10cd8a1fa2adfd6d479cb6e650b4136874f9e70
Peter Gadfort <peter.gadfort@gmail.com>
2025-10-28 14:41:06 -0400

    add handling of klayout screenshot as a flow task

--- siliconcompiler/tools/klayout/show.py
+++ siliconcompiler/tools/klayout/show.py
@@ -15,6 +15,17 @@ class ShowTask(ShowTask, KLayoutTask):
 
         self.set_script("klayout_show.py")
 
+        if f"{self.design_topmodule}.gds.gz" in self.get_files_from_input_nodes():
+            self.add_input_file(ext="gds.gz")
+        elif f"{self.design_topmodule}.gds" in self.get_files_from_input_nodes():
+            self.add_input_file(ext="gds")
+        elif f"{self.design_topmodule}.oas.gz" in self.get_files_from_input_nodes():
+            self.add_input_file(ext="oas.gz")
+        elif f"{self.design_topmodule}.oas" in self.get_files_from_input_nodes():
+            self.add_input_file(ext="oas")
+        else:
+            self.add_required_key("var", "showfilepath")
+
         self.add_commandline_option(["-nc", "-rm"], clobber=True)
 
     def get_supported_show_extentions(self):
@@ -23,8 +34,9 @@ class ShowTask(ShowTask, KLayoutTask):
     def pre_process(self):
         super().pre_process()
 
-        rel_path = os.path.dirname(self.get("var", "showfilepath"))
-        for ext in ('lyt', 'lyp'):
-            ext_file = os.path.join(rel_path, f'{self.design_topmodule}.{ext}')
-            if ext_file and os.path.exists(ext_file):
-                shutil.copy2(ext_file, f"inputs/{self.design_topmodule}.{ext}")
+        if self.get("var", "showfilepath"):
+            rel_path = os.path.dirname(self.get("var", "showfilepath"))
+            for ext in ('lyt', 'lyp'):
+                ext_file = os.path.join(rel_path, f'{self.design_topmodule}.{ext}')
+                if ext_file and os.path.exists(ext_file):
+                    shutil.copy2(ext_file, f"inputs/{self.design_topmodule}.{ext}")
