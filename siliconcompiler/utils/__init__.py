ab5de38a63c3cb2f46ea4a7eb6d516d16902a8f0
Peter Gadfort <gadfort@zeroasic.com>
2025-11-17 12:59:10 -0500

    simplify paths handling

--- siliconcompiler/utils/__init__.py
+++ siliconcompiler/utils/__init__.py
@@ -175,22 +175,20 @@ def get_default_iomap() -> Dict[str, str]:
     return default_iomap
 
 
-def default_credentials_file() -> str:
-    cfg_file = os.path.join(Path.home(), '.sc', 'credentials')
+def default_sc_dir() -> str:
+    return os.path.join(Path.home(), '.sc')
 
-    return cfg_file
+
+def default_credentials_file() -> str:
+    return os.path.join(default_sc_dir(), 'credentials')
 
 
 def default_cache_dir() -> str:
-    cfg_file = os.path.join(Path.home(), '.sc', 'cache')
-
-    return cfg_file
+    return os.path.join(default_sc_dir(), 'cache')
 
 
 def default_email_credentials_file() -> str:
-    cfg_file = os.path.join(Path.home(), '.sc', 'email.json')
-
-    return cfg_file
+    return os.path.join(default_sc_dir(), 'email.json')
 
 
 @contextlib.contextmanager
