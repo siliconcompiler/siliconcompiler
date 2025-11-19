10de4b4552e3e35d90b451ab48f698ffb4d31c8c
Peter Gadfort <peter.gadfort@gmail.com>
2025-11-19 13:02:20 -0500

    fix linelength lint

--- siliconcompiler/tools/klayout/scripts/klayout_utils.py
+++ siliconcompiler/tools/klayout/scripts/klayout_utils.py
@@ -129,7 +129,8 @@ def technology(design, schema):
                                                     map_file))
     for s in get_streams(schema):
         if schema.valid('library', sc_pdk, 'pdk', 'layermapfileset', 'klayout', 'def', s):
-            for fileset in schema.get('library', sc_pdk, 'pdk', 'layermapfileset', 'klayout', 'def', s):
+            for fileset in schema.get('library', sc_pdk, 'pdk', 'layermapfileset', 'klayout',
+                                      'def', s):
                 if schema.valid('library', sc_pdk, "fileset", fileset, "file", "layermap"):
                     map_file = schema.get('library', sc_pdk, "fileset", fileset, "file", "layermap")
                 if map_file:
