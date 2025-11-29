ccbedff89c5c5b9513e7d05886bbab6547c5e601
Peter Gadfort <peter.gadfort@gmail.com>
2025-11-28 20:04:48 -0500

    breakout verilog files

---
+++ examples/mod_A_in_mod_B/and.v
@@ -0,0 +1,12 @@
+module mod_and (
+    input  wire       clk,
+    input  wire [7:0] a,
+    input  wire [7:0] b,
+    output reg  [7:0] y
+);
+
+  always @(posedge clk) begin
+    y <= a & b;
+  end
+
+endmodule
--- tests/examples/test_mod_A_in_mod_B.py
+++ tests/examples/test_mod_A_in_mod_B.py
@@ -8,7 +8,7 @@ import os.path
 @pytest.mark.timeout(150)
 def test_py_silicon_compiler_mod_A_in_mod_B():
     from mod_A_in_mod_B import silicon_compiler_mod_A_in_mod_B
-    silicon_compiler_mod_A_in_mod_B.main()
+    silicon_compiler_mod_A_in_mod_B.build_top()
 
-    assert os.path.exists('build/A/job0/write.gds/0/outputs/A.gds')
-    assert os.path.exists('build/B/job0/write.gds/0/outputs/B.gds')
+    assert os.path.exists('build/and/job0/write.gds/0/outputs/mod_and.gds')
+    assert os.path.exists('build/top/job0/write.gds/0/outputs/top.gds')
