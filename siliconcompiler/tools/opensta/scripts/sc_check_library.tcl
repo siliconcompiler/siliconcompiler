374efb53f2ef8267500b4fe4832c076ad3d2e026
Peter Gadfort <peter.gadfort@gmail.com>
2025-12-04 08:58:33 -0500

    adjust tcl use of constraint timing to account for schema move

--- siliconcompiler/tools/opensta/scripts/sc_check_library.tcl
+++ siliconcompiler/tools/opensta/scripts/sc_check_library.tcl
@@ -22,7 +22,7 @@ set sc_design [sc_top]
 # APR Parameters
 set sc_targetlibs [sc_get_asic_libraries logic]
 set sc_delaymodel [sc_cfg_get asic delaymodel]
-set sc_scenarios [dict keys [sc_cfg_get constraint timing]]
+set sc_scenarios [dict keys [sc_cfg_get constraint timing scenario]]
 
 ###############################
 # Optional
@@ -41,7 +41,7 @@ define_corners {*}$sc_scenarios
 foreach lib "$sc_targetlibs $sc_macrolibs" {
     #Liberty
     foreach corner $sc_scenarios {
-        foreach libcorner [sc_cfg_get constraint timing $corner libcorner] {
+        foreach libcorner [sc_cfg_get constraint timing scenario $corner libcorner] {
             if { [sc_cfg_exists library $lib output $libcorner $sc_delaymodel] } {
                 foreach lib_file [sc_cfg_get library $lib output $libcorner $sc_delaymodel] {
                     puts "Reading liberty file for ${corner} ($libcorner): ${lib_file}"
