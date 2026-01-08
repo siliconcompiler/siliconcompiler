891baaac4284651693a71ccb5c0b731c44da12d9
Peter Gadfort <gadfort@zeroasic.com>
2026-01-08 15:16:53 -0500

    remove left over blah

--- siliconcompiler/tools/openroad/screenshot.py
+++ siliconcompiler/tools/openroad/screenshot.py
@@ -1,3 +1,5 @@
+from typing import Optional, Union
+
 from siliconcompiler import ScreenshotTask
 from siliconcompiler.tools.openroad.show import ShowTask
 
@@ -9,9 +11,35 @@ class ScreenshotTask(ScreenshotTask, ShowTask):
     def __init__(self):
         super().__init__()
 
-        self.add_parameter("show_vertical_resolution", "int", "blah", defvalue=1024)
+        self.add_parameter("show_vertical_resolution", "int",
+                           "Vertical resolution of the screenshot image",
+                           defvalue=1024)
         self.add_parameter("include_report_images", "bool",
                            "true/false, include the images in reports/", defvalue=False)
+
+    def set_openroad_verticalresolution(self, value: int,
+                                        step: Optional[str] = None, index: Optional[Union[int, str]] = None):
+        """
+        Set the vertical resolution for OpenROAD screenshots.
+
+        Args:
+            value (int): The vertical resolution in pixels.
+            step (Optional[str]): The step in the flow where this setting applies.
+            index (Optional[Union[int, str]]): The index for multiple runs or configurations.
+        """
+        self.set("var", "show_vertical_resolution", value, step=step, index=index)
+
+    def set_openroad_includereportimages(self, value: bool,
+                                         step: Optional[str] = None, index: Optional[Union[int, str]] = None):
+        """
+        Set whether to include report images in the output.
+
+        Args:
+            value (bool): Whether to include report images.
+            step (Optional[str]): The step in the flow where this setting applies.
+            index (Optional[Union[int, str]]): The index for multiple runs or configurations.
+        """
+        self.set("var", "include_report_images", value, step=step, index=index)
 
     def setup(self):
         super().setup()
