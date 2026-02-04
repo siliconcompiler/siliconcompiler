46b8f245856c2ec1d13bac768bded1609dd1b062
Peter Gadfort <gadfort@zeroasic.com>
2026-02-04 18:45:22 -0500

    add openroad and opensta version checks

--- tests/tools/test_opensta.py
+++ tests/tools/test_opensta.py
@@ -3,12 +3,27 @@ import pytest
 
 import os.path
 
+from siliconcompiler.scheduler import SchedulerNode
 from siliconcompiler import ASIC, Design, Flowgraph
 from siliconcompiler.tools.opensta import timing
 
 from siliconcompiler.targets import freepdk45_demo
 
 from tools.inputimporter import ImporterTask
+
+
+@pytest.mark.eda
+@pytest.mark.quick
+@pytest.mark.timeout(300)
+def test_version(asic_gcd):
+    flow = Flowgraph("testflow")
+    flow.node("version", timing.TimingTask())
+    asic_gcd.set_flow(flow)
+
+    node = SchedulerNode(asic_gcd, "version", "0")
+    with node.runtime():
+        assert node.setup() is True
+        assert node.task.check_exe_version(node.task.get_exe_version()) is True
 
 
 @pytest.mark.eda
