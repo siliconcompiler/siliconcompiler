ae3bacb558b31fb80b8b5d99e1241371982f9d2f
Peter Gadfort <gadfort@zeroasic.com>
2026-02-04 21:26:58 -0500

    fix remaining string list issue

--- siliconcompiler/tools/openroad/scripts/common/procs.tcl
+++ siliconcompiler/tools/openroad/scripts/common/procs.tcl
@@ -763,7 +763,7 @@ proc sc_check_version { min_major min_minor min_patch } {
             set ord_minor 3
             set ord_patch [expr { [lindex $version 1] - 15587 }]
         }
-    } elseif { [lindex [lindex $version 0] 3] == "Q" } {
+    } elseif { [string index [lindex $version 0] 2] == "Q" } {
         set ord_major [string range [lindex $version 0] 0 1]
         set ord_minor [string index [lindex $version 0] 3]
         set ord_patch [string range [lindex $version 0] 5 end]
