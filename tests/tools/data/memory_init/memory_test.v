44175af63bacd3c551db415869f0245073854a60
Frederick Tombs <fred@zeroasic.com>
2026-02-08 20:00:52 -0500

    Add .hex and .mem as input files to synthesis

---
+++ tests/tools/data/memory_init/memory_test.v
@@ -0,0 +1,21 @@
+module memory_test (
+    input clk,
+    input [3:0] addr,
+    output reg [7:0] data_hex,
+    output reg [7:0] data_mem
+);
+
+    reg [7:0] rom_hex [0:15];
+    reg [7:0] rom_mem [0:15];
+
+    initial begin
+        $readmemh("init.hex", rom_hex);
+        $readmemh("init.mem", rom_mem);
+    end
+
+    always @(posedge clk) begin
+        data_hex <= rom_hex[addr];
+        data_mem <= rom_mem[addr];
+    end
+
+endmodule
