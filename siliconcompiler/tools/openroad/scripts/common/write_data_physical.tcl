570119983c677b14c45729d227527855f198dee3
Peter Gadfort <gadfort@zeroasic.com>
2026-04-03 08:28:08 -0400

    adding some information about file IO

--- siliconcompiler/tools/openroad/scripts/common/write_data_physical.tcl
+++ siliconcompiler/tools/openroad/scripts/common/write_data_physical.tcl
@@ -1,5 +1,10 @@
+puts "Writing database: outputs/${sc_topmodule}.odb.gz"
 write_db "outputs/${sc_topmodule}.odb.gz"
+
+puts "Writing DEF: outputs/${sc_topmodule}.def.gz"
 write_def "outputs/${sc_topmodule}.def.gz"
+
+puts "Writing netlist: outputs/${sc_topmodule}.vg"
 write_verilog -include_pwr_gnd "outputs/${sc_topmodule}.vg"
 
 set remove_cells []
@@ -8,4 +13,5 @@ foreach lib [sc_cfg_get asic asiclib] {
         lappend remove_cells {*}[sc_cfg_get library $lib asic cells $celltype]
     }
 }
+puts "Writing LEC netlist: outputs/${sc_topmodule}.lec.vg"
 write_verilog -remove_cells $remove_cells "outputs/${sc_topmodule}.lec.vg"
--- siliconcompiler/tools/openroad/scripts/common/write_data_timing.tcl
+++ siliconcompiler/tools/openroad/scripts/common/write_data_timing.tcl
@@ -1,3 +1,4 @@
 if { [sc_cfg_tool_task_get var load_sdcs] } {
+    puts "Writing SDC: outputs/${sc_topmodule}.sdc"
     write_sdc "outputs/${sc_topmodule}.sdc"
 }
