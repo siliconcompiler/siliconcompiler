04bb11bd9c20f86480382acd6ce37d612eb53746
Peter Gadfort <gadfort@zeroasic.com>
2026-04-23 13:06:08 -0400

    add hier support to openroad

--- siliconcompiler/tools/openroad/scripts/common/read_input_files.tcl
+++ siliconcompiler/tools/openroad/scripts/common/read_input_files.tcl
@@ -30,10 +30,25 @@ if { [file exists "inputs/${sc_topmodule}.odb"] || [file exists "inputs/${sc_top
         }
     }
 
+    if { [file exists "inputs/${sc_topmodule}.vg.gz"] } {
+        set verilog_file "inputs/${sc_topmodule}.vg.gz"
+    } else {
+        set verilog_file "inputs/${sc_topmodule}.vg"
+    }
+
     if {
         [file exists "inputs/${sc_topmodule}.def"] ||
         [file exists "inputs/${sc_topmodule}.def.gz"]
     } {
+        set def_args []
+        # Read verilog if available and hier is enabled
+        if { [sc_cfg_tool_task_get var enablehier] && [file exists $verilog_file] } {
+            puts "Reading netlist verilog: ${verilog_file}"
+            read_verilog $verilog_file
+            link_design -hier $sc_topmodule
+            lappend def_args -floorplan_initialize
+        }
+
         # Read DEF
         # get from previous step
         if { [file exists "inputs/${sc_topmodule}.def.gz"] } {
@@ -42,11 +57,11 @@ if { [file exists "inputs/${sc_topmodule}.odb"] || [file exists "inputs/${sc_top
             set def_file "inputs/${sc_topmodule}.def"
         }
         puts "Reading DEF: ${def_file}"
-        read_def $def_file
-    } elseif { [file exists "inputs/${sc_topmodule}.vg"] } {
+        read_def {*}$def_args $def_file
+    } elseif { [file exists $verilog_file] } {
         # Read Verilog
-        puts "Reading netlist verilog: inputs/${sc_topmodule}.vg"
-        read_verilog "inputs/${sc_topmodule}.vg"
+        puts "Reading netlist verilog: ${verilog_file}"
+        read_verilog $verilog_file
         link_design $sc_topmodule
     } else {
         utl::error FLW 1 "No input files available"
