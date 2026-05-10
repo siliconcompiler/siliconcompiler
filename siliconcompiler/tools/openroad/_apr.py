b6a26106bd7cb2f10b81b597f5b1918d16105cd8
Peter Gadfort <gadfort@zeroasic.com>
2026-05-10 17:19:05 -0400

    fix step/index

--- siliconcompiler/tools/openroad/_apr.py
+++ siliconcompiler/tools/openroad/_apr.py
@@ -7,6 +7,7 @@ from typing import List, Union, Optional, Set
 from siliconcompiler import sc_open
 from siliconcompiler import utils
 from siliconcompiler.asic import CellArea
+from siliconcompiler.schema import BaseSchema
 from siliconcompiler.schema.parametertype import NodeType
 
 
@@ -1266,7 +1267,7 @@ class APRTask(OpenROADTask):
             patterns = list(report_type)
 
         if any("*" in pattern for pattern in patterns):
-            enum_type = list(NodeType.parse(self.get("var", "skip_reports", field="type")))[0]
+            enum_type = list(NodeType.parse(BaseSchema.get(self, "var", "skip_reports", field="type")))[0]
             supported_report_types = NodeType.parse(enum_type).values
             expanded: List[str] = []
             for pattern in patterns:
