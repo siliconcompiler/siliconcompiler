434768b61409cf0f6eab22ea4766faee1ab07a66
Peter Gadfort <gadfort@zeroasic.com>
2026-06-09 09:00:05 -0400

    remove python 3.9 support

--- tests/scheduler/test_error.py
+++ tests/scheduler/test_error.py
@@ -10,7 +10,6 @@ def test_msg():
     assert exception.msg == "msg"
 
 
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="error changed between 3.9 and 3.10")
 def test_require_arg():
     with pytest.raises(TypeError,
                        match=r"^SCRuntimeError\.__init__\(\) missing 1 required positional "
