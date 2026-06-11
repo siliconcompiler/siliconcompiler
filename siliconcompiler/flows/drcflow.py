e5e5784c2b5c0c42e364f20f98a810bc716fea75
Marcus Mellor <infinitymdm@gmail.com>
2026-06-11 11:22:50 -0500

    DRCFlow: Add 'tool' argument for selecting between magic and klayout

--- siliconcompiler/flows/drcflow.py
+++ siliconcompiler/flows/drcflow.py
@@ -1,5 +1,6 @@
 from siliconcompiler import Flowgraph
-from siliconcompiler.tools.klayout import drc
+from siliconcompiler.tools.klayout import drc as klayout_drc
+from siliconcompiler.tools.magic import drc as magic_drc
 
 
 class DRCFlow(Flowgraph):
@@ -8,16 +9,25 @@ class DRCFlow(Flowgraph):
     This flow is designed to perform a DRC run on an input GDSII file using
     KLayout.
     '''
-    def __init__(self, name: str = "drcflow"):
+    def __init__(self, name: str = "drcflow", tool: str = "klayout"):
         """
         Initializes the DRCFlow.
 
         Args:
-            name (str): The name of the flow.
+            * name (str): The name of the flow.
+            * tool (str): The DRC tool to use. Supported options are 'klayout' and 'magic'.
+
+        Raises:
+            ValueError: If an unsupported tool is specified.
         """
         super().__init__(name)
 
-        self.node("drc", drc.DRCTask())
+        if tool == "klayout":
+            self.node("drc", klayout_drc.DRCTask())
+        elif tool == "magic":
+            self.node("drc", magic_drc.DRCTask())
+        else:
+            raise ValueError(f'{tool} is not a supported tool')
 
 
 ##################################################
