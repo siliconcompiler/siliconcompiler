0101584020c6f152665d399fdf9e74867e52c034
Peter Gadfort <gadfort@zeroasic.com>
2026-08-18 11:24:16 -0400

    switch tool io to use path

--- siliconcompiler/tool.py
+++ siliconcompiler/tool.py
@@ -2956,7 +2956,7 @@ class Task(NamedSchema, PathSchema, DocsSchema):
         schema.insert(
             'input',
             Parameter(
-                '[file]',
+                '[path]',
                 scope=Scope.JOB,
                 pernode=PerNode.REQUIRED,
                 shorthelp="Task: input files",
@@ -2975,7 +2975,7 @@ class Task(NamedSchema, PathSchema, DocsSchema):
         schema.insert(
             'output',
             Parameter(
-                '[file]',
+                '[path]',
                 scope=Scope.JOB,
                 pernode=PerNode.REQUIRED,
                 shorthelp="Task: output files",
